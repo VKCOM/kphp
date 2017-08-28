a6e55ff21e09d8fbc7586843d81dc03dd8a78044
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2017-08-28 19:36:07 +0300

    kphp-disable-warnings attribute can be used in php-docs

--- compiler/compiler-core.cpp
+++ compiler/compiler-core.cpp
@@ -88,6 +88,7 @@ FunctionPtr CompilerCore::create_function (const FunctionInfo &info) {
   function->class_context_name = info.class_context;
   function->class_extends = info.extends;
   function->namespace_uses = info.namespace_uses;
+  function->disabled_warnings = info.disabled_warnings;
   function_root->set_func_id (function);
   function->file_id = stage::get_file();
 
