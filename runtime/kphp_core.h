8f5a4a062a07a30e13ff3ee22401252b6b9d5370
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2017-11-10 16:32:14 +0300

    added LIKELY function

--- runtime/kphp_core.h
+++ runtime/kphp_core.h
@@ -59,6 +59,7 @@ class UnknownType {
 #define NOERR(a, a_type) ({php_disable_warnings++; a_type a_tmp___ = a; php_disable_warnings--; a_tmp___;})
 #define NOERR_VOID(a) ({php_disable_warnings++; a; php_disable_warnings--;})
 
+#define f$LIKELY likely
 
 inline bool lt (const bool &lhs, const bool &rhs);
 
