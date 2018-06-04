f2d7a74709c2f64619af44144ac8cb783ec9e948
Eugene Nemchenko <n.eugene536@gmail.com>
2018-06-04 17:47:30 +0300

    replace `:==:` with `/*:= */`

--- tests/phpt/dl/951_infer.php
+++ tests/phpt/dl/951_infer.php
@@ -1,4 +1,4 @@
-@ok no_php
+@ok
 <?php
 define (A, 1);
 define (B, A + 1);
@@ -34,16 +34,16 @@ test19();
 function test_OrFalse() {
   $a = false;
   $a = true;
-  $a :==: bool;
+  $a /*:= bool*/;
 
   if (1) {
     $a = 1;
   } else {
     $a = false;
   }
-  $a :==: OrFalse < int >;
+  $a /*:= OrFalse < int >*/;
   $b = $a;
-  $b :==: OrFalse < int >;
+  $b /*:= OrFalse < int >*/;
         
   $a = false;
   if (1) {
@@ -56,7 +56,7 @@ function test_OrFalse() {
 
   //var_dump ($a);
 
-  $a :==: OrFalse < array <int> >;
+  $a /*:= OrFalse < array <int> >*/;
 
   json_decode (json_encode (NULL), false);
   
@@ -64,7 +64,7 @@ function test_OrFalse() {
   if (1) {
     $x = true;
   }
-  $x :==: var;
+  $x /*:= var*/;
   //var_dump($x);
 
   $a = false;
@@ -72,16 +72,16 @@ function test_OrFalse() {
     $a = 1;
   }
   $b = $a * $a;
-  $b :==: int;
+  $b /*:= int*/;
 
 
   function pass ($x) {
-    $x :==: OrFalse < int >;
+    $x /*:= OrFalse < int >*/;
     return $x;
   }
 
   $c = pass ($a);
-  $c :==: OrFalse < int >;
+  $c /*:= OrFalse < int >*/;
   //var_dump ($c);
   $a = false;
   $c = pass ($a);
@@ -93,7 +93,7 @@ function test_OrFalse() {
   if (1) {
     $a = false;
   }
-  $a :==: bool;
+  $a /*:= bool*/;
 
   function def_bool ($x = false) {
     return $x;
@@ -104,7 +104,7 @@ function test_OrFalse() {
   if (1) {
     $a = false;
   }
-  $a :==: OrFalse < int >;
+  $a /*:= OrFalse < int >*/;
 
   $arr = false;
   if (1) {
@@ -126,17 +126,17 @@ function test_OrFalse() {
   }
   $c = $arr []= $a;
   //if ($arr[0] !== false[> || $c !== false<]) echo "WA4\n";
-  $arr :==: OrFalse < array <OrFalse <int > > >;
+  $arr /*:= OrFalse < array <OrFalse <int > > >*/;
 
   list ($c) = $arr;
-  $c :==: OrFalse < int >;
+  $c /*:= OrFalse < int >*/;
   //if ($c !== false) echo "WA5\n";
 
   $d = "abc";
   foreach ($arr as $d) {
    //if ($d !== false) echo "WA5\n";
   }
-  $d :==: var;//foreach might not change $d
+  $d /*:= var*/;//foreach might not change $d
 
   $arr = array (false, 1);
   $arr2 = array (1);
@@ -152,7 +152,7 @@ function test_OrFalse() {
   $arr = array ("hello", false) + array ("a", 1);
   
   $arr = array ($a, 1=>$a, 1);
-  $arr :==: array <OrFalse < int > >;
+  $arr /*:= array <OrFalse < int > >*/;
   
   //if ($arr[0] !== false) {
     //echo "WA7\n";
@@ -172,11 +172,11 @@ function test_OrFalse() {
   //var_dump ($t);
 
   $arr1 = array (1);
-  $arr1 :==: array <int>;
+  $arr1 /*:= array <int>*/;
   $arr2 = array (false);
-  $arr2 :==: array <OrFalse < Unknown > >;
+  $arr2 /*:= array <OrFalse < Unknown > >*/;
   $arr3 = array_merge ($arr1, $arr2);
-  $arr3 :==: array <OrFalse < int > >;
+  $arr3 /*:= array <OrFalse < int > >*/;
 
 
   $arr = array (array (1), false);
@@ -189,7 +189,7 @@ function test_OrFalse() {
   if (1) {
     $arr1 = array (1, 2, 3);
   }
-  $arr1 :==: OrFalse <array <int> >;
+  $arr1 /*:= OrFalse <array <int> >*/;
   array_merge ($arr1, $arr1);
 }
 
@@ -205,19 +205,19 @@ function odd ($x) {
 //one set
 function test1() {
 
-  //$a :==: Unknown;
+  //$a /*:= Unknown*/;
   
   $a = 1;
-  $a :==: int;
+  $a /*:= int*/;
 
   $a = 1.0;
-  $a :==: float;
+  $a /*:= float*/;
   
   $a = "1";
-  $a :==: string;
+  $a /*:= string*/;
   
   $a = false;
-  $a :==: OrFalse < Unknown >;
+  $a /*:= OrFalse < Unknown >*/;
   
   if (1) {
     $a = 1;
@@ -225,33 +225,33 @@ function test1() {
     $a = "";
   }
 
-  $a :==: var;
+  $a /*:= var*/;
 
   $a = array(1, 2, 3);
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
   $a = array (1, "");
-  $a :==: array <var>;
+  $a /*:= array <var>*/;
 
   $a = array();
-  $a :==: array <Unknown>;
+  $a /*:= array <Unknown>*/;
 
   if (1) {
     $a = array();
   } else {
     $a = array (1);
   }
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
   $a = array (1, 1.0);
-  $a :==: array <float>;
+  $a /*:= array <float>*/;
 
   $array = array(213);
   shuffle ($array);
-  $array :==: array <int>;
+  $array /*:= array <int>*/;
 
   $array = array (1, 2, 3, 4, 5);
-  $array :==: array <int>;
+  $array /*:= array <int>*/;
   
   
   
@@ -265,42 +265,43 @@ function test1() {
 
 
 
-  $array :==: array <var>;
+  $array /*:= array <var>*/;
  
 
-  1 + 1 :==: int;
-  1.0 + 1 :==: float;
-  1000000 * 1000000 :==: int;
-  "1000000" * 1000000 :==: var;
+  1 + 1 /*:= int*/;
+  1.0 + 1 /*:= float*/;
+  1000000 * 1000000 /*:= int*/;
+  "1000000" * 1000000 /*:= var*/;
   asrt ("100" * 100 == 10000);
 
-  5 / 1 :==: float;
+  5 / 1 /*:= float*/;
 
   $a = array (1);
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
   if (1) {
     $a = array();
   }
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
   function def_empty_arr ($x = array()) {
     return $x;
   }
   $x = def_empty_arr();
-  $x :==: array <int>;
+  $x /*:= array <int>*/;
   def_empty_arr(array (1, 2, 3));
 
 
   $a = min (array (1, 2, 3));
-  $a :==: int;
+  $a /*:= int*/;
   $a = min (1, 1.5);
-  $a :==: float;
+  $a /*:= float*/;
   $a = max (false, "str");
 
   $a = intval ("123");
-  $a :==: int;
-  $a = arrayval (1);
+  $a /*:= int*/;
+  $a = (array) 1;
+  $a = "asdf";
 
   $a = array (array (array (1)), array());
 
@@ -310,12 +311,12 @@ function test1() {
     }
   }
   $x = return_op_empty();
-  $x :==: var;
+  $x /*:= var*/;
 
 
   //Warning on the next line
   $x = isset ($ttt);
-  $x :==: bool;
+  $x /*:= bool*/;
 }
 
 
@@ -403,10 +404,10 @@ function test7($val = B) {
   }
 
   $a = A;
-  $a :==: int;
+  $a /*:= int*/;
 
   $b = B;
-  $b :==: int;
+  $b /*:= int*/;
 }
 
 function test8($mid, $sid_data) {
@@ -470,7 +471,7 @@ function test13() {
   case "A":
     f($tmp = 1);
     //$xx = $tmp;
-    //$tmp :==: int;
+    //$tmp /*:= int*/;
     //$kludges = array ("attach1_type" => "audio");
     //for ($i = 1; $type = $kludges['attach'.$i.'_type']; ++$i) {
     //  if ($type == 'audio' && $kludges['attach'.$i]) {
@@ -481,7 +482,7 @@ function test13() {
   case "B":
     f ($tmp = "str");
     $xx = $tmp;
-    $tmp :==: string;
+    $tmp /*:= string*/;
     $kludges = array ("attach1_type" => "audio");
     for ($i = 1; $type = $kludges['attach'.$i.'_type']; ++$i) {
       if ($type == 'audio' && $kludges['attach'.$i]) {
@@ -497,11 +498,11 @@ function test13() {
 function test14() {
   $a = array();
   array_unshift ($a, 1);
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
   $a = array();
   array_push ($a, 1, 1.0);
-  $a :==: array <float>;
+  $a /*:= array <float>*/;
 
   //function return_void() {
   //  $v = 5; 
@@ -523,22 +524,22 @@ function test14() {
 function test16() {
   $a = 1;
   isset ($a);
-  $a :==: var;
+  $a /*:= var*/;
 
   $a = array (array(1, 2, 3));
   is_array ($a[1]);
-  $a :==: array <var>;
+  $a /*:= array <var>*/;
 
   $a = array (1, 2, 3);
   isset ($a[1]);
-  $a :==: array <int>;
+  $a /*:= array <int>*/;
 
 
   //$a = array (1, 2, 3);
   //$b = $a[1];
   //$c = $b;
   //isset ($c);
-  //$c :==: var;
+  //$c /*:= var*/;
 
   function get_var16(){
     $a = array (1);
@@ -550,7 +551,7 @@ function test16() {
   $b = get_var16();
   //Warning on the next line
   isset ($b);
-  $b :==: var;
+  $b /*:= var*/;
   function test($a) { 
     if ($a == 1) 
       return array('test'); 
@@ -581,7 +582,7 @@ function test16() {
   $b = $a[1];
   $c = $b;
   if ($c === false) {}
-  $c :==: var;
+  $c /*:= var*/;
 
   function check_var16($x) {
     is_array ($x);
@@ -597,17 +598,17 @@ function test16() {
 /** test extern_function **/
 ////extern_function ef1;
 ////function ef1 ($v) {
-////  $v :==: var;
+////  $v /*:= var*/;
 ////}
 
 //extern_function ef2($v);
 //function ef2 ($v) {
-  //$v :==: var;
+  //$v /*:= var*/;
   //return 1;
 //}
 //extern_function ef3 ($v ::: float) ::: array <var>;
 //function ef3 ($v) {
-  //$v :==: float;
+  //$v /*:= float*/;
   //return array (1, 2, 3);
 //}
 
@@ -619,11 +620,11 @@ function test16() {
 
 //function test12() {
   ////$res = ef1();
-  ////$res :==: var;
+  ////$res /*:= var*/;
   //$res = ef2("str");
-  //$res :==: var;
+  //$res /*:= var*/;
   //$res = ef3("str");
-  //$res :==: array <var>;
+  //$res /*:= array <var>*/;
 
   //ef4 (1, "t");
 //}
@@ -650,11 +651,11 @@ function test18() {
   }
   try {
     $a = 1;
-    $a :==: int;
+    $a /*:= int*/;
     $a = true;
-    $a :==: bool;
+    $a /*:= bool*/;
     $a = array(1);
-    $a :==: array <int>;
+    $a /*:= array <int>*/;
     f18($a);
   } catch (Exception $e) {
     echo "exception\n";
@@ -669,7 +670,7 @@ function test19() {
   $a = array ( array (1.0));
   foreach ($a as &$e) {
   }
-  $a :==: array <array <float> >;
+  $a /*:= array <array <float> >*/;
 }
 
 function test20() {
