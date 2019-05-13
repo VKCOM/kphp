cc3bc3a716284e8f4a1a815870a281e6c807b5ad
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2019-05-13 16:32:51 +0300

    add test lost long time ago

---
+++ tests/phpt/pk/013_sched_yield_sleep.php
@@ -0,0 +1,28 @@
+@ok
+<?php
+#ifndef KittenPHP
+?>
+1
+2
+3
+waited 1123456789
+waited 1123456790
+waited 1123456791
+<?php
+exit;
+#endif
+
+function f($x, $y) {
+  sched_yield_sleep($x);
+  echo $y . "\n";
+}
+
+$ids = [];
+$ids[] = fork(f(0.03, 3));
+$ids[] = fork(f(0.01, 1));
+$ids[] = fork(f(0.02, 2));
+
+foreach ($ids as $id) {
+  wait($id);
+  echo "waited $id\n";
+}
