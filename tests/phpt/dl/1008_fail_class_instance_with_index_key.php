d982965b5746887cdd8967b92c4e868e6296cead
Eugene Nemchenko <n.eugene536@gmail.com>
2019-06-19 18:25:20 +0300

    add fail test array_column + instance + index_key

---
+++ tests/phpt/dl/1008_fail_class_instance_with_index_key.php
@@ -0,0 +1,12 @@
+@kphp_should_fail
+<?php
+class A { 
+    public $x = 20; 
+
+    public function __construct($x = 20) {
+        $this->x = $x;
+    }
+}
+
+$a = [[new A(), new A()], [new A(), new A()]];
+$b = array_column($a, 0, 0); 
