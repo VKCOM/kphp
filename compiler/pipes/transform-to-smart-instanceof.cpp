abb0dcd42a739abbd37c0827d8578511e4c16b64
Eugene Nemchenko <n.eugene536@gmail.com>
2019-12-16 16:32:55 +0300

    fix instanceof

--- compiler/pipes/transform-to-smart-instanceof.cpp
+++ compiler/pipes/transform-to-smart-instanceof.cpp
@@ -10,7 +10,7 @@ bool TransformToSmartInstanceof::user_recursion(VertexPtr v, LocalT *, VisitVert
     return false;
   }
 
-  if (infer_class_of_expr(current_function, instance_var).assum_type == AssumType::assum_instance) {
+  if (infer_class_of_expr(current_function, instance_var).assum_type != AssumType::assum_instance) {
     kphp_error(false, "left operand of `instanceof` should be instance of class");
     return false;
   }
