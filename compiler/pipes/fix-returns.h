350d2fc3b95490a3f3d4c50238cc15d8f7fee324
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2020-03-25 18:47:10 +0300

    no warning on warn-unused-result function inside fork

--- compiler/pipes/fix-returns.h
+++ compiler/pipes/fix-returns.h
@@ -5,4 +5,8 @@
 class FixReturnsPass : public FunctionPassBase {
 public:
   VertexPtr on_enter_vertex(VertexPtr root, LocalT *local);
+
+  bool need_recursion(VertexPtr root, LocalT *) {
+    return root->type() != op_fork;
+  }
 };
