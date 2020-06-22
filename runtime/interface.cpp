d9c7cf279a7f50e92527c85ac0a7c09d3b43a7d8
vkcom <>
2020-06-22 17:46:26 +0300

    remove support for multi-line HTTP headers
    RFC 7230 deprecated multi-line headers in 2014:
    
    > A sender MUST NOT generate a message that includes
    > line folding (i.e., that has any field-value that contains a match to
    > the obs-fold rule) unless the message is intended for packaging
    > within the message/http media type.
    
    Given that e.g. nginx does not support multi-line headers,
    generating them can easily lead to header injection attacks.

--- runtime/interface.cpp
+++ runtime/interface.cpp
@@ -254,11 +254,6 @@ static void header(const char *str, int str_len, bool replace = true, int http_r
 
   for (int i = (int)(p - str + 1); i < str_len; i++) {
     if ((0 <= str[i] && str[i] <= 31) || str[i] == 127) {
-      if (i + 2 < str_len && str[i] == '\r' && str[i + 1] == '\n' && (str[i + 2] == ' ' || str[i + 2] == '\t')) {
-        php_warning("Multi-line header \"%s\": to prevent header injection, support for multi-line headers will be removed soon", name.c_str());        i += 2;
-        continue;
-      }
-
       php_warning("Wrong header value: \"%s\"", p + 1);
       return;
     }
