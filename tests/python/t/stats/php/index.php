0085498c7bfdab720c861b01b273dd9296aff448
Aleksandr Korepanov <korepanov.sasha@gmail.com>
2020-07-08 13:48:07 +0300

    Add smoke test for request percentiles.

---
+++ tests/python/t/stats/php/index.php
@@ -0,0 +1,20 @@
+<?php
+
+function sleep_for_net_time() {
+  sched_yield_sleep(0.01);
+}
+
+function wait_for_script_time() {
+  $t = microtime(true) + 0.01;
+  while($t > microtime(true)) {}
+}
+
+function run() {
+  wait_for_script_time();
+
+  $i1 = fork(sleep_for_net_time());
+  wait($i1);
+}
+
+run();
+echo "Hello world!";
