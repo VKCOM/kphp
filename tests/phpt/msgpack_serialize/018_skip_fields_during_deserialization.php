c712f1ececfc26c516d0cdf798925d4199f01b95
Eugene Nemchenko <n.eugene536@gmail.com>
2020-08-13 18:29:58 +0300

    skip old fields in instance_deserialization

---
+++ tests/phpt/msgpack_serialize/018_skip_fields_during_deserialization.php
@@ -0,0 +1,34 @@
+@ok
+<?php
+
+require_once 'polyfills.php';
+
+/** @kphp-serializable */
+class Before {
+    /**
+     * @kphp-serialized-field 2
+     * @var int
+     */
+    public $b = 10;
+
+   /**
+    * @kphp-serialized-field 1
+    * @var int
+    */
+   public $a = 228;
+}
+
+/** @kphp-serializable */
+class After {
+    /**
+     * @kphp-serialized-field 2
+     * @var int
+     */
+    public $b = 10;
+}
+
+$b = new Before();
+$b_raw = instance_serialize($b);
+
+$a_new = instance_deserialize($b_raw, After::class);
+var_dump($a_new->b);
