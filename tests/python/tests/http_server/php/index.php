8ff24cabcca2cdf7eb4bfc5a97cf13568f735ee6
Denis Vaksman <dr.denisv2016@yandex.ru>
2020-10-18 18:50:47 +0300

    add HTTP graceful shutdown test

--- tests/python/tests/http_server/php/index.php
+++ tests/python/tests/http_server/php/index.php
@@ -3,6 +3,13 @@
 
 if ($_SERVER["PHP_SELF"] === "/ini_get") {
   echo ini_get($_SERVER["QUERY_STRING"]);
-} else {
+} else if ($_SERVER["PHP_SELF"] === "/sleep") {
+  $sleep_time = (int)$_GET["time"];
+  echo "before sleep $sleep_time\n";
+  fwrite(STDERR, "sleeping for $sleep_time sec ...\n");
+  sleep($sleep_time);
+  fwrite(STDERR, "wake up!");
+  echo "after sleep";
+}  else {
   echo "Hello world!";
 }
