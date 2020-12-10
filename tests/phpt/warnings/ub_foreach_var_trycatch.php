f3ee7c2e439d91b8b27ce770b29b88d59afc6b5c
Iskander Sharipov <i.sharipov@corp.vk.com>
2020-12-10 11:08:14 +0300

    fix CheckUBF for op_catch + add some tests for it

---
+++ tests/phpt/warnings/ub_foreach_var_trycatch.php
@@ -0,0 +1,12 @@
+@kphp_should_warn
+/Dangerous undefined behaviour unset, \[foreach var = arr\]/
+<?php
+
+try {
+  throw new Exception();
+} catch (Exception $e) {
+  $arr = ['something' => 1];
+  foreach ($arr as &$item) {
+    unset($arr['something']);
+  }
+}
---
+++ tests/phpt/warnings/ub_var_trycatch.php
@@ -0,0 +1,11 @@
+@kphp_should_warn
+/Dangerous undefined behaviour \+, \[var = x\]/
+<?php
+
+try {
+  $x = 10;
+  throw new Exception();
+} catch (Exception $e) {
+  $y = $x++ + $x++;
+  var_dump($y);
+}
