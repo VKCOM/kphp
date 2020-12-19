7fc451267f3b89b816ad2fc1db6fdeac80eb2a86
Aleksandr Korepanov <korepanov.sasha@gmail.com>
2020-12-19 11:01:25 +0300

    Don't crash zlib deconding on large data.

--- tests/phpt/dl/464_gzip.php
+++ tests/phpt/dl/464_gzip.php
@@ -8,20 +8,39 @@ function gzdecode($string) {
 }
 }
 #endif
-  $s = gzcompress ("Hello world");
 
-  $f = fopen ("test.gz", "w");
-  fwrite ($f, $s);
-  fclose ($f);
 
-  var_dump (gzuncompress (file_get_contents ("test.gz")));
+function test_basic() {
+  $s = gzcompress("Hello world");
 
-  $s = gzencode ("Hello world");
+  $f = fopen("test.gz", "w");
+  fwrite($f, $s);
+  fclose($f);
 
-  $f = fopen ("test.gz", "w");
-  fwrite ($f, $s);
-  fclose ($f);
+  var_dump(gzuncompress(file_get_contents("test.gz")));
 
-  var_dump (gzdecode (file_get_contents ("test.gz")));
+  $s = gzencode("Hello world");
 
-  unlink ("test.gz");
+  $f = fopen("test.gz", "w");
+  fwrite($f, $s);
+  fclose($f);
+
+  var_dump(gzdecode(file_get_contents("test.gz")));
+
+  unlink("test.gz");
+}
+
+function test_large_string() {
+  $s = str_repeat("x", 1 + 8*1024*1024);
+  $compressed = gzcompress($s);
+  var_dump(strlen($compressed));
+
+  $unc = gzuncompress($compressed);
+#ifndef KPHP
+  $unc = "";
+#endif
+  var_dump($unc);
+}
+
+test_basic();
+test_large_string();
