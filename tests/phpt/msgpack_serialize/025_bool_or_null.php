6a4b39e7166d4c29f19e5bb938605957e7330428
Eugene Nemchenko <n.eugene536@gmail.com>
2021-03-26 09:58:04 +0300

    fix deserialization of `?bool` (#170)

---
+++ tests/phpt/msgpack_serialize/025_bool_or_null.php
@@ -0,0 +1,22 @@
+@ok
+<?php
+
+require_once 'kphp_tester_include.php';
+
+/** @kphp-serializable **/
+class A {
+    /**
+     * @kphp-serialized-field 1
+     * @var ?bool
+     */
+    public $bool_f = null;
+
+    public function __construct() {
+        $this->bool_f = true;
+    }
+}
+
+$a = new A();
+$s = instance_serialize($a);
+$d = instance_deserialize($s, A::class);
+var_dump($d === null);
