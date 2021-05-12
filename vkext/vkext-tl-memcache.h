e3c0a3e814b6f419969dc524f544ff67984bf5b4
Denis Vaksman <dr.denisv2016@yandex.ru>
2021-05-12 16:00:48 +0300

    [vkext] remove old RpcMemcache

--- vkext/vkext-tl-memcache.h
+++ vkext/vkext-tl-memcache.h
@@ -4,11 +4,5 @@
 
 #ifndef __VKEXT_TL_MEMCACHE_H__
 #define __VKEXT_TL_MEMCACHE_H__
-
-void rpc_memcache_connect(INTERNAL_FUNCTION_PARAMETERS);
-void rpc_memcache_delete(INTERNAL_FUNCTION_PARAMETERS);
-void rpc_memcache_incr(int op, INTERNAL_FUNCTION_PARAMETERS);
-void rpc_memcache_store(int op, INTERNAL_FUNCTION_PARAMETERS);
-void rpc_memcache_get(INTERNAL_FUNCTION_PARAMETERS);
 void php_rpc_fetch_memcache_value(INTERNAL_FUNCTION_PARAMETERS);
 #endif
