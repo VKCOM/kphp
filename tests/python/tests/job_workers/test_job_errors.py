03ea3659ba463999d21d4bf546c2d0f3b5700fce
Denis Vaksman <dr.denisv2016@yandex.ru>
2021-07-14 12:29:35 +0300

    [tests] fix unstable test

--- tests/python/tests/job_workers/test_job_errors.py
+++ tests/python/tests/job_workers/test_job_errors.py
@@ -76,8 +76,8 @@ class TestJobErrors(KphpServerAutoTestCase):
         ])
 
     def test_job_stack_overflow_error(self):
-        self.job_error_test_impl("stack_overflow", self.JOB_STACK_OVERFLOW_ERROR)
-        self.kphp_server.assert_log(2 * [
+        self.job_error_test_impl("stack_overflow", self.JOB_STACK_OVERFLOW_ERROR, data=[[1, 2, 3, 4, 5]], buffers=2)
+        self.kphp_server.assert_log([
             "Critical error during script execution: sigsegv\\(stack overflow\\)",
             "Error -1: Callstack overflow"
         ])
