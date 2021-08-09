d59e58f118fdd61f0b1f4a940badaded3748605c
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2021-08-09 12:41:55 +0300

    allow empty escape_char in fgetcsv/fputcsv functions (#287)
    This is how it works in PHP7.4.
    
    See https://bugs.php.net/bug.php?id=51496

---
+++ tests/phpt/streams/fgetcsv.php
@@ -0,0 +1,12 @@
+@ok php7_4
+<?php
+
+function test_empty_escape() {
+  $stream = fopen(__DIR__ . '/csv_input.txt', 'r');
+  while (($data = fgetcsv($stream, 0, ',', '"', '')) !== false) {
+      var_dump($data);
+  }
+  fclose($stream);
+}
+
+test_empty_escape();
