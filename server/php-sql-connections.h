1821b4bd26f5de2230114ef0fd12a71279ca06e9
Artyom Ivashkevich <artyom.ivashkevich@gmail.com>
2021-08-30 17:04:59 +0700

    mysql with user, password, port and remote host, utf8 header parameter (#288)

--- server/php-sql-connections.h
+++ server/php-sql-connections.h
@@ -11,4 +11,7 @@
 
 void php_worker_run_sql_query_packet(php_worker *worker, php_net_query_packet_t *query);
 bool set_mysql_db_name(const char *db_name);
+bool set_mysql_user(const char *user);
+bool set_mysql_password(const char *password);
+void disable_mysql_same_datacenter_check();
 extern conn_target_t db_ct;
