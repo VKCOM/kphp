bc765b34ef9032fcd2b09f05090ea743ae0d7394
Stanislav Eismont <stanislav.eismont@gmail.com>
2021-10-05 17:05:04 +0300

    fix test; after migrating to c++17 standard evaluation order of expressions are fixated (#326)

--- tests/phpt/phc/codegen/long_opeq.php
+++ tests/phpt/phc/codegen/long_opeq.php
@@ -10,22 +10,7 @@
 		return 1;
 	}
 
-#ifndef KPHP
-  $_6 = f("six");
-  $_3 = f("three");
-  $_1 = f("one");
-  $x[$_1][2][$_3][4][5][$_6] += 1;
+  $x[f("one")][2][f("three")][4][5][f("six")] += 1;
 
-  if (false) {
-#endif
-  if (strpos(get_engine_version(), "Clang") !== false) {
-  	$x[f("six")][2][f("three")][4][5][f("one")] += 1;
-  } else {
-    $x[f("one")][2][f("three")][4][5][f("six")] += 1;
-  }
-
-#ifndef KPHP
-  }
-#endif
 	var_dump($x);
 ?>
