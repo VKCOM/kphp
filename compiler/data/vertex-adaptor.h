a9d36a37b8d0a72745acc214e3d6120685f1731b
Stanislav Eismont <stanislav.eismont@gmail.com>
2021-12-09 18:01:21 +0300

    fix caused by rule of five (#386)
    * fix c++ rule of five by removing strange defined copy assignment
    
    * minor code cleanup

--- compiler/data/vertex-adaptor.h
+++ compiler/data/vertex-adaptor.h
@@ -7,8 +7,8 @@
 #include "auto/compiler/vertex/is-base-of.h"
 #include "common/wrappers/fmt_format.h"
 
+#include "compiler/location.h"
 #include "compiler/operation.h"
-#include "compiler/stage.h"
 
 template<Operation Op>
 class vertex_inner;
@@ -16,161 +16,150 @@ class vertex_inner;
 template<Operation Op>
 class VertexAdaptor;
 
-class Location;
-
 using VertexPtr = VertexAdaptor<meta_op_base>;
 
 VertexPtr clone_vertex(VertexPtr);
 
 template<Operation Op>
 class VertexAdaptor {
-  vertex_inner<Op> *impl;
+  vertex_inner<Op> *impl_{nullptr};
+
+  template<Operation Op2>
+  friend class VertexAdaptor;
+
 public:
   struct Hash {
     size_t operator()(const VertexAdaptor<Op> &arg) const noexcept {
-      return reinterpret_cast<size_t>(arg.impl);
+      return reinterpret_cast<size_t>(arg.impl_);
     }
   };
 
-public:
+  VertexAdaptor() = default;
 
-  VertexAdaptor() :
-    impl(nullptr) {
-  }
-
-  explicit VertexAdaptor(vertex_inner<Op> *impl) :
-    impl(impl) {
-  }
+  explicit VertexAdaptor(vertex_inner<Op> *impl) noexcept
+    : impl_(impl) {}
 
   template<Operation FromOp>
-  VertexAdaptor(const VertexAdaptor<FromOp> &from) :
-    impl(static_cast<vertex_inner<Op> *>(from.impl)) {
+  VertexAdaptor(const VertexAdaptor<FromOp> &from) noexcept
+    : impl_(static_cast<vertex_inner<Op> *>(from.impl_)) {
     static_assert(op_type_is_base_of(Op, FromOp), "Strange cast to not base vertex");
   }
 
   template<Operation FromOp>
-  VertexAdaptor &operator=(const VertexAdaptor<FromOp> &from) & {
+  VertexAdaptor<Op> &operator=(const VertexAdaptor<FromOp> &from) noexcept {
     static_assert(op_type_is_base_of(Op, FromOp), "Strange assignment to not base vertex");
-    impl = static_cast<vertex_inner<Op> *> (from.impl);
+    impl_ = static_cast<vertex_inner<Op> *>(from.impl_);
     return *this;
   }
 
-  VertexAdaptor &operator=(const VertexAdaptor<Op> &) & = default;
-
-  explicit operator bool() const {
-    return impl != nullptr;
+  explicit operator bool() const noexcept {
+    return impl_ != nullptr;
   }
 
-  vertex_inner<Op> *operator->() {
-    assert(impl != nullptr);
-    return impl;
+  vertex_inner<Op> *operator->() noexcept {
+    assert(impl_ != nullptr);
+    return impl_;
   }
 
-  const vertex_inner<Op> *operator->() const {
-    assert(impl != nullptr);
-    return impl;
+  const vertex_inner<Op> *operator->() const noexcept {
+    assert(impl_ != nullptr);
+    return impl_;
   }
 
-  vertex_inner<Op> &operator*() {
-    assert(impl != nullptr);
-    return *impl;
+  vertex_inner<Op> &operator*() noexcept {
+    assert(impl_ != nullptr);
+    return *impl_;
   }
 
-  const vertex_inner<Op> &operator*() const {
-    assert(impl != nullptr);
-    return *impl;
+  const vertex_inner<Op> &operator*() const noexcept {
+    assert(impl_ != nullptr);
+    return *impl_;
   }
 
   template<Operation to>
-  VertexAdaptor<to> as() const {
-    if (!impl) {
+  VertexAdaptor<to> as() const noexcept {
+    if (!impl_) {
       return {};
     };
     auto res = try_as<to>();
-    kphp_assert_msg(res, fmt_format("Can't cast VertexAdaptor<{}>(real type {}) to VertexAdaptor<{}>",
-                                    OpInfo::op_str(Op), impl ? OpInfo::op_str(impl->type()) : "nullptr", OpInfo::op_str(to)));
+    kphp_assert_msg(res, fmt_format("Can't cast VertexAdaptor<{}>(real type {}) to VertexAdaptor<{}>", OpInfo::op_str(Op),
+                                    impl_ ? OpInfo::op_str(impl_->type()) : "nullptr", OpInfo::op_str(to)));
     return res;
   }
 
   template<Operation to>
-  VertexAdaptor<to> try_as() const {
-    if (!impl) {
+  VertexAdaptor<to> try_as() const noexcept {
+    if (!impl_) {
       return {};
     };
     static_assert(op_type_is_base_of(Op, to), "Strange downcast to not derived vertex");
     static_assert(Op != to || Op == meta_op_base, "Useless cast");
-    if (op_type_is_base_of(to, impl->type())) {
-      return VertexAdaptor<to>{static_cast<vertex_inner<to> *>(impl)};
+    if (op_type_is_base_of(to, impl_->type())) {
+      return VertexAdaptor<to>{static_cast<vertex_inner<to> *>(impl_)};
     }
 
     return {};
   }
 
-  static void init_properties(OpProperties *x) {
-    vertex_inner<Op>::init_properties(x);
+  static void init_properties(OpProperties *prop) {
+    vertex_inner<Op>::init_properties(prop);
   }
 
   template<typename... Args>
-  static VertexAdaptor<Op> create(Args &&... args) {
-    return VertexAdaptor<Op>(vertex_inner<Op>::create(std::forward<Args>(args)...));
+  static VertexAdaptor<Op> create(Args &&...args) noexcept {
+    return VertexAdaptor<Op>{vertex_inner<Op>::create(std::forward<Args>(args)...)};
   }
 
   template<typename... Args>
-  static VertexAdaptor<Op> create_vararg(Args &&... args) {
-    return VertexAdaptor<Op>(vertex_inner<Op>::create_vararg(std::forward<Args>(args)...));
+  static VertexAdaptor<Op> create_vararg(Args &&...args) noexcept {
+    return VertexAdaptor<Op>{vertex_inner<Op>::create_vararg(std::forward<Args>(args)...)};
   }
 
-  VertexAdaptor<Op> clone() const {
-    if (impl == nullptr) {
-      return {};
-    }
-    return VertexAdaptor<Op>(clone_vertex(*this).template as<Op>());
+  VertexAdaptor<Op> clone() const noexcept {
+    return impl_ ? VertexAdaptor<Op>(clone_vertex(*this).template as<Op>()) : VertexAdaptor<Op>{};
   }
-
-  template<Operation Op2>
-  friend class VertexAdaptor;
 
   bool operator==(const VertexAdaptor<Op> &other) const noexcept {
-    return impl == other.impl;
+    return impl_ == other.impl_;
   }
 
   bool operator!=(const VertexAdaptor<Op> &other) const noexcept {
-    return impl != other.impl;
+    return impl_ != other.impl_;
   }
 
-  VertexAdaptor &set_rl_type(RLValueType valueType) {
-    impl->rl_type = valueType;
+  VertexAdaptor<Op> &set_rl_type(RLValueType type) noexcept {
+    impl_->rl_type = type;
     return *this;
   }
 
-  VertexAdaptor &set_location(const Location &location) {
-    impl->location = location;
+  VertexAdaptor<Op> &set_location(const Location &location) noexcept {
+    impl_->location = location;
     return *this;
   }
 
   template<Operation Op2>
-  VertexAdaptor &set_location(VertexAdaptor<Op2> v) {
-    impl->location = v.impl->location;
+  VertexAdaptor<Op> &set_location(VertexAdaptor<Op2> other) noexcept {
+    impl_->location = other.impl_->location;
     return *this;
   }
 
-  VertexAdaptor &set_location_recursively(Location l) {
-    set_location(l);
-    for (auto child: *impl) {
-      child.set_location_recursively(l);
+  VertexAdaptor<Op> &set_location_recursively(const Location &location) noexcept {
+    set_location(location);
+    for (auto child : *impl_) {
+      child.set_location_recursively(location);
     }
     return *this;
   }
 
   template<Operation Op2>
-  VertexAdaptor &set_location_recursively(VertexAdaptor<Op2> v) {
-    return set_location_recursively(v.impl->location);
+  VertexAdaptor<Op> &set_location_recursively(VertexAdaptor<Op2> other) noexcept {
+    return set_location_recursively(other.impl_->location);
   }
 
   // use 'vertex.debugPrint()' anywhere in your code while development
   // (in release it's not used and is not linked to a binary)
-  void debugPrint() const {
-    std::string debugVertexTree(VertexPtr root);   // implemented in debug.cpp
+  void debugPrint() const noexcept {
+    std::string debugVertexTree(VertexPtr root); // implemented in debug.cpp
     printf("%s", debugVertexTree(VertexPtr(*this)).c_str());
   }
 };
