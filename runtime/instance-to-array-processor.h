fa2b757bc1016bdaf439a754f23b4cf13c9d39b9
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-01-21 10:22:13 +0300

    refactoring instance_to_array() (#413)
    * DRY instance_to_array()
    
    * make InstanceToArrayVisitor::flush_result() method rvalue
    
    * use fold instead of variadic template recursion for tuples processing
    
    * minor shape processing cleanup

--- runtime/instance-to-array-processor.h
+++ runtime/instance-to-array-processor.h
@@ -7,17 +7,14 @@
 #include "runtime/kphp_core.h"
 
 template<class T>
-std::enable_if_t<std::is_empty<T>{}, array<mixed>> f$instance_to_array(const class_instance<T> &, bool with_class_names = false);
-
-template<class T>
-std::enable_if_t<!std::is_empty<T>{}, array<mixed>> f$instance_to_array(const class_instance<T> &c, bool with_class_names = false);
+array<mixed> f$instance_to_array(const class_instance<T> &c, bool with_class_names = false);
 
 class InstanceToArrayVisitor {
 public:
   explicit InstanceToArrayVisitor(bool with_class_names)
     : with_class_names_(with_class_names) {}
 
-  array<mixed> flush_result() {
+  array<mixed> flush_result() && noexcept {
     return std::move(result_);
   }
 
@@ -56,36 +53,31 @@ private:
 
   template<class ...Args>
   void process_impl(const char *field_name, const std::tuple<Args...> &value) {
-    InstanceToArrayVisitor tuple_processor(with_class_names_);
+    InstanceToArrayVisitor tuple_processor{with_class_names_};
     tuple_processor.result_.reserve(sizeof...(Args), 0, true);
 
-    process_tuple(value, tuple_processor);
-    add_value(field_name, tuple_processor.flush_result());
+    process_tuple(value, tuple_processor, std::index_sequence_for<Args...>{});
+    add_value(field_name, std::move(tuple_processor).flush_result());
   }
 
   template<size_t ...Is, typename ...T>
   void process_impl(const char *field_name, const shape<std::index_sequence<Is...>, T...> &value) {
-    InstanceToArrayVisitor shape_processor(with_class_names_);
+    InstanceToArrayVisitor shape_processor{with_class_names_};
     shape_processor.result_.reserve(sizeof...(Is), 0, true);
 
     process_shape(value, shape_processor);
-    add_value(field_name, shape_processor.flush_result());
+    add_value(field_name, std::move(shape_processor).flush_result());
   }
 
-  template<size_t Index = 0, class ...Args>
-  std::enable_if_t<Index != sizeof...(Args)> process_tuple(const std::tuple<Args...> &value, InstanceToArrayVisitor &tuple_visitor) {
-    tuple_visitor.process_impl("", std::get<Index>(value));
-    process_tuple<Index + 1>(value, tuple_visitor);
+  template<class... Args, std::size_t... Indexes>
+  void process_tuple(const std::tuple<Args...> &tuple, InstanceToArrayVisitor &visitor, std::index_sequence<Indexes...> /*indexes*/) {
+    (visitor.process_impl("", std::get<Indexes>(tuple)), ...);
   }
 
-  template<size_t Index = 0, class ...Args>
-  std::enable_if_t<Index == sizeof...(Args)> process_tuple(const std::tuple<Args...> &, InstanceToArrayVisitor &/*tuple_processor*/) {
-  }
-
-  template<size_t ...Is, typename ...T>
-  void process_shape(const shape<std::index_sequence<Is...>, T...> &value, InstanceToArrayVisitor &shape_visitor) {
+  template<size_t... Is, typename... T>
+  void process_shape(const shape<std::index_sequence<Is...>, T...> &shape, InstanceToArrayVisitor &visitor) {
     // shape doesn't have key names at runtime, that's why result will be a vector-array (whereas associative in PHP)
-    std::initializer_list<int32_t>{((void)shape_visitor.process_impl("", value.template get<Is>()), 0)...};
+    (visitor.process_impl("", shape.template get<Is>()), ...);
   }
 
   template<class T>
@@ -97,34 +89,25 @@ private:
     }
   }
 
-private:
   array<mixed> result_;
   bool with_class_names_{false};
 };
 
-inline void set_class_name_assoc(array<mixed> &arr, const char *class_name) {
-  arr.set_value(string("__class_name"), string(class_name));
-}
-
 template<class T>
-std::enable_if_t<std::is_empty<T>{}, array<mixed>> f$instance_to_array(const class_instance<T> &c, bool with_class_names) {
+array<mixed> f$instance_to_array(const class_instance<T> &c, bool with_class_names) {
   array<mixed> result;
-  if (with_class_names) {
-    set_class_name_assoc(result, c.get_class());
-  }
-  return result;
-}
-
-template<class T>
-std::enable_if_t<!std::is_empty<T>{}, array<mixed>> f$instance_to_array(const class_instance<T> &c, bool with_class_names) {
   if (c.is_null()) {
-    return {};
+    return result;
   }
-  InstanceToArrayVisitor visitor(with_class_names);
-  c.get()->accept(visitor);
-  array<mixed> result = visitor.flush_result();
+
+  if constexpr (!std::is_empty_v<T>) {
+    InstanceToArrayVisitor visitor{with_class_names};
+    c.get()->accept(visitor);
+    result = std::move(visitor).flush_result();
+  }
+
   if (with_class_names) {
-    set_class_name_assoc(result, c.get_class());
+    result.set_value(string("__class_name"), string(c.get_class()));
   }
   return result;
 }
