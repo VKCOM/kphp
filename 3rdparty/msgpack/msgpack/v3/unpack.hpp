c11388de699a4ead18b10ba09f3a845078d5a730
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-05-25 15:56:46 +0300

    reduce unpack

--- 3rdparty/msgpack/msgpack/v3/unpack.hpp
+++ 3rdparty/msgpack/msgpack/v3/unpack.hpp
@@ -12,7 +12,6 @@
 
 #include "msgpack/unpack_decl.hpp"
 #include "msgpack/parse.hpp"
-#include "msgpack/create_object_visitor.hpp"
 
 namespace msgpack {
 
@@ -20,7 +19,7 @@ namespace msgpack {
 MSGPACK_API_VERSION_NAMESPACE(v3) {
 /// @endcond
 
-inline msgpack::object_handle unpack(
+static msgpack::object_handle unpack(
     const char* data, std::size_t len, std::size_t& off, bool& referenced,
     msgpack::unpack_reference_func f, void* user_data,
     msgpack::unpack_limit const& limit
@@ -50,136 +49,6 @@ inline msgpack::object_handle unpack(
 {
     bool referenced;
     return msgpack::v3::unpack(data, len, off, referenced, f, user_data, limit);
-}
-
-inline msgpack::object_handle unpack(
-    const char* data, std::size_t len, bool& referenced,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    std::size_t off = 0;
-    return msgpack::v3::unpack(data, len, off, referenced, f, user_data, limit);
-}
-
-inline msgpack::object_handle unpack(
-    const char* data, std::size_t len,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    bool referenced;
-    std::size_t off = 0;
-    return msgpack::v3::unpack(data, len, off, referenced, f, user_data, limit);
-}
-
-inline void unpack(
-    msgpack::object_handle& result,
-    const char* data, std::size_t len, std::size_t& off, bool& referenced,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    msgpack::object obj;
-    msgpack::unique_ptr<msgpack::zone> z(new msgpack::zone);
-    referenced = false;
-    parse_return ret = detail::unpack_imp(
-        data, len, off, *z, obj, referenced, f, user_data, limit);
-
-    switch(ret) {
-    case msgpack::PARSE_SUCCESS:
-        result.set(obj);
-        result.zone() = msgpack::move(z);
-        return;
-    case msgpack::PARSE_EXTRA_BYTES:
-        result.set(obj);
-        result.zone() = msgpack::move(z);
-        return;
-    default:
-        return;
-    }
-}
-
-inline void unpack(
-    msgpack::object_handle& result,
-    const char* data, std::size_t len, std::size_t& off,
-    msgpack::v3::unpack_reference_func f, void* user_data,
-            msgpack::unpack_limit const& limit)
-{
-    bool referenced;
-    msgpack::v3::unpack(result, data, len, off, referenced, f, user_data, limit);
-}
-
-inline void unpack(
-    msgpack::object_handle& result,
-    const char* data, std::size_t len, bool& referenced,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    std::size_t off = 0;
-    msgpack::v3::unpack(result, data, len, off, referenced, f, user_data, limit);
-}
-
-inline void unpack(
-    msgpack::object_handle& result,
-    const char* data, std::size_t len,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    bool referenced;
-    std::size_t off = 0;
-    msgpack::v3::unpack(result, data, len, off, referenced, f, user_data, limit);
-}
-
-
-inline msgpack::object unpack(
-    msgpack::zone& z,
-    const char* data, std::size_t len, std::size_t& off, bool& referenced,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    msgpack::object obj;
-    referenced = false;
-    parse_return ret = detail::unpack_imp(
-        data, len, off, z, obj, referenced, f, user_data, limit);
-
-    switch(ret) {
-    case msgpack::PARSE_SUCCESS:
-        return obj;
-    case msgpack::PARSE_EXTRA_BYTES:
-        return obj;
-    default:
-        break;
-    }
-    return obj;
-}
-
-inline msgpack::object unpack(
-    msgpack::zone& z,
-    const char* data, std::size_t len, std::size_t& off,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    bool referenced;
-    return msgpack::v3::unpack(z, data, len, off, referenced, f, user_data, limit);
-}
-
-inline msgpack::object unpack(
-    msgpack::zone& z,
-    const char* data, std::size_t len, bool& referenced,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    std::size_t off = 0;
-    return msgpack::v3::unpack(z, data, len, off, referenced, f, user_data, limit);
-}
-
-inline msgpack::object unpack(
-    msgpack::zone& z,
-    const char* data, std::size_t len,
-    msgpack::unpack_reference_func f, void* user_data,
-    msgpack::unpack_limit const& limit)
-{
-    bool referenced;
-    std::size_t off = 0;
-    return msgpack::v3::unpack(z, data, len, off, referenced, f, user_data, limit);
 }
 
 /// @cond
