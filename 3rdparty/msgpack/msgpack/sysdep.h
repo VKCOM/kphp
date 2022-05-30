d4d7f1e029174a7846d075dd74e7e1508883abf5
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-05-30 13:07:35 +0300

    drop support of msvc

--- 3rdparty/msgpack/msgpack/sysdep.h
+++ 3rdparty/msgpack/msgpack/sysdep.h
@@ -9,56 +9,11 @@
  */
 #pragma once
 
-#include <stdlib.h>
-#include <stddef.h>
+#include <cstdlib>
+#include <cstddef>
+#include <cstdint>
 
-#if defined(_MSC_VER) && _MSC_VER <= 1800
-#   define snprintf(buf, len, format,...) _snprintf_s(buf, len, len, format, __VA_ARGS__)
-#endif
-
-#if defined(_MSC_VER) && _MSC_VER < 1600
-    typedef signed __int8 int8_t;
-    typedef unsigned __int8 uint8_t;
-    typedef signed __int16 int16_t;
-    typedef unsigned __int16 uint16_t;
-    typedef signed __int32 int32_t;
-    typedef unsigned __int32 uint32_t;
-    typedef signed __int64 int64_t;
-    typedef unsigned __int64 uint64_t;
-#elif defined(_MSC_VER)  // && _MSC_VER >= 1600
-#   include <stdint.h>
-#else
-#   include <stdint.h>
-#   include <stdbool.h>
-#endif
-
-#ifdef _WIN32
-#   define _msgpack_atomic_counter_header <windows.h>
-#   if !defined(WIN32_LEAN_AND_MEAN)
-#       define WIN32_LEAN_AND_MEAN
-#   endif /* WIN32_LEAN_AND_MEAN */
-    typedef long _msgpack_atomic_counter_t;
-#   define _msgpack_sync_decr_and_fetch(ptr) InterlockedDecrement(ptr)
-#   define _msgpack_sync_incr_and_fetch(ptr) InterlockedIncrement(ptr)
-#else
-    typedef unsigned int _msgpack_atomic_counter_t;
-#   define _msgpack_sync_decr_and_fetch(ptr) __sync_sub_and_fetch(ptr, 1)
-#   define _msgpack_sync_incr_and_fetch(ptr) __sync_add_and_fetch(ptr, 1)
-#endif
-
-#ifdef _WIN32
-
-#   ifdef __cplusplus
-    /* numeric_limits<T>::min,max */
-#       ifdef max
-#           undef max
-#       endif
-#       ifdef min
-#           undef min
-#       endif
-#   endif
-
-#elif defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__)
+#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__)
 
 #include <arpa/inet.h>  /* __BYTE_ORDER */
 #   if defined(linux)
@@ -75,7 +30,7 @@ static_assert(BYTE_ORDER == LITTLE_ENDIAN);
 #   else
 #       if defined(ntohs)
 #           define _msgpack_be16(x) ntohs(x)
-#       elif defined(_byteswap_ushort) || (defined(_MSC_VER) && _MSC_VER >= 1400)
+#       elif defined(_byteswap_ushort)
 #           define _msgpack_be16(x) ((uint16_t)_byteswap_ushort((unsigned short)x))
 #       else
 #           define _msgpack_be16(x) ( \
@@ -89,7 +44,7 @@ static_assert(BYTE_ORDER == LITTLE_ENDIAN);
 #   else
 #       if defined(ntohl)
 #           define _msgpack_be32(x) ntohl(x)
-#       elif defined(_byteswap_ulong) || (defined(_MSC_VER) && _MSC_VER >= 1400)
+#       elif defined(_byteswap_ulong)
 #           define _msgpack_be32(x) ((uint32_t)_byteswap_ulong((unsigned long)x))
 #       else
 #           define _msgpack_be32(x) \
@@ -100,7 +55,7 @@ static_assert(BYTE_ORDER == LITTLE_ENDIAN);
 #       endif
 #   endif
 
-#   if defined(_byteswap_uint64) || (defined(_MSC_VER) && _MSC_VER >= 1400)
+#   if defined(_byteswap_uint64)
 #        define _msgpack_be64(x) (_byteswap_uint64(x))
 #   elif defined(bswap_64)
 #        define _msgpack_be64(x) bswap_64(x)
@@ -138,33 +93,6 @@ static_assert(BYTE_ORDER == LITTLE_ENDIAN);
     do { uint32_t val = _msgpack_be32(num); memcpy(to, &val, 4); } while(0)
 #define _msgpack_store64(to, num) \
     do { uint64_t val = _msgpack_be64(num); memcpy(to, &val, 8); } while(0)
-
-/*
-#define _msgpack_load16(cast, from) \
-    ({ cast val; memcpy(&val, (char*)from, 2); _msgpack_be16(val); })
-#define _msgpack_load32(cast, from) \
-    ({ cast val; memcpy(&val, (char*)from, 4); _msgpack_be32(val); })
-#define _msgpack_load64(cast, from) \
-    ({ cast val; memcpy(&val, (char*)from, 8); _msgpack_be64(val); })
-*/
-
-
-#if !defined(__cplusplus) && defined(_MSC_VER)
-#  if !defined(FALSE)
-#    define FALSE (0)
-#  endif
-#  if !defined(TRUE)
-#    define TRUE (!FALSE)
-#  endif
-#  if _MSC_VER >= 1800
-#    include <stdbool.h>
-#  else
-#    define bool int
-#    define true TRUE
-#    define false FALSE
-#  endif
-#  define inline __inline
-#endif
 
 #ifdef __APPLE__
 #  include <TargetConditionals.h>
