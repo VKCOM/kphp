6277d599e7d77207065f945fc89c431bc038f6af
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-05-30 16:39:24 +0300

    remove useless code from zone class

--- runtime/msgpack/zone.cpp
+++ runtime/msgpack/zone.cpp
@@ -15,71 +15,6 @@
 
 namespace msgpack {
 
-void zone::finalizer_array::call() {
-  finalizer *fin = m_tail;
-  for (; fin != m_array; --fin)
-    (*(fin - 1))();
-}
-
-zone::finalizer_array::~finalizer_array() {
-  call();
-  ::free(m_array);
-}
-
-void zone::finalizer_array::clear() {
-  call();
-  m_tail = m_array;
-}
-
-void zone::finalizer_array::push(void (*func)(void *data), void *data) {
-  finalizer *fin = m_tail;
-
-  if (fin == m_end) {
-    push_expand(func, data);
-    return;
-  }
-
-  fin->m_func = func;
-  fin->m_data = data;
-
-  ++m_tail;
-}
-
-void zone::finalizer_array::push_expand(void (*func)(void *), void *data) {
-  const size_t nused = m_end - m_array;
-  size_t nnext;
-  if (nused == 0) {
-    nnext = (sizeof(finalizer) < 72 / 2) ? 72 / sizeof(finalizer) : 8;
-  } else {
-    nnext = nused * 2;
-  }
-  finalizer *tmp = static_cast<finalizer *>(::realloc(m_array, sizeof(finalizer) * nnext));
-  if (!tmp) {
-    throw std::bad_alloc();
-  }
-  m_array = tmp;
-  m_end = tmp + nnext;
-  m_tail = tmp + nused;
-  new (m_tail) finalizer(func, data);
-
-  ++m_tail;
-}
-
-zone::finalizer_array::finalizer_array(finalizer_array &&other) noexcept
-  : m_tail(other.m_tail)
-  , m_end(other.m_end)
-  , m_array(other.m_array) {
-  other.m_tail = nullptr;
-  other.m_end = nullptr;
-  other.m_array = nullptr;
-}
-
-zone::finalizer_array &zone::finalizer_array::operator=(finalizer_array &&other) noexcept {
-  this->~finalizer_array();
-  new (this) finalizer_array(std::move(other));
-  return *this;
-}
-
 zone::chunk_list::chunk_list(size_t chunk_size) {
   chunk *c = static_cast<chunk *>(::malloc(sizeof(chunk) + chunk_size));
   if (!c) {
@@ -101,41 +36,11 @@ zone::chunk_list::~chunk_list() {
   }
 }
 
-void zone::chunk_list::clear(size_t chunk_size) {
-  chunk *c = m_head;
-  while (true) {
-    chunk *n = c->m_next;
-    if (n) {
-      ::free(c);
-      c = n;
-    } else {
-      m_head = c;
-      break;
-    }
-  }
-  m_head->m_next = nullptr;
-  m_free = chunk_size;
-  m_ptr = reinterpret_cast<char *>(m_head) + sizeof(chunk);
-}
-
-zone::chunk_list::chunk_list(chunk_list &&other) noexcept
-  : m_free(other.m_free)
-  , m_ptr(other.m_ptr)
-  , m_head(other.m_head) {
-  other.m_head = nullptr;
-}
-
-zone::chunk_list &zone::chunk_list::operator=(chunk_list &&other) noexcept {
-  this->~chunk_list();
-  new (this) chunk_list(std::move(other));
-  return *this;
-}
-
 zone::zone(size_t chunk_size) noexcept
   : m_chunk_size(chunk_size)
   , m_chunk_list(m_chunk_size) {}
 
-inline char *zone::get_aligned(char *ptr, size_t align) {
+char *zone::get_aligned(char *ptr, size_t align) {
   return reinterpret_cast<char *>(reinterpret_cast<size_t>((ptr + (align - 1))) / align * align);
 }
 
@@ -153,18 +58,7 @@ void *zone::allocate_align(size_t size, size_t align) {
   return aligned;
 }
 
-inline void *zone::allocate_no_align(size_t size) {
-  char *ptr = m_chunk_list.m_ptr;
-  if (m_chunk_list.m_free < size) {
-    ptr = allocate_expand(size);
-  }
-  m_chunk_list.m_free -= size;
-  m_chunk_list.m_ptr += size;
-
-  return ptr;
-}
-
-inline char *zone::allocate_expand(size_t size) {
+char *zone::allocate_expand(size_t size) {
   chunk_list *const cl = &m_chunk_list;
 
   size_t sz = m_chunk_size;
@@ -192,55 +86,16 @@ inline char *zone::allocate_expand(size_t size) {
   return ptr;
 }
 
-inline void zone::push_finalizer(void (*func)(void *), void *data) {
-  m_finalizer_array.push(func, data);
-}
-
-template<typename T>
-inline void zone::push_finalizer(std::unique_ptr<T> obj) {
-  m_finalizer_array.push(&zone::object_delete<T>, obj.release());
-}
-
-inline void zone::clear() {
-  m_finalizer_array.clear();
-  m_chunk_list.clear(m_chunk_size);
-}
-
-inline void zone::swap(zone &o) {
-  std::swap(*this, o);
-}
-
-template<typename T>
-void zone::object_delete(void *obj) {
-  delete static_cast<T *>(obj);
-}
-
-template<typename T>
-void zone::object_destruct(void *obj) {
-  static_cast<T *>(obj)->~T();
-}
-
-inline void zone::undo_allocate(size_t size) {
-  m_chunk_list.m_ptr -= size;
-  m_chunk_list.m_free += size;
-}
-
-template<typename T, typename... Args>
-T *zone::allocate(Args... args) {
-  void *x = allocate_align(sizeof(T), alignof(T));
-  try {
-    m_finalizer_array.push(&zone::object_destruct<T>, x);
-  } catch (...) {
-    undo_allocate(sizeof(T));
-    throw;
+void *zone::operator new(std::size_t size) {
+  void *p = ::malloc(size);
+  if (!p) {
+    throw std::bad_alloc();
   }
-  try {
-    return new (x) T(args...);
-  } catch (...) {
-    --m_finalizer_array.m_tail;
-    undo_allocate(sizeof(T));
-    throw;
-  }
+  return p;
+}
+
+void zone::operator delete(void *p) noexcept {
+  ::free(p);
 }
 
 } // namespace msgpack
--- runtime/msgpack/zone.h
+++ runtime/msgpack/zone.h
@@ -10,51 +10,19 @@
 #pragma once
 
 #include <cstdlib>
-#include <memory>
+
+#include "common/mixin/not_copyable.h"
 
 namespace msgpack {
 
-class zone {
+class zone : private vk::not_copyable {
 private:
-  struct finalizer {
-    finalizer(void (*func)(void *), void *data)
-      : m_func(func)
-      , m_data(data) {}
-    void operator()() {
-      m_func(m_data);
-    }
-    void (*m_func)(void *){nullptr};
-    void *m_data{nullptr};
-  };
-  struct finalizer_array {
-    void call();
-    finalizer_array() = default;
-    ~finalizer_array();
-    finalizer_array(finalizer_array &&other) noexcept;
-    finalizer_array &operator=(finalizer_array &&other) noexcept;
-    finalizer_array(const finalizer_array &) = delete;
-    finalizer_array &operator=(const finalizer_array &) = delete;
-
-    void clear();
-    void push(void (*func)(void *data), void *data);
-    void push_expand(void (*func)(void *), void *data);
-
-    finalizer *m_tail{nullptr};
-    finalizer *m_end{nullptr};
-    finalizer *m_array{nullptr};
-  };
   struct chunk {
     chunk *m_next{nullptr};
   };
-  struct chunk_list {
-    chunk_list(size_t chunk_size);
+  struct chunk_list : private vk::not_copyable {
+    explicit chunk_list(size_t chunk_size);
     ~chunk_list();
-    chunk_list(chunk_list &&other) noexcept;
-    chunk_list &operator=(chunk_list &&other) noexcept;
-    chunk_list(const chunk_list &) = delete;
-    chunk_list &operator=(const chunk_list &) = delete;
-
-    void clear(size_t chunk_size);
 
     size_t m_free{0};
     char *m_ptr{nullptr};
@@ -63,56 +31,19 @@ private:
 
   size_t m_chunk_size{0};
   chunk_list m_chunk_list;
-  finalizer_array m_finalizer_array;
 
 public:
-  zone(size_t chunk_size = 8192) noexcept;
+  explicit zone(size_t chunk_size = 8192) noexcept;
 
-  void *allocate_align(size_t size, size_t align = sizeof(void *));
-  void *allocate_no_align(size_t size);
+  void *allocate_align(size_t size, size_t align);
 
-  void push_finalizer(void (*func)(void *), void *data);
-
-  template<typename T>
-  void push_finalizer(std::unique_ptr<T> obj);
-
-  void clear();
-
-  void swap(zone &o);
-
-  static void *operator new(std::size_t size) {
-    void *p = ::malloc(size);
-    if (!p)
-      throw std::bad_alloc();
-    return p;
-  }
-  static void operator delete(void *p) noexcept {
-    ::free(p);
-  }
-  static void *operator new(std::size_t /*size*/, void *mem) noexcept {
-    return mem;
-  }
+  static void *operator new(std::size_t size);
+  static void operator delete(void *p) noexcept;
+  static void *operator new(std::size_t /*size*/, void *mem) noexcept { return mem; }
   static void operator delete(void * /*p*/, void * /*mem*/) noexcept {}
 
-  template<typename T, typename... Args>
-  T *allocate(Args... args);
-
-  zone(zone &&) = default;
-  zone &operator=(zone &&) = default;
-  zone(const zone &) = delete;
-  zone &operator=(const zone &) = delete;
-
 private:
-  void undo_allocate(size_t size);
-
-  template<typename T>
-  static void object_destruct(void *obj);
-
-  template<typename T>
-  static void object_delete(void *obj);
-
   static char *get_aligned(char *ptr, size_t align);
-
   char *allocate_expand(size_t size);
 };
 
