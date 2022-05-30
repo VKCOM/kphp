b5074b3cc5ae17646446db46a49e3840b2ff2a5f
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-05-30 11:56:31 +0300

    move pack*.hpp in root dir

--- 3rdparty/msgpack/msgpack/pack_decl.hpp
+++ 3rdparty/msgpack/msgpack/pack_decl.hpp
@@ -1,17 +1,61 @@
 //
 // MessagePack for C++ serializing routine
 //
-// Copyright (C) 2016 KONDO Takatoshi
+// Copyright (C) 2008-2016 FURUHASHI Sadayuki and KONDO Takatoshi
 //
 //    Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //    http://www.boost.org/LICENSE_1_0.txt)
 //
-#ifndef MSGPACK_PACK_DECL_HPP
-#define MSGPACK_PACK_DECL_HPP
+#ifndef MSGPACK_V1_PACK_DECL_HPP
+#define MSGPACK_V1_PACK_DECL_HPP
 
-#include "msgpack/v1/pack_decl.hpp"
-#include "msgpack/v2/pack_decl.hpp"
-#include "msgpack/v3/pack_decl.hpp"
+#include "msgpack/versioning.hpp"
+#include "msgpack/cpp_config.hpp"
+#include "msgpack/sysdep.h"
 
-#endif // MSGPACK_PACK_DECL_HPP
+namespace msgpack {
+
+/// @cond
+MSGPACK_API_VERSION_NAMESPACE(v3) {
+/// @endcond
+
+/// The class template that supports continuous packing.
+/**
+ * @tparam Stream  Any type that have a member function `Stream write(const char*, size_t s)`
+ *
+ */
+template <typename Stream>
+class packer;
+
+/// Pack the value as MessagePack format into the stream
+/**
+ * This function template is left for compatibility.
+ * Use `void pack(Stream& s, const T& v)` instead of the function template.
+ *
+ * @tparam Stream Any type that have a member function `Stream write(const char*, size_t s)`
+ * @tparam T Any type that is adapted to MessagePack
+ * @param s The pointer to packing destination stream
+ * @param v Packing value
+ */
+template <typename Stream, typename T>
+void pack(Stream* s, const T& v);
+
+/// Pack the value as MessagePack format into the stream
+/**
+ * @tparam Stream Any type that have a member function `Stream write(const char*, size_t s)`
+ * @tparam T Any type that is adapted to MessagePack
+ * @param s Packing destination stream
+ * @param v Packing value
+ */
+template <typename Stream, typename T>
+void pack(Stream& s, const T& v);
+
+
+/// @cond
+}  // MSGPACK_API_VERSION_NAMESPACE(v1)
+/// @endcond
+
+}  // namespace msgpack
+
+#endif // MSGPACK_V1_PACK_DECL_HPP
