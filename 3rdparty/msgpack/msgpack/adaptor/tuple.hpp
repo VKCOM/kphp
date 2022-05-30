7f56da5a4877dce313d408cc81504fda22f0e854
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-05-30 10:11:27 +0300

    move *tuple.hpp out of cpp11 dir

--- 3rdparty/msgpack/msgpack/adaptor/tuple.hpp
+++ 3rdparty/msgpack/msgpack/adaptor/tuple.hpp
@@ -11,6 +11,6 @@
 #ifndef MSGPACK_TYPE_CPP11_TUPLE_HPP
 #define MSGPACK_TYPE_CPP11_TUPLE_HPP
 
-#include "msgpack/v1/adaptor/cpp11/tuple.hpp"
+#include "msgpack/v1/adaptor/tuple.hpp"
 
 #endif // MSGPACK_TYPE_CPP11_TUPLE_HPP
--- 3rdparty/msgpack/msgpack/type.hpp
+++ 3rdparty/msgpack/msgpack/type.hpp
@@ -2,5 +2,4 @@
 #include "adaptor/bool.hpp"
 #include "adaptor/float.hpp"
 #include "adaptor/int.hpp"
-
-#include "adaptor/cpp11/tuple.hpp"
+#include "adaptor/tuple.hpp"
