ce571b64678806032cffd21d47f8b6e4b84a049f
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-06-01 17:17:08 +0300

    drop support of non fixed width integer types and some other unused too

--- runtime/msgpack/adaptor/int.h
+++ runtime/msgpack/adaptor/int.h
@@ -66,27 +66,6 @@ inline T convert_integer(const msgpack::object &o) {
 namespace adaptor {
 
 template<>
-struct convert<char> {
-  void operator()(const msgpack::object &o, char &v) const {
-    v = detail::convert_integer<char>(o);
-  }
-};
-
-template<>
-struct convert<signed char> {
-  void operator()(const msgpack::object &o, signed char &v) const {
-    v = detail::convert_integer<signed char>(o);
-  }
-};
-
-template<>
-struct convert<signed short> {
-  void operator()(const msgpack::object &o, signed short &v) const {
-    v = detail::convert_integer<signed short>(o);
-  }
-};
-
-template<>
 struct convert<int32_t> {
   void operator()(const msgpack::object &o, int32_t &v) const {
     v = detail::convert_integer<int32_t>(o);
@@ -101,23 +80,9 @@ struct convert<int64_t> {
 };
 
 template<>
-struct convert<signed long long> {
-  void operator()(const msgpack::object &o, signed long long &v) const {
-    v = detail::convert_integer<signed long long>(o);
-  }
-};
-
-template<>
 struct convert<uint8_t> {
   void operator()(const msgpack::object &o, uint8_t &v) const {
     v = detail::convert_integer<uint8_t>(o);
-  }
-};
-
-template<>
-struct convert<unsigned short> {
-  void operator()(const msgpack::object &o, unsigned short &v) const {
-    v = detail::convert_integer<unsigned short>(o);
   }
 };
 
@@ -132,37 +97,6 @@ template<>
 struct convert<uint64_t> {
   void operator()(const msgpack::object &o, uint64_t &v) const {
     v = detail::convert_integer<uint64_t>(o);
-  }
-};
-
-template<>
-struct convert<unsigned long long> {
-  void operator()(const msgpack::object &o, unsigned long long &v) const {
-    v = detail::convert_integer<unsigned long long>(o);
-  }
-};
-
-template<>
-struct pack<char> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, char v) const {
-    o.pack_char(v);
-  }
-};
-
-template<>
-struct pack<signed char> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, signed char v) const {
-    o.pack_signed_char(v);
-  }
-};
-
-template<>
-struct pack<signed short> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, signed short v) const {
-    o.pack_short(v);
   }
 };
 
@@ -183,26 +117,10 @@ struct pack<int64_t> {
 };
 
 template<>
-struct pack<signed long long> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, signed long long v) const {
-    o.pack_long_long(v);
-  }
-};
-
-template<>
 struct pack<uint8_t> {
   template<typename Stream>
   void operator()(msgpack::packer<Stream> &o, uint8_t v) const {
     o.pack_uint8(v);
-  }
-};
-
-template<>
-struct pack<unsigned short> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, unsigned short v) const {
-    o.pack_unsigned_short(v);
   }
 };
 
@@ -222,13 +140,6 @@ struct pack<uint64_t> {
   }
 };
 
-template<>
-struct pack<unsigned long long> {
-  template<typename Stream>
-  void operator()(msgpack::packer<Stream> &o, unsigned long long v) const {
-    o.pack_unsigned_long_long(v);
-  }
-};
 
 } // namespace adaptor
 
