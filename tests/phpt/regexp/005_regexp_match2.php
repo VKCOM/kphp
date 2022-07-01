8c1c323301d0a12b72e109bc61f351c610eb8d1d
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-07-01 19:17:24 +0300

    [runtime] permit `$offset` argument of `preg_match_all` (#548)
    Also add missing `$offset` parameter for mixed-typed pattern
    parameter of `preg_match` function.
    
    Fixes #546

--- tests/phpt/regexp/005_regexp_match2.php
+++ tests/phpt/regexp/005_regexp_match2.php
@@ -3,12 +3,20 @@
 
 // Tests for the preg_match and preg_match_all function overloading with 2 args (without $matches).
 
-function test_preg_match($re, $s) {
-  var_dump(["preg_match('$re', $s)" => preg_match($re, $s)]);
+function test_preg_match(string $re, $s, $offset) {
+  var_dump(["preg_match('$re', $s, _, 0, $offset)" => preg_match($re, $s, $m, 0, $offset)]);
+
+  /** @var mixed $mixed_re */
+  $mixed_re = $re;
+  var_dump(["preg_match((mixed)'$re', $s, _, 0, $offset)" => preg_match($mixed_re, $s, $m, 0, $offset)]);
 }
 
-function test_preg_match_all($re, $s) {
-  var_dump(["preg_match_all('$re', $s)" => preg_match_all($re, $s)]);
+function test_preg_match_all(string $re, $s, $offset) {
+  var_dump(["preg_match_all('$re', $s, _, 0, $offset)" => preg_match_all($re, $s, $m, 0, $offset)]);
+
+  /** @var mixed $mixed_re */
+  $mixed_re = $re;
+  var_dump(["preg_match_all((mixed)'$re', $s, _, 0, $offset)" => preg_match_all($mixed_re, $s, $m, 0, $offset)]);
 }
 
 $patterns = [
@@ -34,15 +42,21 @@ $inputs = [
   '123',
   '1 2',
   '12 34',
+  '   123921 821488123 4123',
+  '   123921 foo 4123',
   'foo',
   'foofoo',
   'foo foo',
   '<foo>foofoo',
 ];
 
-foreach ($patterns as $pattern) {
-  foreach ($inputs as $s) {
-    test_preg_match($pattern, $s);
-    test_preg_match_all($pattern, $s);
+$offsets = [0, 1, -1, 3, 10, -10, 100, -100];
+
+foreach ($offsets as $offset) {
+  foreach ($patterns as $pattern) {
+    foreach ($inputs as $s) {
+      test_preg_match($pattern, $s, $offset);
+      test_preg_match_all($pattern, $s, $offset);
+    }
   }
 }
