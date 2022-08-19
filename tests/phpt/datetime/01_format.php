1549ef28c09a64fc13cd46f6382c09842610d4e7
Stanislav Eismont <stanislav.eismont@gmail.com>
2022-08-19 13:15:24 +0300

    add DateTimeInterface with predefined constants

--- tests/phpt/datetime/01_format.php
+++ tests/phpt/datetime/01_format.php
@@ -1,37 +1,63 @@
 @ok
 <?php
 
-$date = new DateTime("2005-07-14 22:30:41");
+function test_format_basic() {
+  $date = new DateTime("2005-07-14 22:30:41");
 
-// basic
-var_dump( $date->format( "F j, Y, g:i a") );
-var_dump( $date->format( "m.d.y") );
-var_dump( $date->format( "j, n, Y") );
-var_dump( $date->format( "Ymd") );
-var_dump( $date->format( 'h-i-s, j-m-y, it is w Day') );
-var_dump( $date->format( '\i\t \i\s \t\h\e jS \d\a\y.') );
-var_dump( $date->format( "D M j G:i:s T Y") );
-var_dump( $date->format( 'H:m:s \m \i\s\ \m\o\n\t\h') );
-var_dump( $date->format( "H:i:s") );
-var_dump( $date->format( "c") );
-var_dump( $date->format( "r") );
-var_dump( $date->format( "U") );
+  var_dump( $date->format( "F j, Y, g:i a") );
+  var_dump( $date->format( "m.d.y") );
+  var_dump( $date->format( "j, n, Y") );
+  var_dump( $date->format( "Ymd") );
+  var_dump( $date->format( 'h-i-s, j-m-y, it is w Day') );
+  var_dump( $date->format( '\i\t \i\s \t\h\e jS \d\a\y.') );
+  var_dump( $date->format( "D M j G:i:s T Y") );
+  var_dump( $date->format( 'H:m:s \m \i\s\ \m\o\n\t\h') );
+  var_dump( $date->format( "H:i:s") );
+  var_dump( $date->format( "c") );
+  var_dump( $date->format( "r") );
+  var_dump( $date->format( "U") );
+}
 
-// negative year
-$date2 = new DateTime("-2005-07-14 22:30:41");
-var_dump( $date2->format( "Y") );
-var_dump( $date2->format( "c") );
+function test_format_basic_predefined_constants() {
+  $date = new DateTime("2005-07-14 22:30:41");
 
-// leap year
-var_dump ((new DateTime("92-07-14"))->format( "L"));
-var_dump ((new DateTime("96-07-14"))->format( "L"));
-var_dump ((new DateTime("100-07-14"))->format( "L"));
-var_dump ((new DateTime("104-07-14"))->format( "L"));
-var_dump ((new DateTime("196-07-14"))->format( "L"));
-var_dump ((new DateTime("200-07-14"))->format( "L"));
-var_dump ((new DateTime("204-07-14"))->format( "L"));
-var_dump ((new DateTime("300-07-14"))->format( "L"));
-var_dump ((new DateTime("300-07-14"))->format( "L"));
-var_dump ((new DateTime("396-07-14"))->format( "L"));
-var_dump ((new DateTime("400-07-14"))->format( "L"));
-var_dump ((new DateTime("404-07-14"))->format( "L"));
+  var_dump( $date->format( DateTime::ATOM) ) ;
+  var_dump( $date->format( DateTime::COOKIE) ) ;
+  var_dump( $date->format( DateTime::ISO8601) ) ;
+  var_dump( $date->format( DateTime::RFC822) ) ;
+  var_dump( $date->format( DateTime::RFC850) ) ;
+  var_dump( $date->format( DateTime::RFC1036) ) ;
+  var_dump( $date->format( DateTime::RFC1123) ) ;
+  var_dump( $date->format( DateTime::RFC7231) ) ;
+  var_dump( $date->format( DateTime::RFC2822) ) ;
+  var_dump( $date->format( DateTime::RFC3339) ) ;
+  var_dump( $date->format( DateTime::RFC3339_EXTENDED) ) ;
+  var_dump( $date->format( DateTime::RSS) ) ;
+  var_dump( $date->format( DateTime::W3C) ) ;
+}
+
+function test_format_negative_year() {
+  $date2 = new DateTime("-2005-07-14 22:30:41");
+  var_dump( $date2->format( "Y") );
+  var_dump( $date2->format( "c") );
+}
+
+function test_format_leap_year() {
+  var_dump ((new DateTime("92-07-14"))->format( "L"));
+  var_dump ((new DateTime("96-07-14"))->format( "L"));
+  var_dump ((new DateTime("100-07-14"))->format( "L"));
+  var_dump ((new DateTime("104-07-14"))->format( "L"));
+  var_dump ((new DateTime("196-07-14"))->format( "L"));
+  var_dump ((new DateTime("200-07-14"))->format( "L"));
+  var_dump ((new DateTime("204-07-14"))->format( "L"));
+  var_dump ((new DateTime("300-07-14"))->format( "L"));
+  var_dump ((new DateTime("300-07-14"))->format( "L"));
+  var_dump ((new DateTime("396-07-14"))->format( "L"));
+  var_dump ((new DateTime("400-07-14"))->format( "L"));
+  var_dump ((new DateTime("404-07-14"))->format( "L"));
+}
+
+test_format_basic();
+test_format_basic_predefined_constants();
+test_format_negative_year();
+test_format_leap_year();
