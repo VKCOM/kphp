cec11ad3cc2cbbf58f835ae99abb7ac6d810adaa
Vadim Sadokhov <v.sadokhov@vk.team>
2022-09-01 16:17:11 +0300

    change test name

--- tests/python/tests/http_server/test_ignore_user_abort.py
+++ tests/python/tests/http_server/test_ignore_user_abort.py
@@ -12,8 +12,11 @@ class TestIgnoreUserAbort(KphpServerAutoTestCase):
         except Exception:
             pass
 
-    def test_user_abort_rpc_work(self):
-        self._send_request(uri='/test_ignore_user_abort?type=resumable&level=no_ignore')
+    """
+    Имя изменено для избежания ошибки связанной с asan
+    """
+    def test_user_abort_of_rpc_work(self):
+        self._send_request(uri='/test_ignore_user_abort?type=rpc&level=no_ignore')
         self.kphp_server.assert_log(['Critical error during script execution: http connection close'], timeout=5)
         error = False
         try:
@@ -23,7 +26,7 @@ class TestIgnoreUserAbort(KphpServerAutoTestCase):
         self.assertTrue(error)
 
     def test_user_abort_resumable_work(self):
-        self._send_request(uri='/test_ignore_user_abort?type=rpc&level=no_ignore')
+        self._send_request(uri='/test_ignore_user_abort?type=resumable&level=no_ignore')
         self.kphp_server.assert_log(['Critical error during script execution: http connection close'], timeout=5)
         error = False
         try:
