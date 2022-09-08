2b3cfdafb8417b6edabea540a94d575d7555d90f
Denis Vaksman <dr.denisv2016@yandex.ru>
2022-09-08 17:07:12 +0300

    improve cluster name handling
    - allow cluster name to contain digits and '-'
    - check cluster name for incorrect symbols on start
      instead of force quiet replacing them with '_'
    - handle the case when cluster name is longer than 108 bytes
      to be able to create UNIX_SOCKET for graceful restart

--- server/cluster-name.cpp
+++ server/cluster-name.cpp
@@ -2,11 +2,16 @@
 // Copyright (c) 2020 LLC «V Kontakte»
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
-#include <algorithm>
-#include <cstring>
-#include <cctype>
-
 #include "server/cluster-name.h"
+
+#include <algorithm>
+
+#include <cctype>
+#include <cinttypes>
+#include <cstdio>
+#include <cstring>
+
+#include "common/crc32.h"
 
 ClusterName::ClusterName() {
   set_cluster_name("default");
@@ -14,7 +19,7 @@ ClusterName::ClusterName() {
 
 const char *ClusterName::set_cluster_name(const char *name) noexcept {
   // reserved for shmem, statsd, socket suffixes and prefixes
-  constexpr size_t reserved = 32;
+  constexpr size_t reserved = 42;
   const auto name_len = std::strlen(name);
   if (!name_len) {
     return "empty cluster name";
@@ -23,16 +28,31 @@ const char *ClusterName::set_cluster_name(const char *name) noexcept {
     return "too long cluster name";
   }
   std::copy(name, name + name_len + 1, cluster_name_.begin());
-  std::replace_copy_if(name, name + name_len + 1, socket_name_.begin(),
-                       [](char c) { return c && !std::isalpha(c); }, '_');
+  bool has_wrong_symbols = std::any_of(cluster_name_.begin(), cluster_name_.begin() + name_len, [](char c) {
+    return !std::isalnum(c) && c != '-' && c != '_';
+  });
+  if (has_wrong_symbols) {
+    return "Incorrect symbol in cluster name. Allowed symbols are: alpha-numerics, '-', '_'";
+  }
+
+  const char *socket_prefix = "kphp_master_fd_for_graceful_restart-";
+  auto socket_prefix_len = std::strlen(socket_prefix);
+  std::strcpy(socket_name_.data(), socket_prefix);
+  if (socket_prefix_len + name_len > MAX_SOCKET_NAME_LEN) {
+    // To allow cluster name longer than 107 symbols, we just take crc64 of it as the socket name
+    uint64_t crc = compute_crc64(name, name_len);
+    char hex_crc_str[sizeof(crc) * 2 + 1];
+    std::sprintf(hex_crc_str, "%16" PRIx64, crc);
+    std::strcat(socket_name_.data(), hex_crc_str);
+  } else {
+    std::strcat(socket_name_.data(), cluster_name_.data());
+  }
 
   std::strcpy(shmem_name_.data(), "/");
-  std::strcat(shmem_name_.data(), socket_name_.data());
+  std::strcat(shmem_name_.data(), cluster_name_.data());
   std::strcat(shmem_name_.data(), "_kphp_shm");
 
   std::strcpy(statsd_prefix_.data(), "kphp_stats.");
-  std::strcat(statsd_prefix_.data(), socket_name_.data());
-
-  std::strcat(socket_name_.data(), "_kphp_fd_transfer");
+  std::strcat(statsd_prefix_.data(), cluster_name_.data());
   return nullptr;
 }
