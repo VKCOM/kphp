e177860460b840c8c4c3c07cfa2d2ed50b67dd27
Vadim Sadokhov <v.sadokhov@vk.team>
2022-11-07 16:20:54 +0300

    refactor tests

--- tests/phpt/tcp/basic_connections.php
+++ tests/phpt/tcp/basic_connections.php
@@ -1,20 +1,7 @@
 @ok
 <?php
 
-function test_connection($address, $request) {
-    $fp = stream_socket_client($address, $errno, $errstr);
-    if (!$fp) {
-        echo "Failed to create connection<br />\n";
-    } else {
-        fwrite($fp, $request);
-        $response = fread($fp, 2048);
-        $statusPos = strpos($response, "\r\n");
-        echo substr($response, 0, $statusPos) . "\n";
-        fclose($fp);
-    }
-}
-
-function test_getter($address, $request) {
+function test_simple_connection($address, $request) {
     $fp = stream_socket_client($address, $errno, $errstr);
     if (!$fp) {
         echo "Failed to create connection<br />\n";
@@ -28,25 +15,35 @@ function test_getter($address, $request) {
     }
 }
 
-function test_eof($address, $request) {
-    $fp = stream_socket_client($address, $errno, $errstr, 30);
+function test_full_response($address, $request) {
+    $fp = stream_socket_client($address, $errno, $errstr);
     if (!$fp) {
-        echo "$errstr ($errno)<br />\n";
+        echo "Failed to create connection<br />\n";
     } else {
         fwrite($fp, $request);
-        echo fgets($fp, 1024);
+        $responseStatus = fgets($fp, 2048);
+        echo $responseStatus;
         while (!feof($fp)) {
-            fgets($fp, 1024);
+            fgets($fp, 512);
+            fread($fp, 512);
         }
+        echo feof($fp);
         fclose($fp);
     }
 }
 
-test_connection("tcp://bad_address", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
-test_connection("tcp://wrong_address:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
-test_connection("tcp://example.com:wrong_port", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
-test_connection("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
-test_connection("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: not_working.com:1111\r\nAccept: */*\r\n\r\n");
+function test_no_close($address, $request) {
+    $fp = stream_socket_client($address, $errno, $errstr);
+}
 
-test_getter("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
-test_eof("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: not_working.com:80\r\nAccept: */*\r\n\r\n");
+
+test_simple_connection("tcp://bad_address", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
+test_simple_connection("tcp://wrong_address:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
+test_simple_connection("tcp://example.com:wrong_port", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
+test_simple_connection("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
+test_simple_connection("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: not_working.com:1111\r\nAccept: */*\r\n\r\n");
+
+test_full_response("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
+test_full_response("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: not_working.com:80\r\nAccept: */*\r\n\r\n");
+
+test_no_close("tcp://example.com:80", "GET / HTTP/1.0\r\nHost: example.com:80\r\nAccept: */*\r\n\r\n");
