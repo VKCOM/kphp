0a05de233d37c28f9a1479e687225d896ed0fb90
Aleksandr Kirsanov <unserialize.alias@gmail.com>
2022-12-18 10:12:28 +0300

    fix a race leading to 'invalid ()-invocation of a non-lambda'

--- compiler/pipes/convert-invoke-to-func-call.h
+++ compiler/pipes/convert-invoke-to-func-call.h
@@ -8,6 +8,8 @@
 #include "compiler/function-pass.h"
 
 class ConvertInvokeToFuncCallPass final : public FunctionPassBase {
+  std::forward_list<FunctionPtr> nested_lambdas;
+
 public:
   std::string get_description() override {
     return "Convert invoke to func call";
@@ -20,6 +22,20 @@ public:
   VertexPtr on_exit_vertex(VertexPtr root) final;
 
   VertexPtr on_clone(VertexAdaptor<op_clone> v_clone);
+  VertexPtr on_func_call(VertexAdaptor<op_func_call> v_call);
   VertexPtr on_invoke_call(VertexAdaptor<op_invoke_call> v_invoke_call);
   VertexPtr on_callback_of_builtin(VertexAdaptor<op_callback_of_builtin> v_callback);
+
+  std::forward_list<FunctionPtr> &&flush_nested_lambdas() {
+    return std::move(nested_lambdas);
+  }
+};
+
+// this class is used in the compiler pipeline
+// it's needed to hold lambdas so they don't pass the pipeline before a containing function, see execute()
+class ConvertInvokeToFuncCallF {
+public:
+  void execute(FunctionPtr f, DataStream<FunctionPtr> &os);
+
+  static void execute_with_nested_lambdas(FunctionPtr f, DataStream<FunctionPtr> &os);
 };
