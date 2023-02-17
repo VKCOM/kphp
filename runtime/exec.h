1828276966d3049d211b13c0ddd4668775f1fcad
Stanislav Eismont <stanislav.eismont@gmail.com>
2023-02-17 15:00:56 +0300

    add escapeshellarg() and escapeshellcmd() functions (#738)

--- runtime/exec.h
+++ runtime/exec.h
@@ -12,3 +12,5 @@ Optional<string> f$exec(const string &command);
 Optional<string> f$exec(const string &command, mixed &output, int64_t &result_code = get_dummy_result_code());
 int64_t f$system(const string &command, int64_t &result_code = get_dummy_result_code());
 Optional<bool> f$passthru(const string &command, int64_t &result_code = get_dummy_result_code());
+string f$escapeshellarg(const string &arg) noexcept;
+string f$escapeshellcmd(const string &cmd) noexcept;
