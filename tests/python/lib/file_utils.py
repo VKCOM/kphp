42c146774bd3d478c0f2fa4aa5e9d4d8fee02e9e
mkornaukhov03 <mkornaukhov03@yandex.ru>
2023-03-09 20:52:48 +0300

    Remove php8.2 in list

--- tests/python/lib/file_utils.py
+++ tests/python/lib/file_utils.py
@@ -118,7 +118,7 @@ def can_ignore_sanitizer_log(sanitizer_log_file):
 def search_php_bin(required_php_ver=PhpVersion.fallback):
     if sys.platform == "darwin":
         return shutil.which("php")
-    ordered_version = [PhpVersion.v8_2, PhpVersion.v8_1, PhpVersion.v8, PhpVersion.v7_4];
+    ordered_version = [PhpVersion.v8_1, PhpVersion.v8, PhpVersion.v7_4];
     for php_ver in ordered_version:
         if (php_ver == required_php_ver):
             return shutil.which(php_ver.value)
