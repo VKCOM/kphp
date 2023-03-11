02aacb85787e4817f463b342be871abfd73d2f26
Aleksandr Kirsanov <unserialize.alias@gmail.com>
2023-03-11 12:03:17 +0300

    onRpcRequestSend accepts start_timestamp, not to call microtime in PHP

--- runtime/kphp_tracing.h
+++ runtime/kphp_tracing.h
@@ -15,7 +15,7 @@ namespace kphp_tracing {
 using on_net_to_script_switch_callback_t = std::function<void(double)>;
 using on_fork_state_change_callback_t = std::function<void(int64_t)>;
 using on_fork_switch_callback_t = std::function<void(int64_t, int64_t)>;
-using on_rpc_request_start_callback_t = std::function<void(int64_t, int64_t, int64_t, int64_t)>;
+using on_rpc_request_start_callback_t = std::function<void(int64_t, int64_t, int64_t, int64_t, double)>;
 using on_rpc_request_finish_callback_t = std::function<void(int64_t, int64_t, double, int64_t)>;
 using on_job_request_start_callback_t = std::function<void(int64_t, const class_instance<C$KphpJobWorkerRequest>&)>;
 using on_job_request_finish_callback_t = std::function<void(int64_t, double, int64_t)>;
--- runtime/rpc.cpp
+++ runtime/rpc.cpp
@@ -725,7 +725,7 @@ int64_t rpc_send(const class_instance<C$RpcConnection> &conn, double timeout, bo
   cur->timer = nullptr;
 
   if (kphp_tracing::on_rpc_request_start) {
-    kphp_tracing::on_rpc_request_start(!ignore_answer ? cur->resumable_id : -1, cur->actor_id, cur->function_magic, static_cast<int64_t>(request_size));
+    kphp_tracing::on_rpc_request_start(!ignore_answer ? cur->resumable_id : -1, cur->actor_id, cur->function_magic, static_cast<int64_t>(request_size), cur->send_timestamp);
   }
 
   if (ignore_answer) {
