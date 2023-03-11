c45668264db4475cfdd7ca5eb2956fffe051b2fd
Nikolay Osintsev <niosintsev@miem.hse.ru>
2023-03-12 00:27:27 +0300

    yaml: delete obvious comments in runtime/yaml.h

--- runtime/yaml.h
+++ runtime/yaml.h
@@ -2,22 +2,10 @@
 
 #include "runtime/kphp_core.h"
 
-/*
- * emit mixed into a yaml file
- */
 bool f$yaml_emit_file(const string &filename, const mixed &data);
 
-/*
- * emit mixed into a yaml string
- */
 string f$yaml_emit(const mixed &data);
 
-/*
- * parse yaml file into mixed
- */
 mixed f$yaml_parse_file(const string &filename, int pos = 0);
 
-/*
- * parse yaml string into mixed
- */
 mixed f$yaml_parse(const string &data, int pos = 0);
