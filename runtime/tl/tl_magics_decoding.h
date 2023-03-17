10e7647240fb3a7206b2522b29f6318ae5df6f6d
Denis Vaksman <dr.denisv2016@yandex.ru>
2023-03-17 21:19:16 +0700

    implement TL magic to name converting

---
+++ runtime/tl/tl_magics_decoding.h
@@ -0,0 +1,9 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2023 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include <cstdint>
+
+const char *tl_function_magic_to_name(uint32_t magic) noexcept;
