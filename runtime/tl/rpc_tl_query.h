e6903247f347b529067886b25ff5cdced71e7cbf
Aleksandr Kirsanov <unserialize.alias@gmail.com>
2023-03-27 19:23:56 +0300

    inline last_stored_tl_function_magic_

--- runtime/tl/rpc_tl_query.h
+++ runtime/tl/rpc_tl_query.h
@@ -44,10 +44,12 @@ public:
   void reset();
   void set_current_tl_function(const string &tl_function_name);
   void set_current_tl_function(const class_instance<RpcTlQuery> &current_query);
-  void set_last_stored_tl_function_magic(uint32_t last_stored_tl_function_magic); // called from generated TL serializers
-  uint32_t get_last_stored_tl_function_magic() const;
   void raise_fetching_error(const char *format, ...) __attribute__ ((format (printf, 2, 3)));
   void raise_storing_error(const char *format, ...) __attribute__ ((format (printf, 2, 3)));
+
+  // called from generated TL serializers
+  void set_last_stored_tl_function_magic(uint32_t tl_magic) { last_stored_tl_function_magic_ = tl_magic; }
+  uint32_t get_last_stored_tl_function_magic() const { return last_stored_tl_function_magic_; }
 
   const string &get_current_tl_function_name() const {
     return current_tl_function_name_;
