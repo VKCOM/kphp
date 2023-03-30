754bfdf8852d27dc955cc5468675ca1f5c05f1f2
mkornaukhov03 <mkornaukhov03@yandex.ru>
2023-03-30 22:01:07 +0300

    Remove redundant code

--- compiler/code-gen/raw-data.h
+++ compiler/code-gen/raw-data.h
@@ -26,32 +26,6 @@ private:
 
 std::vector<int> compile_arrays_raw_representation(const std::vector<VarPtr> &const_raw_array_vars, CodeGenerator &W);
 
-template <typename Container,
-  typename = decltype(std::declval<Container>().begin()),
-  typename = decltype(std::declval<Container>().end())>
-std::vector<int> compile_raw_data(CodeGenerator &W, const Container &values) {
-  std::string raw_data;
-  std::vector<int> const_string_shifts(values.size());
-  int ii = 0;
-  for (const auto &s : values) {
-    int shift_to_align = (((int)raw_data.size() + 7) & -8) - (int)raw_data.size();
-    if (shift_to_align != 0) {
-      raw_data.append(shift_to_align, 0);
-    }
-    int raw_len = string_raw_len(static_cast<int>(s.size()));
-    kphp_assert (raw_len != -1);
-    const_string_shifts[ii] = (int)raw_data.size();
-    raw_data.append(raw_len, 0);
-    int err = string_raw(&raw_data[const_string_shifts[ii]], raw_len, s.c_str(), (int)s.size());
-    kphp_assert (err == raw_len);
-    ii++;
-  }
-  if (!raw_data.empty()) {
-    W << "alignas(8) static const char raw[] = " << RawString(raw_data) << ";" << NL;
-  }
-  return const_string_shifts;
-}
-
 inline RawString gen_raw_string(const std::string& data) {
   int raw_len = string_raw_len(data.size());
   std::string response(raw_len, '\0');
