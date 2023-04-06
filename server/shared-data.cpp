c8dfb816c7b29296dbbdb8f75f67093fd7897dc2
Vadim Sadokhov <v.sadokhov@vk.team>
2023-04-06 14:21:51 +0300

    add copyright

--- server/shared-data.cpp
+++ server/shared-data.cpp
@@ -1,3 +1,7 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2023 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
 #include "server/shared-data.h"
 
 #include <new>
