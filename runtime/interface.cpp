f8680d2453e4e6684f6768115c7374cc9077fdfa
Stanislav Sakharov <s.sakharov@vk.team>
2023-04-14 02:42:51 +0300

    reset gzip flag

--- runtime/interface.cpp
+++ runtime/interface.cpp
@@ -102,12 +102,19 @@ void f$ob_clean() {
   coub->clean();
 }
 
+static inline void reset_gzip_header() {
+  if (ob_cur_buffer == 0) {
+    http_need_gzip &= ~4;
+  }
+}
+
 bool f$ob_end_clean() {
   if (ob_cur_buffer == 0) {
     return false;
   }
 
   coub = &oub[--ob_cur_buffer];
+  reset_gzip_header();
   return true;
 }
 
@@ -118,7 +125,7 @@ Optional<string> f$ob_get_clean() {
 
   string result = coub->str();
   coub = &oub[--ob_cur_buffer];
-
+  reset_gzip_header();
   return result;
 }
 
