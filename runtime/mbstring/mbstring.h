9332f451516deb9296e45119d4e71809dbc84313
Andrey Arutiunian <andreylzmw@gmail.com>
2023-04-20 02:18:43 +0300

    finish basic mbstring functions and building libmbfl

--- runtime/mbstring/mbstring.h
+++ runtime/mbstring/mbstring.h
@@ -3,13 +3,18 @@
 #include "runtime/kphp_core.h"
 
 /**
+ * Check if strings are valid for the specified encoding
+ * @param value The byte stream
+ * @param encoding The expected encoding
+ * @return Returns true on success or false on failure
+ */
+bool f$mb_check_encoding(const string &value, const string &encoding);
+
+/**
  * Convert a string from one character encoding to another
  * @param str The string to be converted
  * @param from The desired encoding of the result
  * @param to The current encoding used to interpret string
  * @return The encoded string
- * TODO!: mb_check_encoding(str, from) inside
- * TODO!: own constants for encodings
- * TODO: issue for timelib
  */
 string f$mb_convert_encoding(const string &str, const string &to, const string &from);
---
+++ tests/cpp/runtime/mbstring-test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "runtime/mbstring/mbstring.h"
+
+#ifdef MBFL
+/* TODO: make fun strings for tests */
+TEST(mbstring_test, test_mb_check_encoding) {
+	ASSERT_TRUE(f$mb_check_encoding(string("sdf"), string("Windows-1251")));
+	ASSERT_TRUE(f$mb_check_encoding(string("ыва"), string("Windows-1251")));
+	ASSERT_TRUE(f$mb_check_encoding(string("Ä°nanÃ§ EsaslarÄ±"), string("UTF-8")));
+	ASSERT_TRUE(f$mb_check_encoding(string("Ä°nanÃ§ EsaslarÄ±"), string("Windows-1251")));
+	ASSERT_FALSE(f$mb_check_encoding(string("Ä°nanÃ§ EsaslarÄ±"), string("ASCII")));
+}
+TEST(mbstring_test, test_mb_convert_encoding) {
+	ASSERT_STREQ(f$mb_convert_encoding(string("Hello"), string("UTF-8"), string("EUC-KR")).c_str(), "Hello");
+	ASSERT_STREQ(f$mb_convert_encoding(string("ыавыа"), string("UTF-8"), string("Windows-1251")).c_str(), "С‹Р°РІС‹Р°");
+	ASSERT_STREQ(f$mb_convert_encoding(string("ыва"), string("UTF-8"), string("ASCII")).c_str(), "??????");
+}
+#endif
