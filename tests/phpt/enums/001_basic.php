229599a0ceb01a867acc8afdd4d1a531cf2a27db
mkornaukhov03 <mkornaukhov03@yandex.ru>
2023-05-15 12:28:45 +0300

    Add first test

---
+++ tests/phpt/enums/001_basic.php
@@ -0,0 +1,18 @@
+@ok php8
+<?php
+
+enum MyBool {
+    case T;
+    case F;
+}
+
+$v = MyBool::T;
+echo $v->name;
+
+const c = MyBool::F;
+echo c->name;
+
+echo MyBool::F->name;
+echo MyBool::T->name;
+
+// TODO add 002_comparison
