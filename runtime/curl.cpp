9820286bd51d206813446bddc08e5281193bda42
Vadim Sadokhov <v.sadokhov@vk.team>
2023-07-20 14:51:27 +0300

    one more fix

--- runtime/curl.cpp
+++ runtime/curl.cpp
@@ -678,9 +678,8 @@ mixed f$curl_exec(curl_easy easy_id) noexcept {
   easy_context->error_num = dl::critical_section_call(curl_easy_perform, easy_context->easy_handle);
   double request_finish_time = dl_time();
   if (request_finish_time - request_start_time >= long_curl_query) {
-    const string &url = easy_context->get_info(CURLINFO_EFFECTIVE_URL).as_string();
     kprintf("LONG curl query : %f. Curl id = %d, url = %.100s\n", request_finish_time - request_start_time,
-            easy_context->uniq_id, url.c_str());
+            easy_context->uniq_id, easy_context->get_info(CURLINFO_EFFECTIVE_URL).as_string().c_str());
   }
   if (easy_context->error_num != CURLE_OK && easy_context->error_num != CURLE_PARTIAL_FILE) {
     if (kphp_tracing::is_turned_on()) {
