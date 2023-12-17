bdcfaaf618fcf3862f2b2717d445bd8e8ad62bb9
Арутюнян Андрей Романович <andreylzmw@gmail.com>
2023-12-17 03:16:36 +0300

    try different url setters

--- test.php
+++ test.php
@@ -1,12 +1,21 @@
 <?php
 
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, "nonexistent_url");
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $output = curl_exec($ch);
+	
+var_dump($output);
 
-echo $output;
+
+function to_json_safe($str) {
+  return is_string($str) && $str != '' ? json_decode($str) : $str;
+}
+
+$resp = ["exec_result" => to_json_safe($output)];
+var_dump($resp);
+
 
 curl_close($ch);
