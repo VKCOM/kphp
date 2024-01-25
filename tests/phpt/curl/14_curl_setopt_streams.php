b3a49c99f02c994cfcebffc704ca82a9f2520d2a
Marat Omarov <mtomarov@miem.hse.ru>
2024-01-25 19:48:57 +0300

    Added simple tests for stream options

--- tests/phpt/curl/14_curl_setopt_streams.php
+++ tests/phpt/curl/14_curl_setopt_streams.php
@@ -3,152 +3,51 @@
 
 require_once 'kphp_tester_include.php';
 
-// listen different ports for php and kphp 
-$port = 8080;
-if (kphp)
-  $port = 8081;
-
-class Process {
-  private $pid;
-  private $cmd;
-  private $port;
-
-  public function __construct($cmd, $port) {
-    if ($cmd) {
-      $this->cmd = 'nohup ' . $cmd . ' > /dev/null 2>&1 & echo $!';
-      $this->port = $port;
-      exec($this->cmd);
-    } else {
-        throw new Exception("The command for running a server on the port $port is empty!\n");
-    }
-  }
-
-  public function update_pid() {
-    exec("lsof -i:$this->port | awk 'NR==2' | awk '{print $2}'", $op);
-    if (!isset($op[0])) {
-        throw new Exception("Couldn't define the pid of running server on the port $this->port");
-    }
-    return $this->pid = $op[0];
-  }
-
-  public function status() {
-    $cmd = 'ps -p ' . $this->pid;
-    exec($cmd, $op);
-    return (isset($op[1]));
-  }
-
-  public function start() {
-    $code = false;
-    if ($this->cmd)
-      exec($this->cmd, $op, $code);
-    return $code;
-  }
-
-  public function stop() {
-    $this->update_pid();
-    $cmd = 'kill '. $this->pid;
-    exec($cmd);
-    return $this->status() == false;
-  }
-}
-
 function test_file_option() {
-  global $port;
-  $filename_in = "input_test_file.txt";
-  $filename_out = "output_test_file.txt";
-  $fh_in = fopen("$filename_in", "w+");
-  $fh_out = fopen("$filename_out", "w+");
-  var_dump(fwrite($fh_in, "hello world!"));
+  $c = curl_init();
+  $f = fopen("test_file_option", "w+");
 
-  if (kphp)
-    // got a different behavior for fopen() in "w+" mode
-    // without rewind() in kphp the output file will be empty
-    rewind($fh_in);
+  var_dump(curl_setopt($c, CURLOPT_FILE, $f));
+  fclose($f);
 
-  $c = curl_init("http://localhost:$port");
-  $cmd = "php -S localhost:$port ./$filename_in";
+  var_dump(curl_setopt($c, CURLOPT_FILE, null));
 
-  var_dump(curl_setopt($c, CURLOPT_FILE, $fh_out));
-  var_dump(curl_setopt($c, CURLOPT_VERBOSE, 1)); // get all information about connections
-  $server = new Process($cmd, $port);
-  usleep(1000 * 200);
-  var_dump(curl_exec($c)); // true, if the connection to the localhost is successfull
+  // bad options
+  $f = fopen("test_file_option", "r");
+  var_dump(curl_setopt($c, CURLOPT_FILE, $f));
+  fclose($f);
 
-  var_dump(rewind($fh_out));
-  var_dump(filesize("./$filename_out"));
-  var_dump(fread($fh_out, filesize("./$filename_out")));
-  var_dump(fclose($fh_in));
-  var_dump(fclose($fh_out));
   curl_close($c);
-  exec("rm ./$filename_in ./$filename_out");
-  var_dump($server->stop());
-  usleep(1000 * 200); 
+  exec("rm ./test_file_option");
 }
 
 function test_infile_option() {
-  global $port;
-  $filename_in = "test_file.txt";
-  $filename_out = "router.php";
+  $c = curl_init();
+  exec("touch test_infile_option");
+  $f = fopen("test_infile_option", "r");
 
-  $fh_in = fopen("$filename_in", "w+");
-  $fh_out = fopen("$filename_out", "w+");
-  var_dump(fwrite($fh_in, "hello world from input file!"));
-  var_dump(fwrite($fh_out, "<?php\n\$putdata = file_get_contents('php://input');\n"));
-  var_dump(fwrite($fh_out, "var_dump(\$putdata);"));
-  rewind($fh_in);
-
-  if (kphp)
-    rewind($fh_out);
-
-  $c = curl_init("http://localhost:$port");
-  $cmd = "php -S localhost:$port ./$filename_out";
-
-  var_dump(curl_setopt($c, CURLOPT_POST, true));
-  var_dump(curl_setopt($c, CURLOPT_INFILESIZE, filesize("./$filename_in")));
-  var_dump(curl_setopt($c, CURLOPT_INFILE, $fh_in));
-  var_dump(curl_setopt($c, CURLOPT_VERBOSE, 1));
-
-  $server = new Process($cmd, $port);
-  usleep(1000 * 200);
-  var_dump(curl_exec($c));
-
-  var_dump(fclose($fh_in));
-  var_dump(fclose($fh_out));
+  var_dump(curl_setopt($c, CURLOPT_INFILE, $f);
+  fclose($f);
+  var_dump(curl_setopt($c, CURLOPT_INFILE, null);
   curl_close($c);
-  exec("rm ./$filename_in ./$filename_out");
-  var_dump($server->stop());
-  usleep(1000 * 200);
 }
 
 function test_writeheader_option() {
-  global $port, $cmd;
-  $filename_in = "router.php";
-  $filename_out = "test_file.txt";
+  $c = curl_init();
+  $f = fopen("test_writeheader_option", "w+");
 
-  $fh_in = fopen("$filename_in", "w+");
-  $fh_out = fopen("$filename_out", "w+");
-  var_dump(fwrite($fh_in, "<?php\necho 'hello world\n';"));
+  var_dump(curl_setopt($c, CURLOPT_FILE, $f));
+  fclose($f);
 
-  if (kphp)
-    rewind($fh_in);
+  var_dump(curl_setopt($c, CURLOPT_FILE, null));
 
-  $c = curl_init("http://localhost:$port");
-  $cmd = "php -S localhost:$port ./$filename_in";
-  var_dump(curl_setopt($c, CURLOPT_WRITEHEADER, $fh_out));
-  var_dump(curl_setopt($c, CURLOPT_VERBOSE, 1));
+  // bad options
+  $f = fopen("test_writeheader_option", "r");
+  var_dump(curl_setopt($c, CURLOPT_FILE, $f));
+  fclose($f);
 
-  $server = new Process($cmd, $port);
-  usleep(1000 * 200);
-  var_dump(curl_exec($c));
-
-  var_dump(rewind($fh_out));
-  var_dump(fread($fh_out, 17));
-  var_dump(fclose($fh_in));
-  var_dump(fclose($fh_out));
   curl_close($c);
-  exec("rm ./$filename_in ./$filename_out");
-  var_dump($server->stop());
-  usleep(1000 * 200);
+  exec("rm ./test_writeheader_option");
 }
 
 test_file_option();
