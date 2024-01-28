f8a87381d6c03f590bd640747b5dc31c40b41984
Vladislav Senin <troy4eg@gmail.com>
2024-01-28 17:27:28 +0300

    added additional iconv tests

--- tests/phpt/string_functions/010_iconv.php
+++ tests/phpt/string_functions/010_iconv.php
@@ -9,3 +9,12 @@ var_dump($step2);
 
 $empty = "";
 var_dump(iconv('UTF-8', 'windows-1251', $empty));
+
+// from examples: https://www.php.net/manual/en/function.iconv.php
+$text = "This is the Euro symbol '€'.";
+var_dump($text);
+// TODO a strange bug when comparing via kphp_tester.py
+//var_dump(iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text));
+// TODO works on MacOS but does not work on Linux
+//var_dump(iconv("UTF-8", "ISO-8859-1//IGNORE", $text));
+var_dump(iconv("UTF-8", "ISO-8859-1", $text));
