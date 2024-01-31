a2563bf82458ab166756786f31bf2c30df8ded44
Nikolay Osintsev <niosintsev@miem.hse.ru>
2024-02-01 02:54:54 +0300

    stream_functions: add [[maybe_unused]] attribute to all args of stream_functions struct functions

--- runtime/stream_functions.h
+++ runtime/stream_functions.h
@@ -21,71 +21,72 @@ struct stream_functions {
   string name;
   array<bool> supported_functions;
 
-  virtual Stream fopen(const string &stream, const string &mode) const {
+  virtual Stream fopen([[maybe_unused]] const string &stream, [[maybe_unused]] const string &mode) const {
     php_critical_error("Function \"fopen\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<int64_t> fwrite(const Stream &stream, const string &data) const {
+  virtual Optional<int64_t> fwrite([[maybe_unused]] const Stream &stream, [[maybe_unused]] const string &data) const {
     php_critical_error("Function \"fwrite\" is not supported for %s", name.c_str());
   }
 
-  virtual int64_t fseek(const Stream &stream, int64_t offset, int64_t whence) const {
+  virtual int64_t fseek([[maybe_unused]] const Stream &stream, [[maybe_unused]] int64_t offset, [[maybe_unused]] int64_t whence) const {
     php_critical_error("Function \"Stream\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<int64_t> ftell(const Stream &stream) const {
+  virtual Optional<int64_t> ftell([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"ftell\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<string> fread(const Stream &stream, int64_t length) const {
+  virtual Optional<string> fread([[maybe_unused]] const Stream &stream, [[maybe_unused]] int64_t length) const {
     php_critical_error("Function \"fread\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<string> fgetc(const Stream &stream) const {
+  virtual Optional<string> fgetc([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"fgetc\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<string> fgets(const Stream &stream, int64_t length) const {
+  virtual Optional<string> fgets([[maybe_unused]] const Stream &stream, [[maybe_unused]] int64_t length) const {
     php_critical_error("Function \"fgets\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<int64_t> fpassthru(const Stream &stream) const {
+  virtual Optional<int64_t> fpassthru([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"fpassthru\" is not supported for %s", name.c_str());
   }
 
-  virtual bool fflush(const Stream &stream) const {
+  virtual bool fflush([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"fflush\" is not supported for %s", name.c_str());
   }
 
-  virtual bool feof(const Stream &stream) const {
+  virtual bool feof([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"feof\" is not supported for %s", name.c_str());
   }
 
-  virtual bool fclose(const Stream &stream) const {
+  virtual bool fclose([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"fclose\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<string> file_get_contents(const string &url) const {
+  virtual Optional<string> file_get_contents([[maybe_unused]] const string &url) const {
     php_critical_error("Function \"file_get_contents\" is not supported for %s", name.c_str());
   }
 
-  virtual Optional<int64_t> file_put_contents(const string &url, const string &content, int64_t flags) const {
+  virtual Optional<int64_t> file_put_contents([[maybe_unused]] const string &url, [[maybe_unused]] const string &content, [[maybe_unused]] int64_t flags) const {
     php_critical_error("Function \"file_put_contents\" is not supported for %s", name.c_str());
   }
 
-  virtual Stream stream_socket_client(const string &url, int64_t &error_number, string &error_description, double timeout, int64_t flags, const mixed &context) const {
+  virtual Stream stream_socket_client([[maybe_unused]] const string &url, [[maybe_unused]] int64_t &error_number, [[maybe_unused]] string &error_description,
+                                      [[maybe_unused]] double timeout, [[maybe_unused]] int64_t flags, [[maybe_unused]] const mixed &context) const {
     php_critical_error("Function \"stream_socket_client\" is not supported for %s", name.c_str());
   }
 
-  virtual bool context_set_option(mixed &context, const string &option, const mixed &value) const {
+  virtual bool context_set_option([[maybe_unused]] mixed &context, [[maybe_unused]] const string &option, [[maybe_unused]] const mixed &value) const {
     php_critical_error("Function \"context_set_option\" is not supported for %s", name.c_str());
   }
 
-  virtual bool stream_set_option(const Stream &stream, int64_t option, int64_t value) const {
+  virtual bool stream_set_option([[maybe_unused]] const Stream &stream, [[maybe_unused]] int64_t option, [[maybe_unused]] int64_t value) const {
     php_critical_error("Function \"stream_set_option\" is not supported for %s", name.c_str());
   }
 
-  virtual int32_t get_fd(const Stream &stream) const {
+  virtual int32_t get_fd([[maybe_unused]] const Stream &stream) const {
     php_critical_error("Function \"get_fd\" is not supported for %s", name.c_str());
   }
 };
