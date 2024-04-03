2f6a2a40b8b34e34924ddd959c89771eaadc0f39
Арутюнян Андрей Романович <andreylzmw@gmail.com>
2024-04-03 09:35:42 +0300

    update

--- net/net-tests.cmake
+++ net/net-tests.cmake
@@ -5,5 +5,5 @@ prepend(NET_TESTS_SOURCES ${BASE_DIR}/net/
         time-slice-test.cpp)
 
 prepare_cross_platform_libs(NET_TESTS_LIBS zstd)
-set(NET_TESTS_LIBS vk::common_src vk::net_src vk::binlog_src vk::unicode ${NET_TESTS_LIBS} ${EPOLL_SHIM_LIB} z)
+set(NET_TESTS_LIBS vk::common_src vk::net_src vk::binlog_src vk::unicode ${NET_TESTS_LIBS} ${EPOLL_SHIM_LIB} OpenSSL::Crypto z)
 vk_add_unittest(net "${NET_TESTS_LIBS}" ${NET_TESTS_SOURCES})
