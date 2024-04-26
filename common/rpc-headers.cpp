65e23787d8518a44da91b3d916bb251d4d4de4fa
Alexander Polyakov <al.polyakov@vk.team>
2024-04-26 16:14:02 +0300

    Correct the way we work with doubly set ignore_answer and actor_id

--- common/rpc-headers.cpp
+++ common/rpc-headers.cpp
@@ -5,6 +5,8 @@
 #include "common/algorithms/find.h"
 #include "common/rpc-headers.h"
 #include "common/tl/constants/common.h"
+#include "common/kprintf.h"
+
 
 std::pair<std::optional<std::pair<RpcExtraHeaders, std::size_t>>, std::size_t>
 regularize_wrappers(const char *rpc_payload, std::int32_t actor_id, bool ignore_result) {
@@ -21,34 +23,60 @@ regularize_wrappers(const char *rpc_payload, std::int32_t actor_id, bool ignore_
   RpcExtraHeaders extra_headers{};
   const std::size_t new_wrapper_size{sizeof(RpcDestActorFlagsHeaders)};
   std::size_t cur_wrapper_size{0};
+  std::int32_t cur_wrapper_actor_id{0};
+  bool cur_wrapper_ignore_result{false};
 
   switch (function_magic) {
     case TL_RPC_DEST_ACTOR_FLAGS:
       cur_wrapper_size = sizeof(RpcDestActorFlagsHeaders);
+      cur_wrapper_actor_id = cur_wrapper.rpc_dest_actor_flags.actor_id;
+      cur_wrapper_ignore_result = static_cast<bool>(cur_wrapper.rpc_dest_actor_flags.flags & vk::tl::common::rpc_invoke_req_extra_flags::no_result);
+
       extra_headers.rpc_dest_actor_flags.op = TL_RPC_DEST_ACTOR_FLAGS;
       extra_headers.rpc_dest_actor_flags.actor_id = actor_id != 0 ? actor_id : cur_wrapper.rpc_dest_actor_flags.actor_id;
-      extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_actor_flags.flags |
-                                                 (ignore_result ? vk::tl::common::rpc_invoke_req_extra_flags::no_result : 0x0);
+      if (ignore_result) {
+        extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_actor_flags.flags | vk::tl::common::rpc_invoke_req_extra_flags::no_result;
+      } else {
+        extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_actor_flags.flags & ~vk::tl::common::rpc_invoke_req_extra_flags::no_result;
+      }
+
       break;
     case TL_RPC_DEST_ACTOR:
       cur_wrapper_size = sizeof(RpcDestActorHeaders);
+      cur_wrapper_actor_id = cur_wrapper.rpc_dest_actor.actor_id;
+
       extra_headers.rpc_dest_actor_flags.op = TL_RPC_DEST_ACTOR_FLAGS;
       extra_headers.rpc_dest_actor_flags.actor_id = actor_id != 0 ? actor_id : cur_wrapper.rpc_dest_actor.actor_id;
       extra_headers.rpc_dest_actor_flags.flags = ignore_result ? vk::tl::common::rpc_invoke_req_extra_flags::no_result : 0x0;
+
       break;
     case TL_RPC_DEST_FLAGS:
       cur_wrapper_size = sizeof(RpcDestFlagsHeaders);
+      cur_wrapper_ignore_result = static_cast<bool>(cur_wrapper.rpc_dest_flags.flags & vk::tl::common::rpc_invoke_req_extra_flags::no_result);
+
       extra_headers.rpc_dest_actor_flags.op = TL_RPC_DEST_ACTOR_FLAGS;
       extra_headers.rpc_dest_actor_flags.actor_id = actor_id;
-      extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_flags.flags |
-                                                 (ignore_result ? vk::tl::common::rpc_invoke_req_extra_flags::no_result : 0x0);
+      if (ignore_result) {
+        extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_flags.flags | vk::tl::common::rpc_invoke_req_extra_flags::no_result;
+      } else {
+        extra_headers.rpc_dest_actor_flags.flags = cur_wrapper.rpc_dest_flags.flags & ~vk::tl::common::rpc_invoke_req_extra_flags::no_result;
+      }
+
       break;
     default:
       // we don't have a cur_wrapper, but we do have 'actor_id' or 'ignore_result' set
       extra_headers.rpc_dest_actor_flags.op = TL_RPC_DEST_ACTOR_FLAGS;
       extra_headers.rpc_dest_actor_flags.actor_id = actor_id;
       extra_headers.rpc_dest_actor_flags.flags = ignore_result ? vk::tl::common::rpc_invoke_req_extra_flags::no_result : 0x0;
+
       break;
+  }
+
+  if (actor_id != 0 && cur_wrapper_actor_id != 0) {
+    kprintf("inaccurate use of 'actor_id': overwriting '%d' with '%d'", cur_wrapper_actor_id, actor_id);
+  }
+  if (ignore_result != cur_wrapper_ignore_result) {
+    kprintf("inaccurate use of 'ignore_answer': overwriting '%s' to '%s'", cur_wrapper_ignore_result ? "true" : "false", ignore_result ? "true" : "false");
   }
 
   return {std::pair<RpcExtraHeaders, std::size_t>{extra_headers, new_wrapper_size}, cur_wrapper_size};
