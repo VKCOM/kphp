d4cb0f093df51159edf66a22d808f12bbb3402e7
Mikhail Kornaukhov <66915223+mkornaukhov03@users.noreply.github.com>
2024-04-26 11:33:31 +0300

    Compiling k2-component into dynamic library (#988)

---
+++ compiler/make/objs-to-k2-component-target.h
@@ -0,0 +1,28 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2024 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include <sstream>
+
+#include "compiler/compiler-settings.h"
+#include "compiler/make/target.h"
+
+class Objs2K2ComponentTarget : public Target {
+public:
+  std::string get_cmd() final {
+    std::stringstream ss;
+    ss << settings->cxx.get() << " -shared -o " << target() << " ";
+
+    for (size_t i = 0; i + 1 < deps.size(); ++i) {
+      ss << deps[i]->get_name() << " ";
+    }
+
+    // the last dep is runtime lib
+    // todo:k2 think about kphp-libraries
+    assert(deps.size() >= 1 && "There are should be at least one dependency. It's the runtime lib");
+    ss << "-Wl,--whole-archive " << deps.back()->get_name() << " -Wl,--no-whole-archive ";
+    return ss.str();
+  }
+};
