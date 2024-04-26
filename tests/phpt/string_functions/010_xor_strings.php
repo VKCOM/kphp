13195126e598e2f3f2cd2cecc80c5f71aedbbfd7
Alexander Polyakov <al.polyakov@vk.team>
2024-04-26 20:36:53 +0300

    Correctly use #ifndef KPHP in tests

--- tests/phpt/string_functions/010_xor_strings.php
+++ tests/phpt/string_functions/010_xor_strings.php
@@ -1,18 +1,24 @@
 @ok
 <?php
 
+function is_kphp() {
+#ifndef KPHP
+    return false;
+#endif
+    return true;
+}
+
 function test_xor_strings() {
   $strings = ["handling and manipulating", "ings, take a look", "ctory Access Protocol", "0000000dwxasd", "1111sadsd11"];
 
   foreach ($strings as $str1) {
     foreach($strings as $str2) {
-#ifndef KPHP
-        $res = $str1 ^ $str2;
-#else
+      if (is_kphp()) {
         $res = xor_strings($str1, $str2);
-#endif
-
-        var_dump($res);
+      } else {
+        $res = $str1 ^ $str2;
+      }
+      var_dump($res);
     }
   }
 }
