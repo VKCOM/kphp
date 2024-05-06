eb6bc253d066a706e42aa16dc319bd8ae92586d7
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2024-05-06 11:57:46 +0300

    Integrate kml into kphp runtime and create php wrappers

---
+++ runtime/kphp_ml/kphp_ml_init.h
@@ -0,0 +1,18 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2024 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include "runtime/kphp_core.h"
+
+namespace kphp_ml { struct MLModel; }
+
+extern const char *kml_directory;
+
+void init_kphp_ml_runtime_in_master();
+void init_kphp_ml_runtime_in_worker();
+
+
+char *kphp_ml_get_mutable_buffer_in_current_worker();
+const kphp_ml::MLModel *kphp_ml_find_loaded_model_by_name(const string &model_name);
