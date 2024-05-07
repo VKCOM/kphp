e7c41dedf166f50386fc643ee893ee48139277cf
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2024-05-07 13:43:45 +0300

    Remove redundant compile option

--- cmake/init-compilation-flags.cmake
+++ cmake/init-compilation-flags.cmake
@@ -87,7 +87,7 @@ elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
 endif()
 
 add_compile_options(-Wall -Wextra -Wunused-function -Wfloat-conversion -Wno-sign-compare
-                    -Wuninitialized -Wno-redundant-move -Wno-missing-field-initializers -Wno-vla-cxx-extension)
+                    -Wuninitialized -Wno-redundant-move -Wno-missing-field-initializers)
 
 if(NOT APPLE)
     check_cxx_compiler_flag(-gz=zlib DEBUG_COMPRESSION_IS_FOUND)
