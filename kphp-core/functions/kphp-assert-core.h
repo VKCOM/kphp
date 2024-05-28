7e3630827d0ffc2f7a0e5bf4a3abbda33fdc68de
Vadim Sadokhov <v.sadokhov@vk.team>
2024-05-28 15:50:43 +0300

    add c++11 noreturn

--- kphp-core/functions/kphp-assert-core.h
+++ kphp-core/functions/kphp-assert-core.h
@@ -8,8 +8,8 @@ void php_notice(char const *message, ...) __attribute__ ((format (printf, 1, 2))
 void php_warning(char const *message, ...) __attribute__ ((format (printf, 1, 2)));
 void php_error(char const *message, ...) __attribute__ ((format (printf, 1, 2)));
 
-void php_assert__(const char *msg, const char *file, int line) __attribute__((noreturn));
-void raise_php_assert_signal__() __attribute__((noreturn));
+[[noreturn]] void php_assert__(const char *msg, const char *file, int line);
+[[noreturn]] void raise_php_assert_signal__();
 
 #define php_assert(EX) do {                          \
   if (unlikely(!(EX))) {                             \
