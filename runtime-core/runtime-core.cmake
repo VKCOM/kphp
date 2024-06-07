20268751c879566c0d4cdf747227fbbeea40a8bf
Vadim Sadokhov <v.sadokhov@vk.team>
2024-06-07 19:27:06 +0300

    rename kphp-types and functions to core-types and utils

--- runtime-core/runtime-core.cmake
+++ runtime-core/runtime-core.cmake
@@ -1,18 +1,15 @@
-prepend(KPHP_CORE_RUNTIME_TYPES ${BASE_DIR}/runtime-core/runtime-types/
-        string_cache.cpp
-)
-
-prepend(KPHP_CORE_RUNTIME_FUNTCIONS ${BASE_DIR}/runtime-core/functions/
+prepend(KPHP_CORE_RUNTIME_UTILS ${BASE_DIR}/runtime-core/utils/
         migration-php8.cpp
 )
 
-prepend(KPHP_CORE_KPHP_TYPES ${BASE_DIR}/runtime-core/kphp-types/definition/
+prepend(KPHP_CORE_TYPES ${BASE_DIR}/runtime-core/core-types/definition/
         mixed.cpp
         string.cpp
         string_buffer.cpp
+        string_cache.cpp
 )
 
-prepend(KPHP_CORE_MEMORY_RECOURCE ${BASE_DIR}/runtime-core/memory-resource/
+prepend(KPHP_CORE_MEMORY_RESOURCE ${BASE_DIR}/runtime-core/memory-resource/
         details/memory_chunk_tree.cpp
         details/memory_ordered_chunk_list.cpp
         monotonic_buffer_resource.cpp
@@ -20,10 +17,9 @@ prepend(KPHP_CORE_MEMORY_RECOURCE ${BASE_DIR}/runtime-core/memory-resource/
 )
 
 set(KPHP_CORE_SRC
-        ${KPHP_CORE_RUNTIME_FUNTCIONS}
-        ${KPHP_CORE_RUNTIME_TYPES}
-        ${KPHP_CORE_KPHP_TYPES}
-        ${KPHP_CORE_MEMORY_RECOURCE}
+        ${KPHP_CORE_RUNTIME_UTILS}
+        ${KPHP_CORE_TYPES}
+        ${KPHP_CORE_MEMORY_RESOURCE}
 )
 
 vk_add_library(runtime-core OBJECT ${KPHP_CORE_SRC})
