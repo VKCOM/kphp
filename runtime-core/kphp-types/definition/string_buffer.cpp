72fef6ce0ec8ad7083987c8fc3fd301be9d23352
Vadim Sadokhov <v.sadokhov@vk.team>
2024-06-07 16:19:28 +0300

    Revert "make string_buffer allocations on script mem"
    This reverts commit 9314c27126befeff425deca554698633eb7f0573.

--- runtime-core/kphp-types/definition/string_buffer.cpp
+++ runtime-core/kphp-types/definition/string_buffer.cpp
@@ -5,11 +5,11 @@
 #include "runtime-core/runtime-core.h"
 
 string_buffer::string_buffer(string::size_type buffer_len) noexcept:
-  buffer_end(static_cast<char *>(RuntimeAllocator::current().alloc_script_memory(buffer_len))),
+  buffer_end(static_cast<char *>(RuntimeAllocator::current().alloc_global_memory(buffer_len))),
   buffer_begin(buffer_end),
   buffer_len(buffer_len) {
 }
 
 string_buffer::~string_buffer() noexcept {
-  RuntimeAllocator::current().free_script_memory(buffer_begin, buffer_len);
+  RuntimeAllocator::current().free_global_memory(buffer_begin, buffer_len);
 }
