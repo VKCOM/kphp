efc8605a0b5c357f79946bb447c55c9d3b04c2e9
Alexander Polyakov <al.polyakov@vk.team>
2024-06-13 15:53:56 +0300

    Add TODO to remove unnecessary builtin when 'wait' builtin is ready

--- runtime-light/stdlib/worker/worker_api.h
+++ runtime-light/stdlib/worker/worker_api.h
@@ -15,13 +15,13 @@ constexpr int64_t JOB_WORKER_IGNORED_JOB_ID = -2;
 
 // === Client =====================================================================================
 
-task_t<Optional<int64_t>> f$kphp_job_worker_start(string request, double timeout) noexcept; // TODO: why optional???
+task_t<Optional<int64_t>> f$kphp_job_worker_start(string request, double timeout) noexcept;
 
 task_t<bool> f$kphp_job_worker_start_no_reply(string request, double timeout) noexcept;
 
 task_t<array<Optional<int64_t>>> f$kphp_job_worker_start_multi(array<string> requests, double timeout) noexcept;
 
-task_t<string> f$kphp_job_worker_fetch_response(int64_t job_id) noexcept;
+task_t<string> f$kphp_job_worker_fetch_response(int64_t job_id) noexcept; // TODO: remove when 'wait' builtin is ready
 
 // === Server =====================================================================================
 
