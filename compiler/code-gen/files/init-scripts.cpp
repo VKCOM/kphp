8766c3d6c9e4a925c53ca0c22b3b5612ae6749e5
Vadim Sadokhov <v.sadokhov@vk.team>
2024-06-24 20:15:24 +0300

    fix code-gen of initial function

--- compiler/code-gen/files/init-scripts.cpp
+++ compiler/code-gen/files/init-scripts.cpp
@@ -114,9 +114,7 @@ struct RunInterruptedFunction {
   void compile(CodeGenerator &W) const {
     std::string await_prefix = function->is_interruptible ? "co_await " : "";
     FunctionSignatureGenerator(W) << "task_t<void> " << FunctionName(function) << "$run() " << BEGIN
-                                  << "co_await parse_input_query();" << NL
                                   << await_prefix << FunctionName(function) << "();" << NL
-                                  << "co_await finish (0, false);" << NL
                                   << END;
     W << NL;
   }
