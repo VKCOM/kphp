42ffba55717c3645498813736143b01f86cd6450
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2024-06-25 13:16:19 +0300

    Add a comment in the code

--- common/exit-codes.h
+++ common/exit-codes.h
@@ -3,6 +3,11 @@
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
 #pragma once
+
+// The exit codes were selected according to https://tldp.org/LDP/abs/html/exitcodes.html
+// 100, 101, and 102 are free for statuses defined by the programmer and they look pretty
+// If the app is going to exit because of a signal, then the return code is 128 + %signal_code%,
+// which is also a common practice
 enum class ExitCode {
   KPHP_TO_CPP_STAGE = 100,
   CPP_TO_OBJS_STAGE = 101,
