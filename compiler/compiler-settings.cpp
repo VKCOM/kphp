937d928b48960b888a3d10640646563ab3449b61
Арутюнян Андрей Романович <andreylzmw@gmail.com>
2024-07-17 21:28:26 +0300

    remove -Wno-unused-result flag

--- compiler/compiler-settings.cpp
+++ compiler/compiler-settings.cpp
@@ -312,7 +312,7 @@ void CompilerSettings::init() {
     ss << " -std=c++17";
   #elif __cplusplus <= 202002L
     ss << " -std=c++20";
-    ss << " -Wno-unused-result -Wno-type-limits -Wno-attributes -Wno-ignored-attributes";
+    ss << " -Wno-type-limits -Wno-attributes -Wno-ignored-attributes";
   #else
     #error unsupported __cplusplus value
   #endif
