002ee0b549547f65c96ad8a2ac42ccab803d8681
Alexander Polyakov <al.polyakov@vk.team>
2024-07-25 12:36:02 +0300

    Rework utils

--- runtime-light/utils/panic.h
+++ runtime-light/utils/panic.h
@@ -8,17 +8,18 @@
 
 #include "context.h"
 #include "runtime-light/component/component.h"
+#include "runtime-light/header.h"
 #include "runtime-light/utils/logs.h"
 
 inline void critical_error_handler() {
-  constexpr const char * message = "script panic";
-  const PlatformCtx & ptx = *get_platform_context();
-  ComponentState & ctx = *get_component_context();
-  ptx.log(Debug, strlen(message), message);
+  constexpr const char *message = "script panic";
+  const auto &platform_ctx = *get_platform_context();
+  auto &component_ctx = *get_component_context();
+  platform_ctx.log(Debug, strlen(message), message);
 
-  if (ctx.not_finished()) {
-    ctx.poll_status = PollStatus::PollFinishedError;
+  if (component_ctx.poll_status != PollStatus::PollFinishedOk && component_ctx.poll_status != PollStatus::PollFinishedError) {
+    component_ctx.poll_status = PollStatus::PollFinishedError;
   }
-  ptx.abort();
+  platform_ctx.abort();
   exit(1);
 }
