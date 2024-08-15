c265c18bda83b6a0f8820767bfbc2ec69560539c
Dmitry Solomennikov <144122424+soloth@users.noreply.github.com>
2024-08-15 22:13:26 +0300

    Do not remove empty default values for string and array respectively (#1028)
    * Do not remove empty string and empty array as default values for string and array respectively
    * Save flag initial value for field was eliminated by optimization
    * Add test case
    * Mark test as correct
    
    Signed-off-by: dsolomennikov <dsolomennikov@>

---
+++ tests/phpt/json/38_optimized_init_val.php
@@ -0,0 +1,23 @@
+@ok
+<?php
+
+class User {
+  public string $name = '';
+  public string $password = '';
+}
+
+function test_optimized_init_value(): void {
+  $raw = '{"name": "user"}';
+
+  $user = JsonEncoder::decode($raw, User::class);
+
+  $error = JsonEncoder::getLastError();
+  if (!empty($error)) {
+    echo "Error: " . $error;
+    exit();
+  }
+
+  var_dump(instance_to_array($user));
+}
+
+test_optimized_init_value();
