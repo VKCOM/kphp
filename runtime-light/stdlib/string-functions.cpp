bc3a04ce79f8496bc4d46057c4a84efcdb993eb0
Alexander Polyakov <al.polyakov@vk.team>
2024-08-16 18:38:16 +0300

    Small fixes to silence C++ warnings

--- runtime-light/stdlib/string-functions.cpp
+++ runtime-light/stdlib/string-functions.cpp
@@ -35,14 +35,14 @@ void f$debug_print_string(const string &s) {
 Optional<int64_t> f$byte_to_int(const string &s) {
   if (s.size() != 1) {
     php_warning("Cannot convert non-byte string to int");
-    return Optional<int64_t>();
+    return {};
   }
   return *s.c_str();
 }
 
 Optional<string> f$int_to_byte(int64_t v) {
   if (v > 127 || v < -128) {
-    php_warning("Cannot convert too big int to byte %ld", v);
+    php_warning("Cannot convert too big int to byte %" PRId64, v);
     return false;
   }
   char c = v;
