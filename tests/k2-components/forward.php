65d41c3bd51bd6ad5042cc2ea905015ce27c22d5
Alexander Polyakov <al.polyakov@vk.team>
2024-08-25 23:48:17 +0300

    Update k2 component tests

--- tests/k2-components/forward.php
+++ tests/k2-components/forward.php
@@ -1,10 +1,11 @@
 <?php
 
-$str = component_server_get_query();
-$id = component_client_send_query("out", $str);
+$query = component_server_accept_query();
+$str = component_server_fetch_request($query);
+$id = component_client_send_request("out", $str);
 if (is_null($id)) {
     warning("stream to out is null");
 } else {
-    $result = component_client_get_result($id);
-    component_server_send_result($result);
+    $result = component_client_fetch_response($id);
+    component_server_send_response($query, $result);
 }
