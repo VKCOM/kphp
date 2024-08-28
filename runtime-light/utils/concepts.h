9cc15d9afeee2b88a4fca3a793d39f634c0849f4
Vadim Sadokhov <v.sadokhov@vk.team>
2024-08-28 12:13:46 +0300

    revert runtime changes

--- runtime-light/utils/concepts.h
+++ runtime-light/utils/concepts.h
@@ -5,6 +5,13 @@
 #pragma once
 
 #include <concepts>
+#include <cstddef>
+#include <functional>
 
 template<typename T>
 concept standard_layout = std::is_standard_layout_v<T>;
+
+template<typename T>
+concept hashable = requires(T a) {
+  { std::hash<T>{}(a) } -> std::convertible_to<size_t>;
+};
