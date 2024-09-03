0e2069cc2d4787d50e238b831b8358604d24bf0d
Vadim Sadokhov <v.sadokhov@vk.team>
2024-09-03 17:45:00 +0300

    make phpt work

--- runtime-light/stdlib/string/string-functions.h
+++ runtime-light/stdlib/string/string-functions.h
@@ -8,24 +8,19 @@
 
 #include "runtime-light/stdlib/string/string-context.h"
 
-template<class T>
-Optional<int64_t> f$strpos(const string &, const Optional<T> &, int64_t = 0) {
-  php_critical_error("call to unsupported function");
-}
-
 inline int64_t f$strlen(const string &s) noexcept {
   return s.size();
 }
 
-inline tmp_string f$_tmp_substr(const string &, int64_t, int64_t = std::numeric_limits<int64_t>::max()) {
+inline tmp_string f$_tmp_substr(const string &, int64_t, int64_t  = std::numeric_limits<int64_t>::max()) {
   php_critical_error("call to unsupported function");
 }
 
-inline tmp_string f$_tmp_substr(tmp_string, int64_t, int64_t = std::numeric_limits<int64_t>::max()) {
+inline tmp_string f$_tmp_substr(tmp_string , int64_t, int64_t  = std::numeric_limits<int64_t>::max()) {
   php_critical_error("call to unsupported function");
 }
 
-inline tmp_string f$_tmp_trim(tmp_string, const string & = string()) {
+inline tmp_string f$_tmp_trim(tmp_string , const string & = string()) {
   php_critical_error("call to unsupported function");
 }
 
@@ -33,7 +28,7 @@ inline tmp_string f$_tmp_trim(const string &, const string & = string()) {
   php_critical_error("call to unsupported function");
 }
 
-inline string f$trim(tmp_string, const string & = string()) {
+inline string f$trim(tmp_string , const string & = string()) {
   php_critical_error("call to unsupported function");
 }
 
@@ -41,13 +36,14 @@ inline string f$trim(const string &, const string & = string()) {
   php_critical_error("call to unsupported function");
 }
 
-inline Optional<string> f$substr(const string &, int64_t, int64_t = std::numeric_limits<int64_t>::max()) {
+inline Optional<string> f$substr(const string &, int64_t, int64_t  = std::numeric_limits<int64_t>::max()) {
   php_critical_error("call to unsupported function");
 }
 
-inline Optional<string> f$substr(tmp_string, int64_t, int64_t = std::numeric_limits<int64_t>::max()) {
+inline Optional<string> f$substr(tmp_string, int64_t, int64_t  = std::numeric_limits<int64_t>::max()) {
   php_critical_error("call to unsupported function");
 }
+
 
 inline string f$pack(const string &, const array<mixed> &) {
   php_critical_error("call to unsupported function");
@@ -75,8 +71,7 @@ inline string f$str_replace(const mixed &, const mixed &, const string &, int64_
 }
 
 template<class T1, class T2, class SubjectT, class = enable_if_t_is_optional_string<SubjectT>>
-SubjectT f$str_replace(const T1 &search, const T2 &replace, const SubjectT &subject,
-                       int64_t &replace_count = StringComponentContext::get().str_replace_count_dummy) {
+SubjectT f$str_replace(const T1 &search, const T2 &replace, const SubjectT &subject, int64_t &replace_count = StringComponentContext::get().str_replace_count_dummy) {
   return f$str_replace(search, replace, subject.val(), replace_count);
 }
 
@@ -94,8 +89,7 @@ inline string f$str_ireplace(const mixed &, const mixed &, const string &, int64
 }
 
 template<class T1, class T2, class SubjectT, class = enable_if_t_is_optional_string<SubjectT>>
-SubjectT f$str_ireplace(const T1 &search, const T2 &replace, const SubjectT &subject,
-                        int64_t &replace_count = StringComponentContext::get().str_replace_count_dummy) {
+SubjectT f$str_ireplace(const T1 &search, const T2 &replace, const SubjectT &subject, int64_t &replace_count = StringComponentContext::get().str_replace_count_dummy) {
   return f$str_ireplace(search, replace, subject.val(), replace_count);
 }
 
