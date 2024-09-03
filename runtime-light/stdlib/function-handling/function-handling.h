f0e6402a7198e3a527fa656959a8426b07a46aff
Vadim Sadokhov <v.sadokhov@vk.team>
2024-09-03 14:17:26 +0300

    fix utils compile

--- runtime-light/stdlib/function-handling/function-handling.h
+++ runtime-light/stdlib/function-handling/function-handling.h
@@ -6,22 +6,22 @@
 
 #include "runtime-core/runtime-core.h"
 
-template <typename F>
+template<typename F>
 void f$register_shutdown_function(F &&f) {
   php_critical_error("call to unsupported function");
 }
 
-template <typename F>
+template<typename F>
 void f$register_kphp_on_warning_callback(F &&callback) {
   php_critical_error("call to unsupported function");
 }
 
-template <typename F>
+template<typename F>
 bool f$register_kphp_on_oom_callback(F &&callback) {
   php_critical_error("call to unsupported function");
 }
 
-template <typename F>
+template<typename F>
 void f$kphp_extended_instance_cache_metrics_init(F &&callback) {
   php_critical_error("call to unsupported function");
 }
