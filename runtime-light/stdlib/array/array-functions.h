533373d32b1996c7503ed3f53103dcbc4a095bb8
Alexander Polyakov <al.polyakov@vk.team>
2024-09-17 14:04:16 +0300

    Mark array_map as noexcept

--- runtime-light/stdlib/array/array-functions.h
+++ runtime-light/stdlib/array/array-functions.h
@@ -66,7 +66,7 @@ array<T> f$array_filter_by_key(const array<T> &a, const T1 &callback) noexcept {
 }
 
 template<class A, std::invocable<A> F, class R = async_function_unwrapped_return_type_t<F, A>>
-task_t<array<R>> f$array_map(F f, array<A> arr) {
+task_t<array<R>> f$array_map(F f, array<A> arr) noexcept {
   array<R> result{arr.size()};
   for (const auto &it : arr) {
     if constexpr (is_async_function_v<F, A>) {
