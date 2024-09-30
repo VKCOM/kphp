ac2839f3e2ffe0f0245e8810e8cd3ffa1db2c489
Alexander Polyakov <al.polyakov@vk.team>
2024-09-30 14:19:50 +0300

    Add iterator support to tl::Dictionary

--- runtime-light/tl/tl-types.h
+++ runtime-light/tl/tl-types.h
@@ -221,6 +221,32 @@ template<typename T>
 struct Dictionary final {
   dictionary<T> data{};
 
+  using iterator = dictionary<T>::iterator;
+  using const_iterator = dictionary<T>::const_iterator;
+
+  iterator begin() noexcept {
+    return data.begin();
+  }
+  iterator end() noexcept {
+    return data.end();
+  }
+  const_iterator begin() const noexcept {
+    return data.begin();
+  }
+  const_iterator end() const noexcept {
+    return data.end();
+  }
+  const_iterator cbegin() const noexcept {
+    return data.cbegin();
+  }
+  const_iterator cend() const noexcept {
+    return data.cend();
+  }
+
+  size_t size() const noexcept {
+    return data.size();
+  }
+
   bool fetch(TLBuffer &tlb) noexcept requires tl_deserializable<T> {
     if (tlb.fetch_trivial<uint32_t>().value_or(TL_ZERO) != TL_DICTIONARY) {
       return false;
