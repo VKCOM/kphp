8e93099ed530a6e7e89291cdf39ff090d29b68d0
Vadim Sadokhov <v.sadokhov@vk.team>
2024-10-21 15:27:57 +0300

    fix critical_error_handler

--- runtime-light/utils/panic.cpp
+++ runtime-light/utils/panic.cpp
@@ -13,11 +13,10 @@
 void critical_error_handler() {
   constexpr const char *message = "script panic";
   const auto &platform_ctx = *get_platform_context();
-  auto &component_ctx = *get_component_context();
   platform_ctx.log(Debug, strlen(message), message);
 
-  if (component_ctx.poll_status != PollStatus::PollFinishedOk && component_ctx.poll_status != PollStatus::PollFinishedError) {
-    component_ctx.poll_status = PollStatus::PollFinishedError;
+  if (get_component_context() != nullptr) {
+    get_component_context()->poll_status = PollStatus::PollFinishedError;
   }
   platform_ctx.abort();
   exit(1);
