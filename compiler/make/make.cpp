9277980ae4d55a36e42cbea2b941ae1983f53830
Alexander Polyakov <al.polyakov@vk.team>
2024-11-01 17:34:08 +0300

    Fix naming: unicode_index -> unicode_dir

--- compiler/make/make.cpp
+++ compiler/make/make.cpp
@@ -460,12 +460,12 @@ static std::vector<File *> build_runtime_and_common_from_sources(const std::stri
   const Index &runtime_core_dir = G->get_runtime_core_index();
   const Index &runtime_dir = G->get_runtime_index();
   const Index &common_dir = G->get_common_index();
-  const Index &unicode_index = G->get_unicode_index();
+  const Index &unicode_dir = G->get_unicode_index();
 
   std::vector<File*> objs;
   objs.reserve(runtime_dir.get_files_count() + common_dir.get_files_count());
 
-  for (const auto *dir : std::vector<const Index*>{&runtime_core_dir, &runtime_dir, &common_dir, &unicode_index}) {
+  for (const auto *dir : std::vector<const Index*>{&runtime_core_dir, &runtime_dir, &common_dir, &unicode_dir}) {
     for (File *cpp_file : dir->get_files()) {
       File *obj_file = obj_dir.insert_file(static_cast<std::string>(cpp_file->name_without_ext) + ".o");
       make.create_cpp_target(cpp_file);
