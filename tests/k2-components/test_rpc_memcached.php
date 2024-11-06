5e67a03ea61189fe59cafb7e85f8ac9810b73483
Vadim Sadokhov <v.sadokhov@vk.team>
2024-11-06 12:42:48 +0300

    fix issues

--- tests/k2-components/test_rpc_memcached.php
+++ tests/k2-components/test_rpc_memcached.php
@@ -1,7 +1,7 @@
 <?php
 
 function do_untyped_rpc() {
-  $ids = rpc_send_requests("mc_main", [["_" => "memcache.get", "key" => "xxx"]]);
+  $ids = rpc_send_requests("mc_main", [["_" => "memcache.get", "key" => "xxx"]], -1, false, null, false);
   $response = rpc_fetch_responses($ids)[0];
   $result = $response["result"];
   if ($result["_"] !== "memcache.not_found") {
@@ -9,15 +9,15 @@ function do_untyped_rpc() {
     return false;
   }
 
-  $ids = rpc_send_requests("mc_main", [["_" => "memcache.set", "key" => "foo", "flags" => 0, "delay" => 0, "value" => "bar"]]);
+  $ids = rpc_send_requests("mc_main", [["_" => "memcache.set", "key" => "foo", "flags" => 0, "delay" => 0, "value" => "bar"]], -1, false, null, false);
   $response = rpc_fetch_responses($ids)[0];
   $result = $response["result"];
   if ($result !== true) {
     warning("true expected");
     return false;
   }
-  
-  $ids = rpc_send_requests("mc_main", [["_" => "memcache.get", "key" => "foo"]]);
+
+  $ids = rpc_send_requests("mc_main", [["_" => "memcache.get", "key" => "foo"]], -1, false, null, false);
   $response = rpc_fetch_responses($ids)[0];
   $result = $response["result"];
   if ($result["_"] !== "memcache.strvalue" || $result["value"] !== "bar") {
