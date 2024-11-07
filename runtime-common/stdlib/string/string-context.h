4d4d604138d823c1999b24864b9a38ee1db27982
Alexander Polyakov <al.polyakov@vk.team>
2024-11-07 23:09:10 +0300

    Use std::string_view instead of raw char* in string-context.h

--- runtime-common/stdlib/string/string-context.h
+++ runtime-common/stdlib/string/string-context.h
@@ -6,7 +6,7 @@
 
 #include <array>
 #include <cstdint>
-#include <string>
+#include <string_view>
 
 #include "common/mixin/not_copyable.h"
 #include "common/php-functions.h"
@@ -14,17 +14,19 @@
 
 namespace string_context_impl_ {
 
-inline constexpr auto *COLON_ = ",";
-inline constexpr auto *CP1251_ = "cp1251";
-inline constexpr auto *DOT_ = ".";
-inline constexpr auto *COMMA_ = ",";
-inline constexpr auto *BACKSLASH_ = "\\";
-inline constexpr auto *QUOTE_ = "\"";
-inline constexpr auto *NEWLINE_ = "\n";
-inline constexpr auto *SPACE_ = " ";
-inline constexpr auto *WHAT_ = " \n\r\t\v\0";
-inline constexpr auto *ONE_ = "1";
-inline constexpr auto *PERCENT_ = "%";
+using namespace std::literals;
+
+inline constexpr std::string_view COLON_ = ",";
+inline constexpr std::string_view CP1251_ = "cp1251";
+inline constexpr std::string_view DOT_ = ".";
+inline constexpr std::string_view COMMA_ = ",";
+inline constexpr std::string_view BACKSLASH_ = "\\";
+inline constexpr std::string_view QUOTE_ = "\"";
+inline constexpr std::string_view NEWLINE_ = "\n";
+inline constexpr std::string_view SPACE_ = " ";
+inline constexpr std::string_view WHAT_ = " \n\r\t\v\0"sv;
+inline constexpr std::string_view ONE_ = "1";
+inline constexpr std::string_view PERCENT_ = "%";
 
 }; // namespace string_context_impl_
 
@@ -47,18 +49,17 @@ public:
 };
 
 struct StringLibConstants final : vk::not_copyable {
-  string COLON_STR{string_context_impl_::COLON_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::COLON_))};
-  string CP1251_STR{string_context_impl_::CP1251_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::CP1251_))};
-  string DOT_STR{string_context_impl_::DOT_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::DOT_))};
-  string COMMA_STR{string_context_impl_::COMMA_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::COMMA_))};
-  string BACKSLASH_STR{string_context_impl_::BACKSLASH_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::BACKSLASH_))};
-  string QUOTE_STR{string_context_impl_::QUOTE_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::QUOTE_))};
-  string NEWLINE_STR{string_context_impl_::NEWLINE_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::NEWLINE_))};
-  string SPACE_STR{string_context_impl_::SPACE_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::SPACE_))};
-  // +1 here to since char_traits<char>::length doesn't count '\0' at the end
-  string WHAT_STR{string_context_impl_::WHAT_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::WHAT_)) + 1};
-  string ONE_STR{string_context_impl_::ONE_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::ONE_))};
-  string PERCENT_STR{string_context_impl_::PERCENT_, static_cast<string::size_type>(std::char_traits<char>::length(string_context_impl_::PERCENT_))};
+  string COLON_STR{string_context_impl_::COLON_.data(), static_cast<string::size_type>(string_context_impl_::COLON_.size())};
+  string CP1251_STR{string_context_impl_::CP1251_.data(), static_cast<string::size_type>(string_context_impl_::CP1251_.size())};
+  string DOT_STR{string_context_impl_::DOT_.data(), static_cast<string::size_type>(string_context_impl_::DOT_.size())};
+  string COMMA_STR{string_context_impl_::COMMA_.data(), static_cast<string::size_type>(string_context_impl_::COMMA_.size())};
+  string BACKSLASH_STR{string_context_impl_::BACKSLASH_.data(), static_cast<string::size_type>(string_context_impl_::BACKSLASH_.size())};
+  string QUOTE_STR{string_context_impl_::QUOTE_.data(), static_cast<string::size_type>(string_context_impl_::QUOTE_.size())};
+  string NEWLINE_STR{string_context_impl_::NEWLINE_.data(), static_cast<string::size_type>(string_context_impl_::NEWLINE_.size())};
+  string SPACE_STR{string_context_impl_::SPACE_.data(), static_cast<string::size_type>(string_context_impl_::SPACE_.size())};
+  string WHAT_STR{string_context_impl_::WHAT_.data(), static_cast<string::size_type>(string_context_impl_::WHAT_.size())};
+  string ONE_STR{string_context_impl_::ONE_.data(), static_cast<string::size_type>(string_context_impl_::ONE_.size())};
+  string PERCENT_STR{string_context_impl_::PERCENT_.data(), static_cast<string::size_type>(string_context_impl_::PERCENT_.size())};
 
   const char lhex_digits[17] = "0123456789abcdef";
   const char uhex_digits[17] = "0123456789ABCDEF";
