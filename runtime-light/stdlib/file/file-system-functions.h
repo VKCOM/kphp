51221728773576196f0e33ecf8afe2258525f2af
Alexander Polyakov <al.polyakov@vk.team>
2024-11-14 17:28:48 +0300

    Implement basename builtin

---
+++ runtime-light/stdlib/file/file-system-functions.h
@@ -0,0 +1,39 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2024 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include <string_view>
+
+#include "runtime-common/core/runtime-core.h"
+
+namespace file_system_impl_ {
+
+inline constexpr char SEPARATOR = '/';
+
+} // namespace file_system_impl_
+
+// *** ATTENTION ***
+// For some reason KPHP's implementation of basename works incorrectly for at least following cases:
+// 1. basename("");
+// 2. basename("/");
+//
+// This implementation works the same way as PHP. That means we can face with some problems
+// during transition to K2
+inline string f$basename(const string &path, const string &suffix = {}) noexcept {
+  std::string_view path_view{path.c_str(), path.size()};
+  const std::string_view suffix_view{suffix.c_str(), suffix.size()};
+  // skip trailing separators
+  while (!path_view.empty() && path_view.back() == file_system_impl_::SEPARATOR) {
+    path_view.remove_suffix(1);
+  }
+  const auto last_slash_pos{path_view.find_last_of(file_system_impl_::SEPARATOR)};
+  std::string_view filename_view{last_slash_pos == std::string_view::npos ? path_view : path_view.substr(last_slash_pos + 1)};
+
+  if (!suffix_view.empty() && filename_view.size() >= suffix_view.size() && filename_view.ends_with(suffix_view)) {
+    filename_view.remove_suffix(suffix_view.size());
+  }
+
+  return {filename_view.data(), static_cast<string::size_type>(filename_view.size())};
+}
