27b0c1c73393ef0de226b66da54a30c224fdc76d
Alexander Polyakov <al.polyakov@vk.team>
2024-11-19 14:57:11 +0300

    Add Zlib v1.3.1 as a submodule (#1154)

--- runtime-light/runtime-light.cmake
+++ runtime-light/runtime-light.cmake
@@ -1,3 +1,6 @@
+# prepare third-parties
+update_git_submodules()
+
 include(${RUNTIME_LIGHT_DIR}/allocator/allocator.cmake)
 include(${RUNTIME_LIGHT_DIR}/core/core.cmake)
 include(${RUNTIME_LIGHT_DIR}/scheduler/scheduler.cmake)
