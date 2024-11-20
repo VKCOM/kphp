aa174ed24e45de5636ecd7c2331ab4ad73424ddd
Alexander Polyakov <al.polyakov@vk.team>
2024-11-20 12:08:55 +0300

    Explicitly specify compiler flags for abseil

--- third-party/abseil-cpp-cmake/abseil-cpp.cmake
+++ third-party/abseil-cpp-cmake/abseil-cpp.cmake
@@ -6,9 +6,16 @@ if(COMPILE_RUNTIME_LIGHT)
   set(ABSEIL_LIB_DIR "${OBJS_DIR}/lib")
   set(SAVE_CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}")
   set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${ABSEIL_LIB_DIR})
+  # set compiler flags
+  set(SAVE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  set(ABSL_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fPIC -stdlib=libc++")
+  set(CMAKE_CXX_FLAGS ${ABSL_CXX_FLAGS})
 
   add_subdirectory(${THIRD_PARTY_DIR}/abseil-cpp
                    ${CMAKE_BINARY_DIR}/third-party/abseil-cpp)
+
+  # restore compiler flags
+  set(CMAKE_CXX_FLAGS ${SAVE_CXX_FLAGS})
 
   # restore CMAKE_ARCHIVE_OUTPUT_DIRECTORY
   set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${SAVE_CMAKE_ARCHIVE_OUTPUT_DIRECTORY}")
