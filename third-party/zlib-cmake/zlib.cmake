57c0910de4d94843d6cae12d7344f3a83d728ffe
Andrei Golovatskii <golovatskii.ak@gmail.com>
2024-11-22 17:57:16 +0300

    [vendor] zlib remove rename hack, fixup macos build

--- third-party/zlib-cmake/zlib.cmake
+++ third-party/zlib-cmake/zlib.cmake
@@ -2,11 +2,13 @@ if(COMPILE_RUNTIME_LIGHT)
   set(ZLIB_BUILD_EXAMPLES OFF)
   set(ZLIB_COMPILE_FLAGS "-O3" "-fPIC")
 
+  set(RENAME_ZCONF OFF)
   add_subdirectory(${THIRD_PARTY_DIR}/zlib ${CMAKE_BINARY_DIR}/third-party/zlib)
-  # this is a hack to prevent zconf.h from being renamed to zconf.h.included
-  file(RENAME ${THIRD_PARTY_DIR}/zlib/zconf.h.included
-       ${THIRD_PARTY_DIR}/zlib/zconf.h)
+
+  target_compile_definitions(zlibstatic PRIVATE Z_HAVE_UNISTD_H)
+  target_compile_definitions(zlib PRIVATE Z_HAVE_UNISTD_H)
   target_compile_options(zlibstatic PUBLIC ${ZLIB_COMPILE_FLAGS})
+
   # Set output directories for zlib targets
   set(ZLIB_LIB_DIR "${OBJS_DIR}/lib")
   set_target_properties(zlibstatic PROPERTIES ARCHIVE_OUTPUT_DIRECTORY
