648dd65fbeb714575c4475975b66a72b5b61e223
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-03 18:23:47 +0300

    try fix preg

--- runtime-light/stdlib/string/regex-functions.h
+++ runtime-light/stdlib/string/regex-functions.h
@@ -53,34 +53,40 @@ mixed f$preg_replace(const mixed &pattern, const mixed &replacement, const mixed
                      int64_t &count = RegexInstanceState::get().default_preg_replace_count) noexcept;
 
 Optional<string> f$preg_replace(const mixed &pattern, const mixed &replacement, const Optional<string> &subject, int64_t limit = PREG_REPLACE_NOLIMIT,
-                     int64_t &count = RegexInstanceState::get().default_preg_replace_count) noexcept;
+                                int64_t &count = RegexInstanceState::get().default_preg_replace_count) noexcept;
 
+template<class T1, class T2, class T3, class = enable_if_t_is_optional<T3>>
+auto f$preg_replace_callback(const T1 &regex, const T2 &replace_val, const T3 &subject, int64_t limit = -1, int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
+  return f$preg_replace_callback(regex, replace_val, subject.val(), limit, replace_count);
+}
 
 template<class T>
-Optional<string> f$preg_replace_callback(const regexp &, const T &, const string &, int64_t = -1,
-                                         int64_t & = RegexInstanceState::get().default_preg_replace_count) {
+Optional<string> f$preg_replace_callback(const regexp &regex, const T &replace_val, const string &subject, int64_t limit = -1,
+                                         int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
   php_critical_error("call to unsupported function");
 }
 
 template<class T>
-mixed f$preg_replace_callback(const regexp &, const T &, const mixed &, int64_t = -1, int64_t & = RegexInstanceState::get().default_preg_replace_count) {
+mixed f$preg_replace_callback(const regexp &regex, const T &replace_val, const mixed &subject, int64_t limit = -1,
+                              int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
   php_critical_error("call to unsupported function");
 }
 
 template<class T, class T2>
 auto f$preg_replace_callback(const string &regex, const T &replace_val, const T2 &subject, int64_t limit = -1,
                              int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
-  return f$preg_replace_callback(mixed(regex), replace_val, subject, limit, replace_count);
+  return f$preg_replace_callback(regexp(regex), replace_val, subject, limit, replace_count);
 }
 
 template<class T>
-Optional<string> f$preg_replace_callback(const mixed &, const T &, const string &, int64_t = -1,
-                                         int64_t & = RegexInstanceState::get().default_preg_replace_count) {
+Optional<string> f$preg_replace_callback(const mixed &regex, const T &replace_val, const string &subject, int64_t limit = -1,
+                                         int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
   php_critical_error("call to unsupported function");
 }
 
 template<class T>
-mixed f$preg_replace_callback(const mixed &, const T &, const mixed &, int64_t = -1, int64_t & = RegexInstanceState::get().default_preg_replace_count) {
+mixed f$preg_replace_callback(const mixed &regex, const T &replace_val, const mixed &subject, int64_t limit = -1,
+                              int64_t &replace_count = RegexInstanceState::get().default_preg_replace_count) {
   php_critical_error("call to unsupported function");
 }
 
