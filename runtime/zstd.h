08b1b4bb21a21af49e2a0a5b65a0303590771040
kojidev <hmu@koji.dev>
2024-12-03 19:12:32 +0300

    add zstd_stream_init, zstd_stream_add

--- runtime/zstd.h
+++ runtime/zstd.h
@@ -4,7 +4,10 @@
 
 #pragma once
 
+#include <zstd.h>
+
 #include "runtime-common/core/runtime-core.h"
+#include "runtime/dummy-visitor-methods.h"
 
 constexpr int DEFAULT_COMPRESS_LEVEL = 3;
 
@@ -15,3 +18,19 @@ Optional<string> f$zstd_uncompress(const string &data) noexcept;
 Optional<string> f$zstd_compress_dict(const string &data, const string &dict) noexcept;
 
 Optional<string> f$zstd_uncompress_dict(const string &data, const string &dict) noexcept;
+
+struct C$ZstdCtx : public refcountable_php_classes<C$ZstdCtx>, private DummyVisitorMethods {
+  C$ZstdCtx() = default;
+  using DummyVisitorMethods::accept;
+
+  ~C$ZstdCtx() {
+    dl::CriticalSectionGuard guard;
+    // TODO
+  }
+
+  ZSTD_CStream *ctx;
+};
+
+class_instance<C$ZstdCtx> f$zstd_stream_init(const array<mixed> &options = {}) noexcept;
+
+Optional<string> f$zstd_stream_add(const class_instance<C$ZstdCtx> &ctx, const string &data, bool end) noexcept;
