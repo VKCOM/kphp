f7a7e92c3a5f61818171fa055cfede4f9b58795d
Alexander Polyakov <al.polyakov@vk.team>
2024-12-05 19:20:54 +0300

    Revert redundant changes in url.cpp

--- runtime/url.cpp
+++ runtime/url.cpp
@@ -6,15 +6,15 @@
 
 string AMPERSAND("&", 1);
 
-// string f$base64_decode (const string &s) {
-//   int result_len = (s.size() + 3) / 4 * 3;
-//   string res (result_len, false);
-//   result_len = base64_decode (s.c_str(), reinterpret_cast <unsigned char *> (res.buffer()), result_len + 1);
+//string f$base64_decode (const string &s) {
+//  int result_len = (s.size() + 3) / 4 * 3;
+//  string res (result_len, false);
+//  result_len = base64_decode (s.c_str(), reinterpret_cast <unsigned char *> (res.buffer()), result_len + 1);
 //
-//   if (result_len < 0) {
-//     return string();
-//   }
+//  if (result_len < 0) {
+//    return string();
+//  }
 //
-//   res.shrink (result_len);
-//   return res;
-// }
+//  res.shrink (result_len);
+//  return res;
+//}
