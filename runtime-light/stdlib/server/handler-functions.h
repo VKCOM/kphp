32150d8f660f71166199c3697e05fce46f208245
Alexander Polyakov <al.polyakov@vk.team>
2024-12-08 13:25:00 +0300

    Add a bit of explanation to shutdown function task creation

--- runtime-light/stdlib/server/handler-functions.h
+++ runtime-light/stdlib/server/handler-functions.h
@@ -13,14 +13,17 @@
 
 template<typename F, typename... Args>
 requires(std::invocable<F, Args...>) void f$register_shutdown_function(F &&f, Args &&...args) noexcept {
-  // it's crucial that we don't use lambda capturing and accept parameters by value instead.
-  // the reason for that is coroutine's lifetime
-  auto shutdown_function_task{[](F f, Args... args) noexcept -> task_t<void> {
-    if constexpr (is_async_function_v<F, Args...>) {
-      co_await std::invoke(f, args...);
-    } else {
-      std::invoke(f, args...);
-    }
-  }(std::forward<F>(f), std::forward<Args>(args)...)};
+  // it's a lambda coroutine, so:
+  // 1. don't capture anything;
+  // 2. parameters are passed by value.
+  auto shutdown_function_task{std::invoke(
+    [](F f, Args... args) noexcept -> task_t<void> {
+      if constexpr (is_async_function_v<F, Args...>) {
+        co_await std::invoke(f, args...);
+      } else {
+        std::invoke(f, args...);
+      }
+    },
+    std::forward<F>(f), std::forward<Args>(args)...)};
   InstanceState::get().shutdown_functions.emplace_back(std::move(shutdown_function_task));
 }
