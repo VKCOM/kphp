c21b311bf84c615996eae00e54534c0bae0716ac
Alexander Polyakov <al.polyakov@vk.team>
2024-12-09 19:22:51 +0300

    Move serialize & unserialize builtins to runtime-common (#1180)

--- runtime-common/stdlib/serialization/serialize-functions.h
+++ runtime-common/stdlib/serialization/serialize-functions.h
@@ -6,7 +6,6 @@
 
 #include "common/mixin/not_copyable.h"
 #include "runtime-common/core/runtime-core.h"
-#include "runtime/context/runtime-context.h"
 
 namespace impl_ {
 
@@ -30,9 +29,10 @@ private:
 
 template<class T>
 void PhpSerializer::serialize(const array<T> &arr) noexcept {
-  kphp_runtime_context.static_SB.append("a:", 2);
-  kphp_runtime_context.static_SB << arr.count();
-  kphp_runtime_context.static_SB.append(":{", 2);
+  auto &static_SB{RuntimeContext::get().static_SB};
+  static_SB.append("a:", 2);
+  static_SB << arr.count();
+  static_SB.append(":{", 2);
   for (auto p : arr) {
     auto key = p.get_key();
     if (array<T>::is_int_key(key)) {
@@ -42,7 +42,7 @@ void PhpSerializer::serialize(const array<T> &arr) noexcept {
     }
     serialize(p.get_value());
   }
-  kphp_runtime_context.static_SB << '}';
+  static_SB << '}';
 }
 
 template<class T>
@@ -61,9 +61,10 @@ void PhpSerializer::serialize(const Optional<T> &opt) noexcept {
 
 template<class T>
 string f$serialize(const T &v) noexcept {
-  kphp_runtime_context.static_SB.clean();
+  auto &static_SB{RuntimeContext::get().static_SB};
+  static_SB.clean();
   impl_::PhpSerializer::serialize(v);
-  return kphp_runtime_context.static_SB.str();
+  return static_SB.str();
 }
 
 mixed f$unserialize(const string &v) noexcept;
--- runtime-common/stdlib/stdlib.cmake
+++ runtime-common/stdlib/stdlib.cmake
@@ -1,8 +1,9 @@
 prepend(STDLIB_ARRAY stdlib/array/ array-functions.cpp)
 prepend(STDLIB_SERIALIZATION stdlib/serialization/ json-functions.cpp
-                                                   json-writer.cpp)
+        json-writer.cpp serialize-functions.cpp)
 prepend(STDLIB_STRING stdlib/string/ mbstring-functions.cpp
-                                     string-functions.cpp)
+        string-functions.cpp)
 prepend(STDLIB_SERVER stdlib/server/ url-functions.cpp)
 
-set(STDLIB_SRC ${STDLIB_ARRAY} ${STDLIB_SERIALIZATION} ${STDLIB_STRING} ${STDLIB_SERVER})
+set(STDLIB_SRC ${STDLIB_ARRAY} ${STDLIB_SERIALIZATION} ${STDLIB_STRING}
+               ${STDLIB_SERVER})
