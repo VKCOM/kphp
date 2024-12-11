82550b76d38ae9f202e3b15512a0a767c5b44db0
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-11 16:16:09 +0300

    global rework

--- runtime-light/stdlib/file/udp-streams/udp-streams.cpp
+++ runtime-light/stdlib/file/udp-streams/udp-streams.cpp
@@ -5,7 +5,30 @@
 #include "runtime-light/stdlib/file/udp-streams/udp-streams.h"
 
 #include "runtime-light/state/instance-state.h"
+#include "runtime-light/streams/streams.h"
 
-std::pair<uint64_t, int32_t> connect_to_host_by_udp(const string &hostname) noexcept {
-  return InstanceState::get().connect_to(hostname, false);
+namespace resource_impl_ {
+
+task_t<int64_t> UdpResourceWrapper::write(const std::string_view text) {
+  if (stream_d == INVALID_PLATFORM_DESCRIPTOR) {
+    co_return 0;
+  }
+  co_return co_await write_all_to_stream(stream_d, text.data(), text.size());
+}
+
+void UdpResourceWrapper::close() {
+  InstanceState::get().release_stream(stream_d);
+  stream_d = INVALID_PLATFORM_DESCRIPTOR;
+}
+
+std::pair<class_instance<UdpResourceWrapper>, int32_t> connect_to_host_by_udp(const std::string_view scheme) {
+  const std::string_view url{scheme.substr(UDP_RESOURCE_PREFIX.size(), scheme.size() - UDP_RESOURCE_PREFIX.size())};
+  auto connection{InstanceState::get().connect_to(url, k2::udp_connect)};
+  class_instance<UdpResourceWrapper> wrapper;
+  if (connection.stream_d != INVALID_PLATFORM_DESCRIPTOR) {
+    wrapper.alloc();
+    wrapper.get()->stream_d = connection.stream_d;
+  }
+  return {wrapper, connection.error_code};
+}
 }
