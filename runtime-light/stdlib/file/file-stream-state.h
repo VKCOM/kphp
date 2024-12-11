63ccf11e18dc78f39bbe88e20aed4ce2a2aa88fb
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-11 19:08:19 +0300

    remove php streams context

--- runtime-light/stdlib/file/file-stream-state.h
+++ runtime-light/stdlib/file/file-stream-state.h
@@ -7,13 +7,15 @@
 #include "common/mixin/not_copyable.h"
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-light/stdlib/file/file-resource.h"
-#include "runtime-light/stdlib/file/php-streams/php-streams-context.h"
+#include "runtime-light/stdlib/file/php-streams/php-streams.h"
 
 struct FileStreamInstanceState final : private vk::not_copyable {
   mixed error_number_dummy;
   mixed error_description_dummy;
 
-  PhpStreamInstanceState php_stream_state{};
+  class_instance<resource_impl_::PhpResourceWrapper> stdin_wrapper;
+  class_instance<resource_impl_::PhpResourceWrapper> stdout_wrapper;
+  class_instance<resource_impl_::PhpResourceWrapper> stderr_wrapper;
 
   static FileStreamInstanceState &get() noexcept;
 };
