a90c531a2480663f67da01368d8cb1dac2290784
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-11 17:37:32 +0300

    small fix

--- runtime-light/stdlib/file/udp-streams/udp-streams.cpp
+++ runtime-light/stdlib/file/udp-streams/udp-streams.cpp
@@ -21,7 +21,7 @@ void UdpResourceWrapper::close() noexcept {
   stream_d = INVALID_PLATFORM_DESCRIPTOR;
 }
 
-std::pair<class_instance<UdpResourceWrapper>, int32_t> connect_to_host_by_udp(const std::string_view scheme) {
+std::pair<class_instance<UdpResourceWrapper>, int32_t> connect_to_host_by_udp(const std::string_view scheme) noexcept {
   const std::string_view url{scheme.substr(UDP_RESOURCE_PREFIX.size(), scheme.size() - UDP_RESOURCE_PREFIX.size())};
   auto connection{InstanceState::get().connect_to(url, k2::udp_connect)};
   class_instance<UdpResourceWrapper> wrapper;
