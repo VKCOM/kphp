0b390c3aa0216237cd243a45eb69ca3d8545644f
Alexander Polyakov <al.polyakov@vk.team>
2024-12-18 18:35:17 +0300

    temporary rename runtime-common's regex-functions.cpp -> regex-functions-common.cpp

--- runtime-common/stdlib/stdlib.cmake
+++ runtime-common/stdlib/stdlib.cmake
@@ -2,8 +2,8 @@ prepend(STDLIB_ARRAY stdlib/array/ array-functions.cpp)
 prepend(STDLIB_MATH stdlib/math/ math-functions.cpp)
 prepend(STDLIB_SERIALIZATION stdlib/serialization/ json-functions.cpp
         json-writer.cpp serialize-functions.cpp)
-prepend(STDLIB_STRING stdlib/string/ mbstring-functions.cpp regex-functions.cpp
-        string-functions.cpp)
+prepend(STDLIB_STRING stdlib/string/ mbstring-functions.cpp
+        regex-functions-common.cpp string-functions.cpp)
 prepend(STDLIB_SERVER stdlib/server/ url-functions.cpp)
 
 set(STDLIB_SRC ${STDLIB_ARRAY} ${STDLIB_MATH} ${STDLIB_SERIALIZATION}
