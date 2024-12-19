ed68d6edc1cda1ac6e7a7206ef1c4aa58f0785fb
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-19 19:01:53 +0300

    fix init

--- runtime-common/stdlib/math/math-context.cpp
+++ runtime-common/stdlib/math/math-context.cpp
@@ -16,16 +16,15 @@ MathLibConstants::MathLibConstants() noexcept
   : ONE(ONE_.data(), static_cast<string::size_type>(ONE_.size()))
   , ZERO(ZERO_.data(), static_cast<string::size_type>(ZERO_.size()))
   , TEN(TEN_.data(), static_cast<string::size_type>(TEN_.size()))
-  , ZERO_DOT_FIVE(ZERO_DOT_FIVE_.data(), static_cast<string::size_type>(ZERO_DOT_FIVE_.size())) {
+  , ZERO_DOT_FIVE(ZERO_DOT_FIVE_.data(), static_cast<string::size_type>(ZERO_DOT_FIVE_.size()))
+  , BC_NUM_ONE(bcmath_impl_::bc_parse_number(ONE).first)
+  , BC_NUM_ZERO(bcmath_impl_::bc_parse_number(ZERO).first)
+  , BC_ZERO_DOT_FIVE(bcmath_impl_::bc_parse_number(ZERO_DOT_FIVE).first) {
 
   ONE.set_reference_counter_to(ExtraRefCnt::for_global_const);
   ZERO.set_reference_counter_to(ExtraRefCnt::for_global_const);
   TEN.set_reference_counter_to(ExtraRefCnt::for_global_const);
   ZERO_DOT_FIVE.set_reference_counter_to(ExtraRefCnt::for_global_const);
-
-  BC_NUM_ONE = bcmath_impl_::bc_parse_number(ONE).first;
-  BC_NUM_ZERO = bcmath_impl_::bc_parse_number(ZERO).first;
-  BC_ZERO_DOT_FIVE = bcmath_impl_::bc_parse_number(ZERO_DOT_FIVE).first;
 
   BC_NUM_ONE.str.set_reference_counter_to(ExtraRefCnt::for_global_const);
   BC_NUM_ZERO.str.set_reference_counter_to(ExtraRefCnt::for_global_const);
