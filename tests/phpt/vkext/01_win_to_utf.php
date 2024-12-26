c4904e87354fb149d66661bc171dae9ded8ae72e
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-26 14:29:51 +0300

    fix tests

--- tests/phpt/vkext/01_win_to_utf.php
+++ tests/phpt/vkext/01_win_to_utf.php
@@ -8,19 +8,11 @@ $s4 = "\x74\x65\x73\x74";
 $s5 = "\x54\x45\x53\x54";
 $s6 = $s5;
 
-var_dump(vk_win_to_utf8($s1));
-var_dump(vk_win_to_utf8($s2));
-var_dump(vk_win_to_utf8($s3));
-var_dump(vk_win_to_utf8($s4));
-var_dump(vk_win_to_utf8($s5));
-var_dump(vk_win_to_utf8($s6));
+var_dump(vk_win_to_utf8($s1) === "");
+var_dump(vk_win_to_utf8($s2) === " ");
+var_dump(vk_win_to_utf8($s3) === "t");
+var_dump(vk_win_to_utf8($s4) === "test");
+var_dump(vk_win_to_utf8($s5) === "TEST");
+var_dump(vk_win_to_utf8($s6) === "TEST");
 
 ?>
-
---EXPECT--
-string(0) ""
-string(1) " "
-string(1) "t"
-string(4) "test"
-string(4) "TEST"
-string(4) "TEST"
--- tests/phpt/vkext/05_sp_to_lower.php
+++ tests/phpt/vkext/05_sp_to_lower.php
@@ -11,25 +11,14 @@ $s7 = 'AbCdeFG';
 $s8 = ' ';
 $s9 = $s7;
 
-var_dump(vk_sp_to_lower($s1));
-var_dump(vk_sp_to_lower($s2));
-var_dump(vk_sp_to_lower($s3));
-var_dump(vk_sp_to_lower($s4));
-var_dump(vk_sp_to_lower($s5));
-var_dump(vk_sp_to_lower($s6));
-var_dump(vk_sp_to_lower($s7));
-var_dump(vk_sp_to_lower($s8));
-var_dump(vk_sp_to_lower($s9));
+var_dump(vk_sp_to_lower($s1) === "");
+var_dump(vk_sp_to_lower($s2) === "a");
+var_dump(vk_sp_to_lower($s3) === "a");
+var_dump(vk_sp_to_lower($s4) === "abc");
+var_dump(vk_sp_to_lower($s5) === "abc");
+var_dump(vk_sp_to_lower($s6) === "abc");
+var_dump(vk_sp_to_lower($s7) === "abcdefg");
+var_dump(vk_sp_to_lower($s8) === " ");
+var_dump(vk_sp_to_lower($s9) === "abcdefg");
 
 ?>
-
---EXPECT--
-string(0) ""
-string(1) "a"
-string(1) "a"
-string(3) "abc"
-string(3) "abc"
-string(3) "abc"
-string(7) "abcdefg"
-string(1) " "
-string(7) "abcdefg"
