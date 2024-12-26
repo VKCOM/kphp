ef8cd3e2bb16ba0aacb714ef8a65eaf0c5016508
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-26 13:01:31 +0300

    fix test

--- tests/phpt/vkext/07_sp_deunicode.php
+++ tests/phpt/vkext/07_sp_deunicode.php
@@ -2,7 +2,7 @@
 <?php
 
 var_dump(vk_sp_deunicode('') === '');
-var_dump(vk_sp_deunicode('- "ïðèâåò, ýòî ÿ!"') === '- "ïðèâåò, ýòî ÿ!"');
+var_dump(vk_sp_deunicode('- "hello, it\'s me!"') === '- "hello, it\'s me!"');
 var_dump(vk_sp_deunicode('&#65;&#66;&#67;') === 'ABC');
 var_dump(vk_sp_deunicode('&#193;&#385;&#262;') === 'ABC');
 var_dump(vk_sp_deunicode('&#45;&#32;&#34;&#104;&#101;&#108;&#108;&#111;&#44;&#32;&#105;&#116;&#39;&#115;&#32;&#109;&#101;&#33;&#34;') === ('- "hello, it\'s me!"'));
