2b7a6f9afc893ec88dd78c99975dbc3769070c0b
Vadim Sadokhov <v.sadokhov@vk.team>
2024-12-27 16:22:01 +0300

    fix f$vk_flex

--- runtime/vkext.cpp
+++ runtime/vkext.cpp
@@ -8,17 +8,20 @@
 #include "flex/flex.h"
 #include "runtime/php_assert.h"
 
+#define BUFF_LEN (1 << 16)
+static char buff[BUFF_LEN];
+
 string f$vk_flex(const string &name, const string &case_name, int64_t sex, const string &type, int64_t lang_id) noexcept {
-  constexpr size_t BUFF_LEN = (1 << 16);
-  string buff{BUFF_LEN, false};
-  const size_t error_msg_buf_size = 1000;
-  static char ERROR_MSG_BUF[error_msg_buf_size] = {'\0'};
+  constexpr size_t ERROR_MSG_BUG_SIZE = 1000;
+  static char ERROR_MSG_BUF[ERROR_MSG_BUG_SIZE] = {'\0'};
   ERROR_MSG_BUF[0] = '\0';
-  vk::string_view ref = flex(vk::string_view{name.c_str(), name.size()}, vk::string_view{case_name.c_str(), case_name.size()}, sex == 1,
-                             vk::string_view{type.c_str(), type.size()}, lang_id, buff.buffer(), ERROR_MSG_BUF, error_msg_buf_size);
+  vk::string_view res = flex(vk::string_view{name.c_str(), name.size()}, vk::string_view{case_name.c_str(), case_name.size()}, sex == 1,
+                             vk::string_view{type.c_str(), type.size()}, lang_id, buff, ERROR_MSG_BUF, ERROR_MSG_BUG_SIZE);
   if (ERROR_MSG_BUF[0] != '\0') {
     php_warning("%s", ERROR_MSG_BUF);
   }
-  buff.shrink(ref.size());
-  return buff;
+  if (res.data() == name.c_str()) {
+    return name;
+  }
+  return string{res.data(), static_cast<string::size_type>(res.size())};
 }
