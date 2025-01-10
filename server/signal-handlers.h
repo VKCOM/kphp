9c2ca0c719c9ff19084ec74dae7727460c8acb21
Petr Shumilov <p.shumilov@vkteam.ru>
2025-01-10 17:02:21 +0300

    Replace __asan_handle_no_return into __asan_unpoison_memory_region (#1205)
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- server/signal-handlers.h
+++ server/signal-handlers.h
@@ -5,6 +5,9 @@
 #include "server/php-runner.h"
 #include "server/workers-control.h"
 
+// The size of buffer for alternative signal stack
+constexpr auto signal_stack_buffer_size = 65536;
+
 void perform_error_if_running(const char *msg, script_error_t error_type, const std::optional<int> &triggered_by_signal);
 
 void init_handlers();
