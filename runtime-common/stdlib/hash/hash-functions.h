a3c3ff2fa585013e12ec26aa86377cc05c5c692b
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2025-01-16 17:12:30 +0300

    Add pragma once

--- runtime-common/stdlib/hash/hash-functions.h
+++ runtime-common/stdlib/hash/hash-functions.h
@@ -2,6 +2,8 @@
 // Copyright (c) 2025 LLC «V Kontakte»
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
+#pragma once
+
 #include "runtime-common/core/runtime-core.h"
 
 bool f$hash_equals(const string &known_string, const string &user_string) noexcept;
