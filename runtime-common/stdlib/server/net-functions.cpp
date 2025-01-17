3afc997f5da907791afe30fc559ebdcbcfc5254f
Vadim Sadokhov <v.sadokhov@vk.team>
2025-01-17 17:01:44 +0300

    fix

--- runtime-common/stdlib/server/net-functions.cpp
+++ runtime-common/stdlib/server/net-functions.cpp
@@ -7,6 +7,10 @@
 #include <arpa/inet.h>
 #include <cstdio>
 
+namespace {
+constexpr int64_t IPV4_SIZE = 25;
+}
+
 Optional<int64_t> f$ip2long(const string &ip) noexcept {
   struct in_addr result;
   if (inet_pton(AF_INET, ip.c_str(), &result) != 1) {
@@ -21,15 +25,14 @@ Optional<string> f$ip2ulong(const string &ip) noexcept {
     return false;
   }
 
-  const size_t buf_size = 25;
-  char buf[buf_size];
-  int len = snprintf(buf, buf_size, "%u", ntohl(result.s_addr));
+  char buf[IPV4_SIZE];
+  int len = snprintf(buf, IPV4_SIZE, "%u", ntohl(result.s_addr));
   return string(buf, len);
 }
 
 string f$long2ip(int64_t num) noexcept {
   auto &runtime_context{RuntimeContext::get()};
-  runtime_context.static_SB.clean().reserve(100);
+  runtime_context.static_SB.clean().reserve(IPV4_SIZE);
   for (int i = 3; i >= 0; i--) {
     runtime_context.static_SB << ((num >> (i * 8)) & 255);
     if (i) {
