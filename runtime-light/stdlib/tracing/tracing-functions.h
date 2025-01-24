7c30064a34413ac2df13005c3ad72a11a3e63f67
Vadim Sadokhov <v.sadokhov@vk.team>
2025-01-24 14:58:55 +0300

    add tracing stubs

--- runtime-light/stdlib/tracing/tracing-functions.h
+++ runtime-light/stdlib/tracing/tracing-functions.h
@@ -5,19 +5,33 @@
 #pragma once
 
 #include "runtime-common/core/runtime-core.h"
+#include "runtime-light/stdlib/tracing/tracing-event.h"
+#include "runtime-light/stdlib/tracing/tracing-span.h"
 
-template <typename F>
-void f$kphp_tracing_register_on_finish(F &&cb_should_be_flushed) {
-  php_critical_error("call to unsupported function");
+struct C$KphpDiv;
+
+inline class_instance<C$KphpSpan> f$kphp_tracing_get_current_active_span() noexcept {
+  return {};
 }
 
-template <typename F>
-void f$kphp_tracing_register_enums_provider(F &&cb_custom_enums) {
-  php_critical_error("call to unsupported function");
+class_instance<C$KphpSpan> f$kphp_tracing_get_root_span() noexcept {
+  return {};
 }
 
-template <typename F1, typename F2>
-void f$kphp_tracing_register_rpc_details_provider(F1 &&cb_for_typed, F2 &&cb_for_untyped) {
-  php_critical_error("call to unsupported function");
+class_instance<C$KphpDiv> f$kphp_tracing_init([[maybe_unused]] const string &root_span_title) noexcept {
+  return {};
 }
 
+class_instance<C$KphpSpan> f$kphp_tracing_start_span([[maybe_unused]] const string &title, [[maybe_unused]] const string &short_desc,
+                                                     [[maybe_unused]] double start_timestamp) noexcept {
+  return {};
+}
+
+template<typename F>
+void f$kphp_tracing_register_enums_provider([[maybe_unused]] F &&cb_custom_enums) noexcept {}
+
+template<typename F>
+void f$kphp_tracing_register_on_finish([[maybe_unused]] F &&cb_should_be_flushed) noexcept {}
+
+template<typename F1, typename F2>
+void f$kphp_tracing_register_rpc_details_provider([[maybe_unused]] F1 &&cb_for_typed, [[maybe_unused]] F2 &&cb_for_untyped) noexcept {}
