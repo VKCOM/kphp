6ee3fbf1ca36efccdc561dcffd94fff89f30dea4
Vadim Sadokhov <v.sadokhov@vk.team>
2025-01-24 14:58:55 +0300

    add tracing stubs

---
+++ runtime-light/stdlib/tracing/tracing-span.h
@@ -0,0 +1,36 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2025 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include "runtime-common/core/runtime-core.h"
+#include "runtime-common/stdlib/visitors/common-visitors-methods.h"
+#include "runtime-light/stdlib/tracing/tracing-event.h"
+
+struct C$KphpSpan : public refcountable_php_classes<C$KphpSpan>, private CommonDefaultVisitorMethods {
+  using CommonDefaultVisitorMethods::accept;
+
+  int span_id{0};
+
+  C$KphpSpan() = default;
+  explicit C$KphpSpan(int span_id)
+    : span_id(span_id) {}
+};
+
+inline void f$KphpSpan$$addAttributeInt([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &key,
+                                        [[maybe_unused]] int64_t value) noexcept {}
+
+inline void f$KphpSpan$$addAttributeString([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &key,
+                                           [[maybe_unused]] const string &value) noexcept {}
+
+inline class_instance<C$KphpSpanEvent> f$KphpSpan$$addEvent([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &name,
+                                                            [[maybe_unused]] const Optional<double> &manual_timestamp = {}) noexcept {
+  return {};
+}
+
+inline void f$KphpSpan$$finish([[maybe_unused]] const class_instance<C$KphpSpan> &v$this,
+                               [[maybe_unused]] const Optional<double> &manual_timestamp = {}) noexcept {}
+
+inline void f$KphpSpan$$updateName([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &title,
+                                   [[maybe_unused]] const string &short_desc) noexcept {}
