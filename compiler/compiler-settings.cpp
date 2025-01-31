7e7b3460848e80a992cedeb3e3c70f75cd740c40
Petr Shumilov <p.shumilov@vkteam.ru>
2025-01-31 16:16:25 +0300

    Return the linking flex as system-installed lib by default
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- compiler/compiler-settings.cpp
+++ compiler/compiler-settings.cpp
@@ -417,8 +417,8 @@ void CompilerSettings::init() {
   append_if_doesnt_contain(ld_flags.value_, vk::to_array({"vk-flex-data"}), flex_prefix, ".a");
   system_installed_dynamic_libs.emplace_back("iconv");
 #else
-  append_3dparty_lib(ld_flags.value_, third_party_path, "vk-flex-data");
   system_installed_static_libs.emplace_back("numa");
+  system_installed_static_libs.emplace_back("vk-flex-data");
   append_if_doesnt_contain(ld_flags.value_, system_installed_static_libs, "-l:lib", ".a");
   system_installed_dynamic_libs.emplace_back("rt");
 #endif
