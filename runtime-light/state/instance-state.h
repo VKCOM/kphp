6a4b352fbd75c91b81803d3cd764dab55ba2b82f
Alexander Polyakov <al.polyakov@vk.team>
2025-01-31 17:43:52 +0300

    do not initialize StringLibContext::static_buf and StringLibContext::mask_buffer

--- runtime-light/state/instance-state.h
+++ runtime-light/state/instance-state.h
@@ -120,7 +120,7 @@ struct InstanceState final : vk::not_copyable {
   RegexInstanceState regex_instance_state;
   CurlInstanceState curl_instance_state{};
   CryptoInstanceState crypto_instance_state{};
-  StringInstanceState string_instance_state{};
+  StringInstanceState string_instance_state;
   SystemInstanceState system_instance_state{};
   FileSystemInstanceState file_system_instance_state{};
 
