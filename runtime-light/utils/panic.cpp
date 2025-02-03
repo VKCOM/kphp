3726be881271122a64f6c4b813a88f31e0f81af1
Alexander Polyakov <al.polyakov@vk.team>
2025-02-03 18:53:36 +0300

    use std::to_underlying instead of raw static_cast

--- runtime-light/utils/panic.cpp
+++ runtime-light/utils/panic.cpp
@@ -3,7 +3,7 @@
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
 #include <string_view>
-#include <type_traits>
+#include <utility>
 
 #include "runtime-common/core/utils/kphp-assert-core.h"
 #include "runtime-light/k2-platform/k2-api.h"
@@ -12,7 +12,7 @@
 
 void critical_error_handler() {
   constexpr std::string_view message = "script panic";
-  k2::log(static_cast<std::underlying_type_t<LogLevel>>(LogLevel::Debug), message.size(), message.data());
+  k2::log(std::to_underlying(LogLevel::Debug), message.size(), message.data());
 
   if (k2::instance_state() != nullptr) {
     InstanceState::get().poll_status = k2::PollStatus::PollFinishedError;
