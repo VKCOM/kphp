ed1875d4113a5b9983c90e87785361f82ba70dea
Vadim Sadokhov <v.sadokhov@vk.team>
2025-02-06 14:06:14 +0300

    remove less files

--- runtime-light/stdlib/tracing/tracing-functions.h
+++ runtime-light/stdlib/tracing/tracing-functions.h
@@ -5,9 +5,31 @@
 #pragma once
 
 #include "runtime-common/core/runtime-core.h"
+#include "runtime-light/stdlib/tracing/tracing-div.h"
 #include "runtime-light/stdlib/tracing/tracing-event.h"
 #include "runtime-light/stdlib/tracing/tracing-span.h"
-#include "runtime-light/stdlib/tracing/tracing-div.h"
+
+struct C$KphpSpanEvent : public refcountable_php_classes<C$KphpSpanEvent> {
+  int span_id{0};
+
+  C$KphpSpanEvent() = default;
+  explicit C$KphpSpanEvent(int span_id)
+    : span_id(span_id) {}
+};
+
+struct C$KphpDiv : public refcountable_php_classes<C$KphpDiv>, private CommonDefaultVisitorMethods {
+  using CommonDefaultVisitorMethods::accept;
+};
+
+struct C$KphpSpan : public refcountable_php_classes<C$KphpSpan>, private CommonDefaultVisitorMethods {
+  using CommonDefaultVisitorMethods::accept;
+
+  int span_id{0};
+
+  C$KphpSpan() = default;
+  explicit C$KphpSpan(int span_id)
+    : span_id(span_id) {}
+};
 
 inline class_instance<C$KphpSpan> f$kphp_tracing_get_current_active_span() noexcept {
   php_warning("called stub kphp_tracing_get_current_active_span");
@@ -25,7 +47,7 @@ inline class_instance<C$KphpDiv> f$kphp_tracing_init([[maybe_unused]] const stri
 }
 
 inline class_instance<C$KphpSpan> f$kphp_tracing_start_span([[maybe_unused]] const string &title, [[maybe_unused]] const string &short_desc,
-                                                     [[maybe_unused]] double start_timestamp) noexcept {
+                                                            [[maybe_unused]] double start_timestamp) noexcept {
   php_warning("called stub kphp_tracing_start_span");
   return {};
 }
@@ -43,4 +65,30 @@ void f$kphp_tracing_register_on_finish([[maybe_unused]] F &&cb_should_be_flushed
 template<typename F1, typename F2>
 void f$kphp_tracing_register_rpc_details_provider([[maybe_unused]] F1 &&cb_for_typed, [[maybe_unused]] F2 &&cb_for_untyped) noexcept {
   php_warning("called stub kphp_tracing_register_rpc_details_provider");
+}
+
+inline void f$KphpSpan$$addAttributeInt([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &key,
+                                        [[maybe_unused]] int64_t value) noexcept {
+  php_warning("called stub KphpSpan::addAttributeInt");
+}
+
+inline void f$KphpSpan$$addAttributeString([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &key,
+                                           [[maybe_unused]] const string &value) noexcept {
+  php_warning("called stub KphpSpan::addAttributeString");
+}
+
+inline class_instance<C$KphpSpanEvent> f$KphpSpan$$addEvent([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &name,
+                                                            [[maybe_unused]] const Optional<double> &manual_timestamp = {}) noexcept {
+  php_warning("called stub KphpSpan::addEvent");
+  return {};
+}
+
+inline void f$KphpSpan$$finish([[maybe_unused]] const class_instance<C$KphpSpan> &v$this,
+                               [[maybe_unused]] const Optional<double> &manual_timestamp = {}) noexcept {
+  php_warning("called stub KphpSpan::finish");
+}
+
+inline void f$KphpSpan$$updateName([[maybe_unused]] const class_instance<C$KphpSpan> &v$this, [[maybe_unused]] const string &title,
+                                   [[maybe_unused]] const string &short_desc) noexcept {
+  php_warning("called stub KphpSpan::updateName");
 }
