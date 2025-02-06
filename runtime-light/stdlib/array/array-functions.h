6f955620fdf67964f15f77f2b1309053a22479de
Vadim Sadokhov <v.sadokhov@vk.team>
2025-02-06 16:38:04 +0300

    small fix

--- runtime-light/stdlib/array/array-functions.h
+++ runtime-light/stdlib/array/array-functions.h
@@ -397,7 +397,7 @@ array<T> f$array_fill(int64_t start_index, int64_t num, const T &value) {
     result.fill_vector(num, value);
   } else {
     result.set_value(start_index, value);
-    while (--num > 0) {
+    for (int64_t i = num; i > 0; --i) {
       result.push_back(value);
     }
   }
