3fc1aefb2dec68e7a3d633a6ac1a233400d2432a
Petr Shumilov <p.shumilov@vkteam.ru>
2025-02-07 19:01:59 +0300

    Remove BUILD_IN_SOURCE to keep third-party directories clean
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- third-party/curl-cmake/curl.cmake
+++ third-party/curl-cmake/curl.cmake
@@ -1,7 +1,10 @@
 update_git_submodule(${THIRD_PARTY_DIR}/curl "--remote")
 
-set(CURL_INSTALL_DIR ${CMAKE_BINARY_DIR}/third-party/curl)
-# Ensure the installation directory exists
+set(CURL_SOURCE_DIR  ${THIRD_PARTY_DIR}/curl)
+set(CURL_BUILD_DIR   ${CMAKE_BINARY_DIR}/third-party/curl/build)
+set(CURL_INSTALL_DIR ${CMAKE_BINARY_DIR}/third-party/curl/install)
+# Ensure the build and installation directories exists
+file(MAKE_DIRECTORY ${CURL_BUILD_DIR})
 file(MAKE_DIRECTORY ${CURL_INSTALL_DIR})
 
 set(CURL_COMPILE_FLAGS "$ENV{CFLAGS} -g0 -Wno-deprecated-declarations")
@@ -15,10 +18,7 @@ endif()
 # The configuration has been based on:
 # https://github.com/VKCOM/curl/commit/00364cc6d672d9271032dbfbae3cfbc5e5f8542c
 # ./configure --prefix=/opt/curl7600 --without-librtmp --without-libssh2 --disable-ldap --disable-ldaps --disable-threaded-resolver --with-nghttp2 --enable-versioned-symbols
-ExternalProject_Add(
-        curl
-        SOURCE_DIR ${THIRD_PARTY_DIR}/curl
-        CMAKE_ARGS
+set(CURL_CMAKE_ARGS
         -DBUILD_TESTING=OFF
         -DCURL_WERROR=OFF                   # Recommend to enable when optimization level less than -O3
         -DBUILD_CURL_EXE=OFF
@@ -30,17 +30,30 @@ ExternalProject_Add(
         -DENABLE_THREADED_RESOLVER=OFF      # Disable threaded resolver.
         -DUSE_NGHTTP2=ON                    # Enable HTTP/2 support.
         -DCMAKE_USE_OPENSSL=ON
+        -DOPENSSL_FOUND=ON
         -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}
         -DOPENSSL_LIBRARIES=${OPENSSL_LIBRARIES}
-        -DCMAKE_INSTALL_PREFIX=${CURL_INSTALL_DIR}
+        -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR}
         -DCMAKE_C_FLAGS=${CURL_COMPILE_FLAGS}
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . --config $<CONFIG>
+        -DCURL_TEST=Off
+)
+
+ExternalProject_Add(
+        curl
+        DEPENDS openssl
+        PREFIX ${CURL_BUILD_DIR}
+        SOURCE_DIR ${CURL_SOURCE_DIR}
+        INSTALL_DIR ${CURL_INSTALL_DIR}
         BUILD_BYPRODUCTS ${CURL_INSTALL_DIR}/lib/libcurl.a
+        CONFIGURE_COMMAND
+            COMMAND ${CMAKE_COMMAND} ${CURL_CMAKE_ARGS} -S ${CURL_SOURCE_DIR} -B ${CURL_BUILD_DIR}
+        BUILD_COMMAND
+            COMMAND ${CMAKE_COMMAND} --build ${CURL_BUILD_DIR} --config $<CONFIG>
         INSTALL_COMMAND
-        ${CMAKE_COMMAND}  --install . --config $<CONFIG> &&
-        ${CMAKE_COMMAND} -E copy_directory ${CURL_INSTALL_DIR}/include ${OBJS_DIR}/include &&
-        ${CMAKE_COMMAND} -E copy ${CURL_INSTALL_DIR}/lib/libcurl.a ${OBJS_DIR}/lib/
-        BUILD_IN_SOURCE 1
+            COMMAND ${CMAKE_COMMAND} --install ${CURL_BUILD_DIR} --prefix ${CURL_INSTALL_DIR} --config $<CONFIG>
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CURL_INSTALL_DIR}/include ${INCLUDE_DIR}
+            COMMAND ${CMAKE_COMMAND} -E copy ${CURL_INSTALL_DIR}/lib/libcurl.a ${LIB_DIR}
+        BUILD_IN_SOURCE 0
 )
 
 set(CURL_INCLUDE_DIR ${CURL_INSTALL_DIR}/include)
@@ -56,3 +69,5 @@ set_target_properties(CURL::curl PROPERTIES
 # Set variables indicating that curl has been installed
 set(CURL_FOUND ON)
 set(CURL_LIBRARIES ${CURL_INSTALL_DIR}/lib/libcurl.a)
+
+cmake_print_variables(CURL_LIBRARIES)
--- third-party/openssl-cmake/openssl.cmake
+++ third-party/openssl-cmake/openssl.cmake
@@ -1,7 +1,10 @@
 update_git_submodule(${THIRD_PARTY_DIR}/openssl "--remote")
 
-set(OPENSSL_INSTALL_DIR ${CMAKE_BINARY_DIR}/third-party/openssl)
-# Ensure the installation directory exists
+set(OPENSSL_SOURCE_DIR  ${THIRD_PARTY_DIR}/openssl)
+set(OPENSSL_BUILD_DIR   ${CMAKE_BINARY_DIR}/third-party/openssl/build)
+set(OPENSSL_INSTALL_DIR ${CMAKE_BINARY_DIR}/third-party/openssl/install)
+# Ensure the build and installation directories exists
+file(MAKE_DIRECTORY ${OPENSSL_BUILD_DIR})
 file(MAKE_DIRECTORY ${OPENSSL_INSTALL_DIR})
 
 set(OPENSSL_COMPILE_FLAGS "$ENV{CFLAGS} -g0")
@@ -19,17 +22,24 @@ if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
     set(OPENSSL_CONFIGURE_EXTRA_OPTION enable-ec_nistp_64_gcc_128)
 endif()
 
-ExternalProject_Add(openssl
-        SOURCE_DIR ${THIRD_PARTY_DIR}/openssl
-        CONFIGURE_COMMAND env CFLAGS=${OPENSSL_COMPILE_FLAGS} ./config --prefix=${OPENSSL_INSTALL_DIR} --openssldir=/usr/lib/ssl no-shared no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms ${OPENSSL_CONFIGURE_EXTRA_OPTION}
+ExternalProject_Add(
+        openssl
+        PREFIX ${OPENSSL_BUILD_DIR}
+        SOURCE_DIR ${OPENSSL_SOURCE_DIR}
+        INSTALL_DIR ${OPENSSL_INSTALL_DIR}
+        BINARY_DIR ${OPENSSL_BUILD_DIR}
         BUILD_BYPRODUCTS ${OPENSSL_INSTALL_DIR}/lib/libssl.a ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a
-        BUILD_COMMAND env CFLAGS=${OPENSSL_COMPILE_FLAGS} make
+        CONFIGURE_COMMAND
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${OPENSSL_SOURCE_DIR} ${OPENSSL_BUILD_DIR}
+            COMMAND ./config --prefix=${OPENSSL_INSTALL_DIR} --openssldir=/usr/lib/ssl no-shared no-pic no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms no-tests ${OPENSSL_CONFIGURE_EXTRA_OPTION}
+        BUILD_COMMAND
+            COMMAND ${CMAKE_COMMAND} -E env CFLAGS=${OPENSSL_COMPILE_FLAGS} make
         INSTALL_COMMAND
-            make install_sw &&
-            ${CMAKE_COMMAND} -E copy_directory ${CMAKE_BINARY_DIR}/third-party/openssl/include/ ${OBJS_DIR}/include &&
-            ${CMAKE_COMMAND} -E copy ${OPENSSL_INSTALL_DIR}/lib/libssl.a ${OBJS_DIR}/lib/ &&
-            ${CMAKE_COMMAND} -E copy ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a ${OBJS_DIR}/lib/
-        BUILD_IN_SOURCE 1
+            COMMAND make install_sw
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${OPENSSL_INSTALL_DIR}/include ${INCLUDE_DIR}
+            COMMAND ${CMAKE_COMMAND} -E copy ${OPENSSL_INSTALL_DIR}/lib/libssl.a ${LIB_DIR}
+            COMMAND ${CMAKE_COMMAND} -E copy ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a ${LIB_DIR}
+        BUILD_IN_SOURCE 0
 )
 
 set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
@@ -58,4 +68,6 @@ set(OPENSSL_ROOT_DIR ${OPENSSL_INSTALL_DIR})
 set(OPENSSL_LIBRARIES ${OPENSSL_INSTALL_DIR}/lib/libssl.a ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a)
 set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a)
 set(OPENSSL_SSL_LIBRARY ${OPENSSL_INSTALL_DIR}/lib/libssl.a)
-set(OPENSSL_USE_STATIC_LIBS TRUE)
+set(OPENSSL_USE_STATIC_LIBS ON)
+
+cmake_print_variables(OPENSSL_LIBRARIES)
