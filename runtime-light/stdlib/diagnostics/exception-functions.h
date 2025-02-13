ccf07d414cdcd14ab6d4d31021d8f587837adc13
Alexander Polyakov <al.polyakov@vk.team>
2025-02-13 18:01:52 +0300

    add exception types and some functions to k2 runtime

--- runtime-light/stdlib/diagnostics/exception-functions.h
+++ runtime-light/stdlib/diagnostics/exception-functions.h
@@ -4,34 +4,70 @@
 
 #pragma once
 
-#include "runtime-common/core/runtime-core.h"
+#include <concepts>
+#include <cstdint>
 
-#define THROW_EXCEPTION(e) {php_critical_error("Exceptions unsupported");}
+#include "runtime-common/core/runtime-core.h"
+#include "runtime-light/stdlib/diagnostics/exception-types.h"
+
+#define THROW_EXCEPTION(e)                                                                                                                                     \
+  { php_critical_error("Exceptions unsupported"); }
 
 #define CHECK_EXCEPTION(action)
 
 #ifdef __clang__
-  #define TRY_CALL_RET_(x) x
+#define TRY_CALL_RET_(x) x
 #else
-  #define TRY_CALL_RET_(x) std::move(x)
+#define TRY_CALL_RET_(x) std::move(x)
 #endif
 
-#define TRY_CALL_(CallT, call, action) ({ \
-CallT x_tmp___ = (call);                \
-CHECK_EXCEPTION(action);                \
-TRY_CALL_RET_(x_tmp___);                \
-})
+#define TRY_CALL_(CallT, call, action)                                                                                                                         \
+  ({                                                                                                                                                           \
+    CallT x_tmp___ = (call);                                                                                                                                   \
+    CHECK_EXCEPTION(action);                                                                                                                                   \
+    TRY_CALL_RET_(x_tmp___);                                                                                                                                   \
+  })
 
-#define TRY_CALL_VOID_(call, action) ({(call); CHECK_EXCEPTION(action); void();})
+#define TRY_CALL_VOID_(call, action)                                                                                                                           \
+  ({                                                                                                                                                           \
+    (call);                                                                                                                                                    \
+    CHECK_EXCEPTION(action);                                                                                                                                   \
+    void();                                                                                                                                                    \
+  })
 
 #define TRY_CALL(CallT, ResT, call) TRY_CALL_(CallT, call, return (ResT()))
 #define TRY_CALL_VOID(ResT, call) TRY_CALL_VOID_(call, return (ResT()))
 
-#define TRY_CALL_EXIT(CallT, message, call) TRY_CALL_(CallT, call, php_critical_error (message))
-#define TRY_CALL_VOID_EXIT(message, call) TRY_CALL_VOID_(call, php_critical_error (message))
+#define TRY_CALL_EXIT(CallT, message, call) TRY_CALL_(CallT, call, php_critical_error(message))
+#define TRY_CALL_VOID_EXIT(message, call) TRY_CALL_VOID_(call, php_critical_error(message))
 
+// ================================================================================================
+
+namespace kphp::exceptions {
+
+template<std::derived_from<C$Throwable> T>
+class_instance<T> make_throwable(const string &file, int64_t line, int64_t code, const string &desc) noexcept {
+  auto ci{make_instance<T>()};
+
+  auto *ins_ptr{ci.get()};
+  ins_ptr->$file = file;
+  ins_ptr->$line = line;
+  ins_ptr->$code = code;
+  ins_ptr->$message = desc;
+  return ci;
+}
+
+} // namespace kphp::exceptions
+
+// ================================================================================================
 
 template<typename T>
-T f$_exception_set_location(const T &e, const string &file, int64_t line) {
-  php_critical_error("call to unsupported function");
+T f$_exception_set_location(const T &e, const string &file, int64_t line) noexcept {
+  e->$file = file;
+  e->$line = line;
+  return e;
+}
+
+inline Exception f$err(const string &file, int64_t line, const string &code, const string &desc = {}) noexcept {
+  return kphp::exceptions::make_throwable<C$Exception>(file, line, 0, (RuntimeContext::get().static_SB.clean() << "ERR_" << code << ": " << desc).str());
 }
