243b27698f253ddd5cf36d8e1e18658376be02a7
Vadim Sadokhov <v.sadokhov@vk.team>
2025-02-28 13:18:49 +0300

    fix array-functions

--- runtime-light/stdlib/array/array-functions.h
+++ runtime-light/stdlib/array/array-functions.h
@@ -419,7 +419,7 @@ requires(std::invocable<Comparator, T, T>) task_t<void> f$uasort(array<T> &a, Co
 
 template<class T, class Comparator>
 requires(std::invocable<Comparator, typename array<T>::key_type, typename array<T>::key_type>) task_t<void> f$uksort(array<T> &a, Comparator compare) {
-  if constexpr (is_async_function_v<Comparator, T, T>) {
+  if constexpr (is_async_function_v<Comparator, typename array<T>::key_type, typename array<T>::key_type>) {
     /* ATTENTION: temporary copy is necessary since functions is coroutine and sort is inplace */
     array<T> tmp{a};
     co_await array_functions_impl_::async_ksort<task_t<void>>(tmp, std::move(compare), false);
