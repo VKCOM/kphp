c688744d9ec2f45a3dc7e2e280e20bfe69252b6a
Petr Shumilov <p.shumilov@vkteam.ru>
2025-02-28 17:20:03 +0300

    Introduce pic and no-pic build variants for curl
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- third-party/curl-cmake/curl.cmake
+++ third-party/curl-cmake/curl.cmake
@@ -1,107 +1,124 @@
 update_git_submodule(${THIRD_PARTY_DIR}/curl "--remote")
+get_submodule_version(${THIRD_PARTY_DIR}/curl CURL_VERSION)
+get_submodule_remote_url(third-party/curl CURL_SOURCE_URL)
 
-set(CURL_SOURCE_DIR     ${THIRD_PARTY_DIR}/curl)
-set(CURL_BUILD_DIR      ${CMAKE_BINARY_DIR}/third-party/curl/build)
-set(CURL_INSTALL_DIR    ${CMAKE_BINARY_DIR}/third-party/curl/install)
-set(CURL_LIBRARIES      ${CURL_INSTALL_DIR}/lib/libcurl.a)
-set(CURL_INCLUDE_DIRS   ${CURL_INSTALL_DIR}/include)
-# Ensure the build, installation and "include" directories exists
-file(MAKE_DIRECTORY ${CURL_BUILD_DIR})
-file(MAKE_DIRECTORY ${CURL_INSTALL_DIR})
-file(MAKE_DIRECTORY ${CURL_INCLUDE_DIRS})
+set(PROJECT_GENERIC_NAME curl)
+set(PROJECT_GENERIC_NAMESPACE CURL)
+set(ARTIFACT_NAME libcurl)
 
-set(CURL_COMPILE_FLAGS "$ENV{CFLAGS} -g0 -fno-pic -Wno-deprecated-declarations")
-if(NOT APPLE)
-    set(CURL_COMPILE_FLAGS "${CURL_COMPILE_FLAGS} -static")
-endif()
+function(build_curl PIC_ENABLED)
+    make_third_party_configuration(${PIC_ENABLED} ${PROJECT_GENERIC_NAME} ${PROJECT_GENERIC_NAMESPACE} ${ARTIFACT_NAME}
+            project_name
+            target_name
+            extra_compile_flags
+            archive_name
+            pic_namespace
+            pic_lib_specifier
+    )
 
-# Suppress compiler-specific warnings caused by -O3
-if(COMPILER_CLANG)
-    set(CURL_COMPILE_FLAGS "${CURL_COMPILE_FLAGS} -Wno-string-plus-int")
-else()
-    set(CURL_COMPILE_FLAGS "${CURL_COMPILE_FLAGS} -Wno-stringop-truncation -Wno-maybe-uninitialized -Wno-stringop-overflow")
-endif()
+    set(source_dir      ${THIRD_PARTY_DIR}/${PROJECT_GENERIC_NAME})
+    set(build_dir       ${CMAKE_BINARY_DIR}/third-party/${project_name}/build)
+    set(install_dir     ${CMAKE_BINARY_DIR}/third-party/${project_name}/install)
+    set(include_dirs    ${install_dir}/include)
+    set(libraries       ${install_dir}/lib/${archive_name})
+    # Ensure the build, installation and "include" directories exists
+    file(MAKE_DIRECTORY ${build_dir})
+    file(MAKE_DIRECTORY ${install_dir})
+    file(MAKE_DIRECTORY ${include_dirs})
 
-# The configuration has been based on:
-# https://github.com/VKCOM/curl/commit/00364cc6d672d9271032dbfbae3cfbc5e5f8542c
-# ./configure --prefix=/opt/curl7600 --without-librtmp --without-libssh2 --disable-ldap --disable-ldaps --disable-threaded-resolver --with-nghttp2 --enable-versioned-symbols
-set(CURL_CMAKE_ARGS
-        -DCMAKE_C_FLAGS=${CURL_COMPILE_FLAGS}
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-        -DCMAKE_POSITION_INDEPENDENT_CODE=OFF
-        -DBUILD_TESTING=OFF
-        -DCURL_WERROR=ON                    # Recommend to enable when optimization level less than -O3
-        -DBUILD_CURL_EXE=OFF
-        -DCURL_STATICLIB=ON
-        -DUSE_LIBRTMP=OFF                   # Disable RTMP support.
-        -DCMAKE_USE_LIBSSH2=OFF             # Disable libssh2 support.
-        -DCURL_DISABLE_LDAP=ON              # Disable LDAP support.
-        -DCURL_DISABLE_LDAPS=ON             # Disable LDAPS support.
-        -DENABLE_THREADED_RESOLVER=OFF      # Disable threaded resolver.
-        -DUSE_NGHTTP2=ON                    # Enable HTTP/2 support.
-        -DCMAKE_USE_OPENSSL=ON
-        -DOPENSSL_FOUND=ON
-        -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}
-        -DOPENSSL_LIBRARIES=${OPENSSL_LIBRARIES}
-        -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR}
-        -DCURL_ZLIB=ON
-        -DZLIB_FOUND=ON
-        -DCURL_SPECIAL_LIBZ=ON
-        -DZLIB_ROOT=${ZLIB_NO_PIC_ROOT}
-        -DZLIB_LIBRARIES=${ZLIB_NO_PIC_LIBRARIES}
-        -DZLIB_INCLUDE_DIR=${ZLIB_NO_PIC_INCLUDE_DIRS}/zlib
-        -DNGHTTP2_FOUND=ON
-        -DNGHTTP2_ROOT=${NGHTTP2_ROOT}
-        -DNGHTTP2_LIBRARY=${NGHTTP2_LIBRARIES}
-        -DNGHTTP2_INCLUDE_DIR=${NGHTTP2_INCLUDE_DIRS}
-)
+    set(compile_flags "$ENV{CFLAGS} -g0 -Wno-deprecated-declarations ${extra_compile_flags}")
 
-ExternalProject_Add(
-        curl
-        DEPENDS openssl zlib-no-pic nghttp2
-        PREFIX ${CURL_BUILD_DIR}
-        SOURCE_DIR ${CURL_SOURCE_DIR}
-        INSTALL_DIR ${CURL_INSTALL_DIR}
-        BUILD_BYPRODUCTS ${CURL_INSTALL_DIR}/lib/libcurl.a
-        CONFIGURE_COMMAND
-            COMMAND ${CMAKE_COMMAND} ${CURL_CMAKE_ARGS} -S ${CURL_SOURCE_DIR} -B ${CURL_BUILD_DIR} -Wno-dev
-        BUILD_COMMAND
-            COMMAND ${CMAKE_COMMAND} --build ${CURL_BUILD_DIR} --config $<CONFIG> -j
-        INSTALL_COMMAND
-            COMMAND ${CMAKE_COMMAND} --install ${CURL_BUILD_DIR} --prefix ${CURL_INSTALL_DIR} --config $<CONFIG>
-            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CURL_INSTALL_DIR}/include ${INCLUDE_DIR}
-            COMMAND ${CMAKE_COMMAND} -E copy ${CURL_INSTALL_DIR}/lib/libcurl.a ${LIB_DIR}
-        BUILD_IN_SOURCE 0
-)
+    # Suppress compiler-specific warnings caused by -O3
+    if(COMPILER_CLANG)
+        set(compile_flags "${compile_flags} -Wno-string-plus-int")
+    else()
+        set(compile_flags "${compile_flags} -Wno-stringop-truncation -Wno-maybe-uninitialized -Wno-stringop-overflow")
+    endif()
 
-add_library(CURL::curl STATIC IMPORTED)
-set_target_properties(CURL::curl PROPERTIES
-        IMPORTED_LOCATION ${CURL_LIBRARIES}
-        INTERFACE_INCLUDE_DIRECTORIES ${CURL_INCLUDE_DIRS}
-)
+    message(STATUS "CURL Summary:
 
-add_dependencies(CURL::curl curl)
+        PIC enabled:    ${PIC_ENABLED}
+        Version:        ${CURL_VERSION}
+        Source:         ${CURL_SOURCE_URL}
+        Include dirs:   ${include_dirs}
+        Libraries:      ${libraries}
+        Target name:    ${target_name}
+        Compiler:
+          C compiler:   ${CMAKE_C_COMPILER}
+          CFLAGS:       ${compile_flags}
+    ")
 
+    # The configuration has been based on:
+    # https://github.com/VKCOM/curl/commit/00364cc6d672d9271032dbfbae3cfbc5e5f8542c
+    # ./configure --prefix=/opt/curl7600 --without-librtmp --without-libssh2 --disable-ldap --disable-ldaps --disable-threaded-resolver --with-nghttp2 --enable-versioned-symbols
+    set(cmake_args
+            -DCMAKE_C_FLAGS=${compile_flags}
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DCMAKE_POSITION_INDEPENDENT_CODE=${PIC_ENABLED}
+            -DBUILD_TESTING=OFF
+            -DCURL_WERROR=ON                    # Recommend to enable when optimization level less than -O3
+            -DBUILD_CURL_EXE=OFF
+            -DCURL_STATICLIB=ON
+            -DUSE_LIBRTMP=OFF                   # Disable RTMP support.
+            -DCMAKE_USE_LIBSSH2=OFF             # Disable libssh2 support.
+            -DCURL_DISABLE_LDAP=ON              # Disable LDAP support.
+            -DCURL_DISABLE_LDAPS=ON             # Disable LDAPS support.
+            -DENABLE_THREADED_RESOLVER=OFF      # Disable threaded resolver.
+            -DUSE_NGHTTP2=ON                    # Enable HTTP/2 support.
+            -DCMAKE_USE_OPENSSL=ON
+            -DOPENSSL_FOUND=ON
+            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}
+            -DOPENSSL_LIBRARIES=${OPENSSL_LIBRARIES}
+            -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR}
+            -DCURL_ZLIB=ON
+            -DZLIB_FOUND=ON
+            -DCURL_SPECIAL_LIBZ=ON
+            -DZLIB_ROOT=${ZLIB_${pic_lib_specifier}_ROOT}
+            -DZLIB_LIBRARIES=${ZLIB_${pic_lib_specifier}_LIBRARIES}
+            -DZLIB_INCLUDE_DIR=${ZLIB_${pic_lib_specifier}_INCLUDE_DIRS}/zlib
+            -DNGHTTP2_FOUND=ON
+            -DNGHTTP2_ROOT=${NGHTTP2_${pic_lib_specifier}_ROOT}
+            -DNGHTTP2_LIBRARY=${NGHTTP2_${pic_lib_specifier}_LIBRARIES}
+            -DNGHTTP2_INCLUDE_DIR=${NGHTTP2_${pic_lib_specifier}_INCLUDE_DIRS}
+    )
 
-##############################
-add_library(CURL::pic::curl STATIC IMPORTED)
-set_target_properties(CURL::pic::curl PROPERTIES
-        IMPORTED_LOCATION ${CURL_LIBRARIES}
-        INTERFACE_INCLUDE_DIRECTORIES ${CURL_INCLUDE_DIRS}
-)
+    ExternalProject_Add(
+            ${project_name}
+            DEPENDS openssl ZLIB::${pic_namespace}::zlib NGHTTP2::${pic_namespace}::nghttp2
+            PREFIX ${build_dir}
+            SOURCE_DIR ${source_dir}
+            INSTALL_DIR ${install_dir}
+            BUILD_BYPRODUCTS ${libraries}
+            CONFIGURE_COMMAND
+                COMMAND ${CMAKE_COMMAND} ${cmake_args} -S ${source_dir} -B ${build_dir} -Wno-dev
+            BUILD_COMMAND
+                COMMAND ${CMAKE_COMMAND} --build ${build_dir} --config $<CONFIG> -j
+            INSTALL_COMMAND
+                COMMAND ${CMAKE_COMMAND} --install ${build_dir} --prefix ${install_dir} --config $<CONFIG>
+                COMMAND ${CMAKE_COMMAND} -E copy ${install_dir}/lib/${ARTIFACT_NAME}.a ${libraries}
+                COMMAND ${CMAKE_COMMAND} -E copy ${libraries} ${LIB_DIR}
+                COMMAND ${CMAKE_COMMAND} -E copy_directory ${include_dirs} ${INCLUDE_DIR}
+            BUILD_IN_SOURCE 0
+    )
 
-add_dependencies(CURL::pic::curl curl)
+    add_library(${target_name} STATIC IMPORTED)
+    set_target_properties(${target_name} PROPERTIES
+            IMPORTED_LOCATION ${libraries}
+            INTERFACE_INCLUDE_DIRECTORIES ${include_dirs}
+    )
 
-add_library(CURL::no-pic::curl STATIC IMPORTED)
-set_target_properties(CURL::no-pic::curl PROPERTIES
-        IMPORTED_LOCATION ${CURL_LIBRARIES}
-        INTERFACE_INCLUDE_DIRECTORIES ${CURL_INCLUDE_DIRS}
-)
+    # Ensure that the curl are built before they are used
+    add_dependencies(${target_name} ${project_name})
 
-add_dependencies(CURL::no-pic::curl curl)
-###############################
+    # Set variables indicating that curl has been installed
+    set(${PROJECT_GENERIC_NAMESPACE}_${pic_lib_specifier}_ROOT ${install_dir} PARENT_SCOPE)
+    set(${PROJECT_GENERIC_NAMESPACE}_${pic_lib_specifier}_INCLUDE_DIRS ${include_dirs} PARENT_SCOPE)
+    set(${PROJECT_GENERIC_NAMESPACE}_${pic_lib_specifier}_LIBRARIES ${libraries} PARENT_SCOPE)
+endfunction()
 
-# Set variables indicating that curl has been installed
+# PIC is OFF
+build_curl(OFF)
+# PIC is ON
+build_curl(ON)
+
 set(CURL_FOUND ON)
-
-cmake_print_variables(CURL_LIBRARIES)
