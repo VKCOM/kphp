6de22faf82016a0d84cda6611b47798d8703b287
Vadim Sadokhov <v.sadokhov@vk.team>
2025-02-28 12:59:52 +0300

    fix jw visitor

--- runtime-light/stdlib/job-worker/job-worker.h
+++ runtime-light/stdlib/job-worker/job-worker.h
@@ -11,17 +11,19 @@
 
 #include "runtime-common/core/class-instance/refcountable-php-classes.h"
 #include "runtime-common/core/runtime-core.h"
+#include "runtime-common/stdlib/visitors/memory-visitors.h"
+#include "runtime-light/stdlib/visitors/array-visitors.h"
 
 inline constexpr int64_t JOB_WORKER_VALID_JOB_ID_RANGE_START = 0;
 inline constexpr int64_t JOB_WORKER_INVALID_JOB_ID = -1;
-
-class ToArrayVisitor;
 
 namespace job_worker_impl_ {
 
 struct SendableBase : virtual abstract_refcountable_php_interface {
 
   virtual void accept(ToArrayVisitor & /*unused*/) noexcept {}
+
+  virtual void accept(CommonMemoryEstimateVisitor & /*unused*/) noexcept {}
 
   virtual const char *get_class() const noexcept = 0;
   virtual int32_t get_hash() const noexcept = 0;
