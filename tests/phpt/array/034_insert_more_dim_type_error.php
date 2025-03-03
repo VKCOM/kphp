f70007e5c94363e3912fae9ee13273925ef704a9
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2025-03-03 17:31:09 +0300

    Fire error for T[] inserting T[]

---
+++ tests/phpt/array/034_insert_more_dim_type_error.php
@@ -0,0 +1,15 @@
+@kphp_should_fail
+/insert int\[\]\[\] into \$y\[\]/
+/but it's declared as @var int/
+<?php
+
+function main() {
+    /** @var int[] */
+    $y = [];
+    
+    /** @var int[][]*/
+    $x = [];
+    $y[0] = $x;
+}
+
+main();
