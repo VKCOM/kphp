61bf0c9aa8861caff834c53d898992410c5654aa
Vadim Sadokhov <v.sadokhov@vk.team>
2025-03-03 14:47:01 +0300

    add f$strtotime stub

--- runtime-light/stdlib/time/time-functions.h
+++ runtime-light/stdlib/time/time-functions.h
@@ -70,3 +70,12 @@ string f$gmdate(const string &format, Optional<int64_t> timestamp = {}) noexcept
 string f$date(const string &format, Optional<int64_t> timestamp = {}) noexcept;
 
 bool f$date_default_timezone_set(const string &s) noexcept;
+
+inline Optional<int64_t> f$strtotime(const string &time_str, [[maybe_unused]] int64_t timestamp = std::numeric_limits<int64_t>::min()) {
+  if (time_str == string("2021-09-01 00:00:00")) {
+    return 1630443600;
+  } else {
+    php_warning("strtotime called %s", time_str.c_str());
+    return false;
+  }
+}
