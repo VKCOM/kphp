573f35bf189eff95f4f1a19b5c98cd918478231a
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2025-04-18 14:39:40 +0300

    Small improvements

--- runtime-light/stdlib/instance-cache/instance-cache.h
+++ runtime-light/stdlib/instance-cache/instance-cache.h
@@ -17,9 +17,6 @@
 #include "runtime-light/tl/tl-functions.h"
 #include "runtime-light/tl/tl-types.h"
 
-// TODO:
-// * Better warnings
-// * Better return value
 template<typename ClassInstanceType>
 kphp::coro::task<bool> f$instance_cache_store(const string& key, const ClassInstanceType& instance, int64_t ttl = 0) noexcept {
   constexpr std::string_view IC_COMPONENT_NAME = "instance_cache";
@@ -32,8 +29,8 @@ kphp::coro::task<bool> f$instance_cache_store(const string& key, const ClassInst
   }
 
   if (ttl > std::numeric_limits<uint32_t>::max()) [[unlikely]] {
-    php_warning("ttl is too big");
-    co_return false;
+    php_warning("ttl is too big, will store forever");
+    ttl = 0;
   }
 
   auto tl_key = tl::string(std::string_view{key.c_str(), key.size()});
@@ -50,9 +47,16 @@ kphp::coro::task<bool> f$instance_cache_store(const string& key, const ClassInst
   auto query = f$component_client_send_request(string{IC_COMPONENT_NAME.data(), static_cast<string::size_type>(IC_COMPONENT_NAME.size())},
                                                string{buffer.data(), static_cast<string::size_type>(buffer.size())});
 
-  auto _ = co_await f$component_client_fetch_response(co_await query);
+  auto resp = co_await f$component_client_fetch_response(co_await query);
+  buffer.clean();
+  buffer.store_bytes({resp.c_str(), static_cast<size_t>(resp.size())});
+  tl::Bool tl_resp;
+  if (!tl_resp.fetch(buffer)) [[unlikely]] {
+    php_warning("incorrect instance_cache_store() response");
+    co_return false;
+  }
 
-  co_return true;
+  co_return tl_resp.value;
 }
 
 template<typename ClassInstanceType>
@@ -72,12 +76,11 @@ kphp::coro::task<ClassInstanceType> f$instance_cache_fetch(const string& /*class
   buffer.store_bytes({resp.c_str(), static_cast<size_t>(resp.size())});
 
   tl::Maybe<tl::string> tl_resp;
-  if (!tl_resp.fetch(buffer)) {
-    php_notice("Wrong response for key \"%s\"", key.c_str());
+  if (!tl_resp.fetch(buffer)) [[unlikely]] {
+    php_warning("incorrect instance_cache_fetch() response");
     co_return ClassInstanceType{};
   }
   if (!tl_resp.opt_value.has_value()) {
-    php_notice("Key \"%s\" is not in cache", key.c_str());
     co_return ClassInstanceType{};
   }
   co_return f$instance_deserialize<ClassInstanceType>(string(tl_resp.opt_value->value.data(), tl_resp.opt_value->value.size()), {});
@@ -91,8 +94,8 @@ inline kphp::coro::task<bool> f$instance_cache_update_ttl(const string& key, int
   }
 
   if (ttl > std::numeric_limits<uint32_t>::max()) [[unlikely]] {
-    php_warning("ttl is too big");
-    co_return false;
+    php_warning("ttl is too big, will store forever");
+    ttl = 0;
   }
   tl::TLBuffer buffer;
   auto tl_key = tl::string(std::string_view{key.c_str(), key.size()});
@@ -103,9 +106,15 @@ inline kphp::coro::task<bool> f$instance_cache_update_ttl(const string& key, int
                                                string{buffer.data(), static_cast<string::size_type>(buffer.size())});
 
   auto resp = co_await f$component_client_fetch_response(co_await query);
-  // TODO change api to better return value
+  buffer.clean();
+  buffer.store_bytes({resp.c_str(), static_cast<size_t>(resp.size())});
+  tl::Bool tl_resp;
+  if (!tl_resp.fetch(buffer)) [[unlikely]] {
+    php_warning("incorrect instance_cache_update_ttl() response");
+    co_return false;
+  }
 
-  co_return true;
+  co_return tl_resp.value;
 }
 
 inline kphp::coro::task<bool> f$instance_cache_delete(const string& key) noexcept {
@@ -119,7 +128,13 @@ inline kphp::coro::task<bool> f$instance_cache_delete(const string& key) noexcep
                                                string{buffer.data(), static_cast<string::size_type>(buffer.size())});
 
   auto resp = co_await f$component_client_fetch_response(co_await query);
-  // TODO change api to better return value
+  buffer.clean();
+  buffer.store_bytes({resp.c_str(), static_cast<size_t>(resp.size())});
+  tl::Bool tl_resp;
+  if (!tl_resp.fetch(buffer)) [[unlikely]] {
+    php_warning("incorrect instance_cache_delete() response");
+    co_return false;
+  }
 
-  co_return true;
+  co_return tl_resp.value;
 }
