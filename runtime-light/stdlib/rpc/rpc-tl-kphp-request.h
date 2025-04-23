52c278e5d3788618e2e4fcb7458ff323c918b25e
Alexander Polyakov <al.polyakov@vk.team>
2025-04-23 12:02:42 +0300

    fix implementation of typed rpc request store

--- runtime-light/stdlib/rpc/rpc-tl-kphp-request.h
+++ runtime-light/stdlib/rpc/rpc-tl-kphp-request.h
@@ -5,29 +5,34 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
+#include "common/containers/final_action.h"
+#include "runtime-common/core/utils/kphp-assert-core.h"
 #include "runtime-light/allocator/allocator.h"
+#include "runtime-light/stdlib/diagnostics/exception-functions.h"
+#include "runtime-light/stdlib/fork/fork-state.h" // it's actually used by exception handling stuff
 #include "runtime-light/stdlib/rpc/rpc-state.h"
 #include "runtime-light/stdlib/rpc/rpc-tl-defs.h"
 #include "runtime-light/stdlib/rpc/rpc-tl-request.h"
 
-namespace tl_rpc_request_impl_ {
+namespace kphp::rpc::rpc_impl {
 // use template, because t_ReqResult_ is unknown on runtime compilation
 template<template<typename, uint32_t> class t_ReqResult_>
 class KphpRpcRequestResult final : public RpcRequestResult {
 public:
   using RpcRequestResult::RpcRequestResult;
 
-  explicit KphpRpcRequestResult(std::unique_ptr<tl_func_base>&& result_fetcher)
+  explicit KphpRpcRequestResult(std::unique_ptr<tl_func_base>&& result_fetcher) noexcept
       : RpcRequestResult(true, std::move(result_fetcher)) {}
 
-  class_instance<C$VK$TL$RpcResponse> fetch_typed_response() final {
+  class_instance<C$VK$TL$RpcResponse> fetch_typed_response() noexcept final {
     class_instance<C$VK$TL$RpcResponse> $response;
     t_ReqResult_<tl_exclamation_fetch_wrapper, 0>(tl_exclamation_fetch_wrapper(std::move(result_fetcher))).typed_fetch_to($response);
     return $response;
   }
 
-  std::unique_ptr<tl_func_base> extract_untyped_fetcher() final {
+  std::unique_ptr<tl_func_base> extract_untyped_fetcher() noexcept final {
     php_assert(!"Forbidden to call for typed rpc requests");
   }
 };
@@ -38,23 +43,20 @@ class KphpRpcRequest final : public RpcRequest {
 public:
   using RpcRequest::RpcRequest;
 
-  std::unique_ptr<RpcRequestResult> store_request() const final {
-    //    php_assert(CurException.is_null());
-    auto& rpc_client_instance_st{RpcClientInstanceState::get()};
-    rpc_client_instance_st.current_query.set_current_tl_function(tl_function_name());
-    std::unique_ptr<tl_func_base> stored_fetcher = storing_function.get()->store();
-    rpc_client_instance_st.current_query.reset();
-    //    if (!CurException.is_null()) {
-    //      CurException = Optional<bool>{};
-    //      return {};
-    //    }
+  std::unique_ptr<RpcRequestResult> store_request() const noexcept final {
+    CHECK_EXCEPTION(php_assert(false));
+    auto& cur_query{RpcClientInstanceState::get().current_query};
+    cur_query.set_current_tl_function(tl_function_name());
+    const vk::final_action finalizer{[&cur_query] noexcept { cur_query.reset(); }};
 
+    std::unique_ptr<tl_func_base> stored_fetcher{storing_function.get()->store()};
+    CHECK_EXCEPTION(return {});
     return make_unique_on_script_memory<KphpRpcRequestResult<t_ReqResult_>>(std::move(stored_fetcher));
   }
 };
-} // namespace tl_rpc_request_impl_
+} // namespace kphp::rpc::rpc_impl
 
 template<class T0, uint32_t inner_magic0>
 struct t_ReqResult; // the definition appears after the TL scheme codegen, during the site build
 
-using KphpRpcRequest = tl_rpc_request_impl_::KphpRpcRequest<t_ReqResult>;
+using KphpRpcRequest = kphp::rpc::rpc_impl::KphpRpcRequest<t_ReqResult>;
