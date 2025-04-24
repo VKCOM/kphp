0f3aaffb14f3a5acac56c9c260f2bce56269c6ce
Alexander Polyakov <al.polyakov@vk.team>
2025-04-24 17:14:00 +0300

    add rpc init-functions to cmake

--- runtime-light/server/server.cmake
+++ runtime-light/server/server.cmake
@@ -1,3 +1,8 @@
-prepend(RUNTIME_LIGHT_SERVER_SRC server/ http/init-functions.cpp
-        http/http-server-state.cpp job-worker/job-worker-server-state.cpp
-        rpc/rpc-server-state.cpp)
+prepend(
+  RUNTIME_LIGHT_SERVER_SRC
+  server/
+  http/init-functions.cpp
+  http/http-server-state.cpp
+  job-worker/job-worker-server-state.cpp
+  rpc/init-functions.cpp
+  rpc/rpc-server-state.cpp)
