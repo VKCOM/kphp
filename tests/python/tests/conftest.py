2c4d8341636a66cb361688cc8fef8def5819a020
Alexander Polyakov <al.polyakov@vk.team>
2025-05-01 23:40:21 +0300

    add kphp_skip decorators to skip RPC server tests in KPHP mode

--- tests/python/tests/conftest.py
+++ tests/python/tests/conftest.py
@@ -20,3 +20,20 @@ def skip_k2_unsupported_test_suite(request):
             request.cls.custom_setup = lambda: None
             request.cls.custom_teardown = lambda: None
             pytest.skip("K2 skipped test")
+
+@pytest.fixture(autouse=True)
+def skip_kphp_unsupported_test(request):
+    if search_k2_bin() is None:
+        kphp_skip_mark = request.node.get_closest_marker("kphp_skip")
+        if kphp_skip_mark:
+            pytest.skip("KPHP skipped test")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def skip_kphp_unsupported_test_suite(request):
+    if search_k2_bin() is None:
+        kphp_skip_mark = request.node.get_closest_marker("kphp_skip_suite")
+        if kphp_skip_mark:
+            request.cls.custom_setup = lambda: None
+            request.cls.custom_teardown = lambda: None
+            pytest.skip("KPHP skipped test")
