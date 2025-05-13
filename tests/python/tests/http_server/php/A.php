09f55abf06fa32c322fd214ea2217cf552238459
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2025-05-13 17:41:45 +0300

    Fix test

--- tests/python/tests/http_server/php/A.php
+++ tests/python/tests/http_server/php/A.php
@@ -1,9 +1,15 @@
 <?php
 
-/** @kphp-immutable-class */
+/** @kphp-immutable-class
+ *  @kphp-serializable
+*/
 class A {
-  /** @var int */
+  /** @var int
+   *  @kphp-serialized-field 0
+  */
   public $a = 42;
-  /** @var string */
+  /** @var string
+   *  @kphp-serialized-field 1
+  */
   public $b = "hello";
 }
