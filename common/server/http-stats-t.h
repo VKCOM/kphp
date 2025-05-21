1b8274258e57e50b0fad07d58df651fa0d0b208d
Danil Ovchinnikov <danil42worker@gmail.com>
2025-05-21 19:59:37 +0300

    added the time to the metrics

--- common/server/http-stats-t.h
+++ common/server/http-stats-t.h
@@ -8,6 +8,10 @@
 
 class http_stats_t : public stats_t {
 public:
+  http_stats_t() noexcept {
+    now_time = time(nullptr);
+  }
+
   void add_general_stat([[maybe_unused]] const char* key, [[maybe_unused]] const char* value_format, ...) noexcept final {
     // ignore
   }
@@ -15,28 +19,33 @@ public:
 protected:
   void add_stat([[maybe_unused]] char type, const char* key, double value) noexcept final {
     sb_printf(&sb, "%s%s\t", normalize_key(key, "%s", ""), stats_suffix);
-    sb_printf(&sb, "%.3f", value);
+    sb_printf(&sb, "%.3f\t", value);
+    sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
   void add_stat([[maybe_unused]] char type, const char* key, long long value) noexcept final {
     sb_printf(&sb, "%s%s\t", normalize_key(key, "%s", ""), stats_suffix);
-    sb_printf(&sb, "%lld", value);
+    sb_printf(&sb, "%lld\t", value);
+    sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
   void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, double value) noexcept override {
     sb_printf(&sb, "%s_%s%s\t", normalize_key(key, "%s", ""), type_tag, stats_suffix);
-    sb_printf(&sb, "%.3f", value);
+    sb_printf(&sb, "%.3f\t", value);
+    sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
   void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, long long int value) noexcept override {
     sb_printf(&sb, "%s_%s%s\t", normalize_key(key, "%s", ""), type_tag, stats_suffix);
-    sb_printf(&sb, "%lld", value);
+    sb_printf(&sb, "%lld\t", value);
+    sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
 private:
   const char* stats_suffix{"{family=\"gauges\",dimension=\"gauge\"}"};
+  time_t now_time;
 };
