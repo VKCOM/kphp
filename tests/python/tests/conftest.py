1ad3ce206679d51f740ac11bb418d0d31cb15284
Vadim Sadokhov <v.sadokhov@vk.team>
2025-05-21 12:53:28 +0300

    move conftest impl to lib

--- tests/python/tests/conftest.py
+++ tests/python/tests/conftest.py
@@ -1,39 +1,4 @@
 import os
 import pytest
 
-from python.lib.file_utils import search_k2_bin
-
-
-@pytest.fixture(autouse=True)
-def skip_k2_unsupported_test(request):
-    if search_k2_bin() is not None:
-        k2_skip_mark = request.node.get_closest_marker("k2_skip")
-        if k2_skip_mark:
-            pytest.skip("K2 skipped test")
-
-
-@pytest.fixture(scope="class", autouse=True)
-def skip_k2_unsupported_test_suite(request):
-    if search_k2_bin() is not None:
-        k2_skip_mark = request.node.get_closest_marker("k2_skip_suite")
-        if k2_skip_mark:
-            request.cls.custom_setup = lambda: None
-            request.cls.custom_teardown = lambda: None
-            pytest.skip("K2 skipped test")
-
-@pytest.fixture(autouse=True)
-def skip_kphp_unsupported_test(request):
-    if search_k2_bin() is None:
-        kphp_skip_mark = request.node.get_closest_marker("kphp_skip")
-        if kphp_skip_mark:
-            pytest.skip("KPHP skipped test")
-
-
-@pytest.fixture(scope="class", autouse=True)
-def skip_kphp_unsupported_test_suite(request):
-    if search_k2_bin() is None:
-        kphp_skip_mark = request.node.get_closest_marker("kphp_skip_suite")
-        if kphp_skip_mark:
-            request.cls.custom_setup = lambda: None
-            request.cls.custom_teardown = lambda: None
-            pytest.skip("KPHP skipped test")
+from python.lib.conftest_impl import skip_k2_unsupported_test, skip_k2_unsupported_test_suite
