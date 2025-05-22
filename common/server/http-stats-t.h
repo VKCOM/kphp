2cebfc740b8189c4bf40342e7a458ea5224c741e
Danil Ovchinnikov <danil42worker@gmail.com>
2025-05-22 06:18:08 +0300

    small refactoring

--- common/server/http-stats-t.h
+++ common/server/http-stats-t.h
@@ -6,39 +6,39 @@
 
 #include "common/stats/provider.h"
 
-class http_stats_t : public stats_t {
+class http_stats_t final : public stats_t {
 public:
   http_stats_t() noexcept {
     now_time = time(nullptr);
   }
 
-  void add_general_stat([[maybe_unused]] const char* key, [[maybe_unused]] const char* value_format, ...) noexcept final {
+  void add_general_stat([[maybe_unused]] const char* key, [[maybe_unused]] const char* value_format, ...) noexcept override {
     // ignore
   }
 
 protected:
-  void add_stat([[maybe_unused]] char type, const char* key, double value) noexcept final {
+  void add_stat([[maybe_unused]] char type, const char* key, const double value) noexcept override {
     sb_printf(&sb, "%s%s\t", normalize_key(key, "%s", ""), stats_suffix);
     sb_printf(&sb, "%.3f\t", value);
     sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
-  void add_stat([[maybe_unused]] char type, const char* key, long long value) noexcept final {
+  void add_stat([[maybe_unused]] char type, const char* key, const long long value) noexcept override {
     sb_printf(&sb, "%s%s\t", normalize_key(key, "%s", ""), stats_suffix);
     sb_printf(&sb, "%lld\t", value);
     sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
-  void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, double value) noexcept override {
+  void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, const double value) noexcept override {
     sb_printf(&sb, "%s_%s%s\t", normalize_key(key, "%s", ""), type_tag, stats_suffix);
     sb_printf(&sb, "%.3f\t", value);
     sb_printf(&sb, "%ld", now_time);
     sb_printf(&sb, "\n");
   }
 
-  void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, long long int value) noexcept override {
+  void add_stat_with_tag_type([[maybe_unused]] char type, const char* key, const char* type_tag, const long long int value) noexcept override {
     sb_printf(&sb, "%s_%s%s\t", normalize_key(key, "%s", ""), type_tag, stats_suffix);
     sb_printf(&sb, "%lld\t", value);
     sb_printf(&sb, "%ld", now_time);
