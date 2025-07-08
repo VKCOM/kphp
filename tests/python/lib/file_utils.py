0e2ed307ce5f876b664261ffd7df94baad73602a
Vadim Sadokhov <v.sadokhov@vk.team>
2025-07-08 17:54:27 +0300

    fix file creation wait

--- tests/python/lib/file_utils.py
+++ tests/python/lib/file_utils.py
@@ -3,6 +3,7 @@ import subprocess
 import re
 import sys
 import shutil
+import time
 
 _SUPPORTED_PHP_VERSIONS = ["php7.4", "php8", "php8.1", "php8.2", "php8.3"]
 
@@ -122,3 +123,7 @@ def search_php_bin(php_version: str):
 
 def search_k2_bin():
     return os.getenv("K2_BIN")
+
+def wait_for_file_creation(file_path, check_interval=0.1):
+    while not os.path.exists(file_path):
+        time.sleep(check_interval)
