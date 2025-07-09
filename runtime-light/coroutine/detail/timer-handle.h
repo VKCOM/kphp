cf5b61d62ab53f42b5596de8d5cad776d5bb8552
Alexander Polyakov <al.polyakov@vk.team>
2025-07-09 21:26:58 +0300

    small changes to timers and scheduler

--- runtime-light/coroutine/detail/timer-handle.h
+++ runtime-light/coroutine/detail/timer-handle.h
@@ -14,7 +14,7 @@
 namespace kphp::coro::detail {
 
 class timer_handle {
-  k2::descriptor m_handle{k2::INVALID_PLATFORM_DESCRIPTOR};
+  k2::descriptor m_descriptor{k2::INVALID_PLATFORM_DESCRIPTOR};
   k2::TimePoint m_time_point{};
 
 public:
@@ -30,35 +30,30 @@ public:
   timer_handle& operator=(timer_handle&&) = delete;
 
   auto clear() noexcept -> void {
-    if (m_handle != k2::INVALID_PLATFORM_DESCRIPTOR) {
-      k2::free_descriptor(std::exchange(m_handle, k2::INVALID_PLATFORM_DESCRIPTOR));
+    if (m_descriptor != k2::INVALID_PLATFORM_DESCRIPTOR) {
+      k2::free_descriptor(std::exchange(m_descriptor, k2::INVALID_PLATFORM_DESCRIPTOR));
     }
     m_time_point = {};
   }
 
   auto reset(k2::TimePoint time_point) noexcept -> std::expected<void, int32_t> {
+    clear();
+
     k2::TimePoint now{};
     k2::instant(std::addressof(now));
-    if (time_point.time_point_ns < now.time_point_ns) [[unlikely]] {
-      return std::unexpected{k2::errno_einval};
-    }
-    if (time_point.time_point_ns == now.time_point_ns) [[unlikely]] {
-      return std::expected<void, int32_t>{};
+    if (time_point.time_point_ns <= now.time_point_ns) [[unlikely]] {
+      time_point.time_point_ns = now.time_point_ns + 1;
     }
 
-    k2::descriptor new_descriptor{};
-    if (const auto errc{k2::new_timer(std::addressof(new_descriptor), time_point.time_point_ns - now.time_point_ns)}; errc != k2::errno_ok) [[unlikely]] {
+    if (const auto errc{k2::new_timer(std::addressof(m_descriptor), time_point.time_point_ns - now.time_point_ns)}; errc != k2::errno_ok) [[unlikely]] {
       return std::unexpected{errc};
     }
-
-    clear();
-    m_handle = new_descriptor;
     m_time_point = time_point;
     return std::expected<void, int32_t>{};
   }
 
   auto handle() const noexcept -> k2::descriptor {
-    return m_handle;
+    return m_descriptor;
   }
 
   auto time_point() const noexcept -> k2::TimePoint {
