d5002d69d7bdcd2e25a2cb7830ff8ec8635d79c0
Alexander Polyakov <al.polyakov@vk.team>
2025-07-17 13:46:52 +0300

    update coroutine concepts and type traits

--- runtime-light/coroutine/type-traits.h
+++ runtime-light/coroutine/type-traits.h
@@ -8,6 +8,7 @@
 #include <type_traits>
 #include <utility>
 
+#include "runtime-light/coroutine/concepts.h"
 #include "runtime-light/coroutine/shared-task.h"
 #include "runtime-light/coroutine/task.h"
 
@@ -44,9 +45,9 @@ public:
 template<typename F, typename... Args>
 requires std::invocable<F, Args...>
 inline constexpr bool is_async_function_v = requires {
-  { static_cast<task<>>(std::declval<std::invoke_result_t<F, Args...>>()) };
+  { static_cast<kphp::coro::task<>>(std::declval<std::invoke_result_t<F, Args...>>()) };
 } || requires {
-  { static_cast<shared_task<>>(std::declval<std::invoke_result_t<F, Args...>>()) };
+  { static_cast<kphp::coro::shared_task<>>(std::declval<std::invoke_result_t<F, Args...>>()) };
 };
 
 // ================================================================================================
@@ -54,5 +55,26 @@ inline constexpr bool is_async_function_v = requires {
 template<typename F, typename... Args>
 requires std::invocable<F, Args...>
 using async_function_return_type_t = kphp::coro::type_traits_impl::async_function_return_type<F, Args...>::type;
+
+// ================================================================================================
+
+template<kphp::coro::concepts::awaitable awaitable_type, typename = void>
+struct awaitable_traits {};
+
+template<kphp::coro::concepts::awaitable awaitable_type>
+struct awaitable_traits<awaitable_type> {
+  using awaiter_type = decltype(kphp::coro::concepts::detail::get_awaiter(std::declval<awaitable_type>()));
+  using awaiter_return_type = decltype(std::declval<awaiter_type>().await_resume());
+};
+
+// ================================================================================================
+
+template<kphp::coro::concepts::coroutine coroutine_type, typename = void>
+struct coroutine_traits {};
+
+template<kphp::coro::concepts::coroutine coroutine_type>
+struct coroutine_traits<coroutine_type> {
+  using return_type = awaitable_traits<coroutine_type>::awaiter_return_type;
+};
 
 } // namespace kphp::coro
