17b7ded097ee69b95c14634aa79aa4e557c95984
Alexander Polyakov <al.polyakov@vk.team>
2025-07-17 13:46:52 +0300

    update coroutine concepts and type traits

--- runtime-light/coroutine/concepts.h
+++ runtime-light/coroutine/concepts.h
@@ -8,7 +8,8 @@
 #include <coroutine>
 #include <utility>
 
-#include "runtime-light/coroutine/async-stack.h"
+#include "runtime-light/coroutine/shared-task.h"
+#include "runtime-light/coroutine/task.h"
 
 namespace kphp::coro::concepts {
 
@@ -18,12 +19,15 @@ template<typename T, typename... Ts>
 concept in_types = (std::same_as<T, Ts> || ...);
 
 template<typename T>
-concept valid_await_suspend_return_type = in_types<T, void, bool> || std::convertible_to<T, std::coroutine_handle<>>;
+concept valid_await_suspend_return_type = in_types<void, bool> || std::convertible_to<T, std::coroutine_handle<>>;
 
 template<typename T>
-concept awaiter = requires(T t, std::coroutine_handle<kphp::coro::async_stack_element> coro) {
+concept awaiter = requires(T t) {
   { t.await_ready() } noexcept -> std::same_as<bool>;
-  { t.await_suspend(coro) } noexcept -> valid_await_suspend_return_type;
+  // TODO find a way to add 'await_suspend' here. for now there is a problem that we have two kinds of await_suspend:
+  // 1. one that accepts type erase coroutine handle: std::coroutine_handle<>
+  // 2. one that accepts generic coroutine handle: std::coroutine_handle<promise_type> where promise_type
+  //    is inherited from kphp::coro::async_stack_element
   { t.await_resume() } noexcept;
 };
 
@@ -42,28 +46,26 @@ concept global_co_await_awaitable = requires(T t) {
 template<typename T>
 concept awaitable = detail::member_co_await_awaitable<T> || detail::global_co_await_awaitable<T> || detail::awaiter<T>;
 
-template<awaitable awaitable, typename = void>
-struct awaitable_traits {};
-
 namespace detail {
 
-template<awaitable awaitable>
-auto get_awaiter(awaitable&& value) noexcept {
-  if constexpr (member_co_await_awaitable<awaitable>) {
-    return std::forward<awaitable>(value).operator co_await();
-  } else if constexpr (global_co_await_awaitable<awaitable>) {
-    return operator co_await(std::forward<awaitable>(value));
-  } else if constexpr (awaiter<awaitable>) {
-    return std::forward<awaitable>(value);
+template<awaitable awaitable_type>
+auto get_awaiter(awaitable_type&& value) noexcept {
+  if constexpr (member_co_await_awaitable<awaitable_type>) {
+    return std::forward<awaitable_type>(value).operator co_await();
+  } else if constexpr (global_co_await_awaitable<awaitable_type>) {
+    return operator co_await(std::forward<awaitable_type>(value));
+  } else if constexpr (awaiter<awaitable_type>) {
+    return std::forward<awaitable_type>(value);
   }
 }
 
 } // namespace detail
 
-template<awaitable awaitable>
-struct awaitable_traits<awaitable> {
-  using awaiter_type = decltype(detail::get_awaiter(std::declval<awaitable>()));
-  using awaiter_return_type = decltype(std::declval<awaiter_type>().await_resume());
-};
+template<typename T>
+concept coroutine = awaitable<T> && (requires {
+  { static_cast<kphp::coro::task<>>(std::declval<T>()) };
+} || requires {
+  { static_cast<kphp::coro::shared_task<>>(std::declval<T>()) };
+});
 
 } // namespace kphp::coro::concepts
