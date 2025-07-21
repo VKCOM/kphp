bd58ac704ef76d21b57f71259951eca5ec7e8d2c
Alexander Polyakov <al.polyakov@vk.team>
2025-07-21 20:19:57 +0300

    preliminary rework of resource

--- runtime-light/stdlib/file/file-system-functions.cpp
+++ runtime-light/stdlib/file/file-system-functions.cpp
@@ -14,13 +14,13 @@
 
 resource f$fopen(const string& filename, [[maybe_unused]] const string& mode, [[maybe_unused]] bool use_include_path,
                  [[maybe_unused]] const resource& context) noexcept {
-  underlying_resource_t rsrc{{filename.c_str(), filename.size()}};
-  if (rsrc.last_errc != k2::errno_ok) [[unlikely]] {
+  kphp::fs::underlying_resource rsrc{{filename.c_str(), filename.size()}};
+  if (rsrc.error_code() != k2::errno_ok) [[unlikely]] {
     kphp::log::warning("fopen failed: {}", filename.c_str());
     return {};
   }
 
-  return f$to_mixed(make_instance<underlying_resource_t>(std::move(rsrc)));
+  return f$to_mixed(make_instance<kphp::fs::underlying_resource>(std::move(rsrc)));
 }
 
 resource f$stream_socket_client(const string& address, mixed& error_code, [[maybe_unused]] mixed& error_message, [[maybe_unused]] double timeout,
@@ -30,29 +30,29 @@ resource f$stream_socket_client(const string& address, mixed& error_code, [[mayb
    *       but it can't be expressed simple ways by awaitables since we blocked inside k2
    * */
   const std::string_view address_view{address.c_str(), address.size()};
-  const auto address_kind{resource_impl_::uri_to_resource_kind(address_view)};
-  if (address_kind != resource_kind::UDP) [[unlikely]] {
+  const auto address_kind{kphp::fs::uri_to_resource_kind(address_view)};
+  if (address_kind != kphp::fs::resource_kind::UDP) [[unlikely]] {
     return static_cast<int64_t>(k2::errno_einval);
   }
 
-  underlying_resource_t rsrc{address_view};
-  if (rsrc.last_errc != k2::errno_ok) [[unlikely]] {
-    error_code = static_cast<int64_t>(rsrc.last_errc);
+  kphp::fs::underlying_resource rsrc{address_view};
+  if (rsrc.error_code() != k2::errno_ok) [[unlikely]] {
+    error_code = static_cast<int64_t>(rsrc.error_code());
     return {};
   }
-  return f$to_mixed(make_instance<underlying_resource_t>(std::move(rsrc)));
+  return f$to_mixed(make_instance<kphp::fs::underlying_resource>(std::move(rsrc)));
 }
 
 Optional<string> f$file_get_contents(const string& stream) noexcept {
-  underlying_resource_t rsrc{{stream.c_str(), stream.size()}};
-  if (rsrc.last_errc != k2::errno_ok) [[unlikely]] {
+  kphp::fs::underlying_resource rsrc{{stream.c_str(), stream.size()}};
+  if (rsrc.error_code() != k2::errno_ok) [[unlikely]] {
     return false;
   }
   return rsrc.get_contents();
 }
 
 kphp::coro::task<Optional<int64_t>> f$fwrite(resource stream, string text) noexcept {
-  auto rsrc{from_mixed<class_instance<underlying_resource_t>>(stream, {})};
+  auto rsrc{from_mixed<class_instance<kphp::fs::underlying_resource>>(stream, {})};
   if (rsrc.is_null()) [[unlikely]] {
     kphp::log::warning("unexpected resource in fwrite: {}", stream.to_string().c_str());
     co_return false;
@@ -61,7 +61,7 @@ kphp::coro::task<Optional<int64_t>> f$fwrite(resource stream, string text) noexc
 }
 
 bool f$fflush(const resource& stream) noexcept {
-  auto rsrc{from_mixed<class_instance<underlying_resource_t>>(stream, {})};
+  auto rsrc{from_mixed<class_instance<kphp::fs::underlying_resource>>(stream, {})};
   if (rsrc.is_null()) [[unlikely]] {
     kphp::log::warning("unexpected resource in fflush: {}", stream.to_string().c_str());
     return false;
@@ -72,7 +72,7 @@ bool f$fflush(const resource& stream) noexcept {
 }
 
 bool f$fclose(const resource& stream) noexcept {
-  auto rsrc{from_mixed<class_instance<underlying_resource_t>>(stream, {})};
+  auto rsrc{from_mixed<class_instance<kphp::fs::underlying_resource>>(stream, {})};
   if (rsrc.is_null()) [[unlikely]] {
     kphp::log::warning("unexpected resource in fclose: {}", stream.to_string().c_str());
     return false;
