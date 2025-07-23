5a544a6dd793cbfd2b02da6d3237beb71886c656
Alexander Polyakov <al.polyakov@vk.team>
2025-07-23 18:25:51 +0300

    add TODO comment to set_timer

--- runtime-light/stdlib/time/timer-functions.h
+++ runtime-light/stdlib/time/timer-functions.h
@@ -21,7 +21,9 @@ void f$set_timer(int64_t timeout_ms, T&& on_timer_callback) noexcept {
     kphp::log::warning("can't set timer for negative duration {}ms", timeout_ms);
     return;
   }
-
+  // TODO choose from:
+  // 1. someone should pop that fork from ForkInstanceState since it will stay there unless we perform f$wait on fork
+  // 2. start timer_task via kphp::coro::io_scheduler::spawn (it won't have distinct fork id)
   auto timer_task{std::invoke(
       [](std::chrono::milliseconds duration, T on_timer_callback) noexcept -> kphp::coro::task<> {
         co_await kphp::forks::id_managed(kphp::coro::io_scheduler::get().schedule_after(duration));
