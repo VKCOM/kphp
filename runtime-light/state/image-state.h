772f3c362882fcb0ccdb5ff4aba2cd586b02fc34
Vadim Sadokhov <v.sadokhov@vk.team>
2025-07-23 18:28:10 +0300

    remove less include

--- runtime-light/state/image-state.h
+++ runtime-light/state/image-state.h
@@ -14,7 +14,6 @@
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-light/allocator/allocator-state.h"
 #include "runtime-light/k2-platform/k2-api.h"
-#include "runtime-light/stdlib/diagnostics/error-handling-state.h"
 #include "runtime-light/stdlib/math/math-state.h"
 #include "runtime-light/stdlib/rpc/rpc-client-state.h"
 #include "runtime-light/stdlib/string/string-state.h"
