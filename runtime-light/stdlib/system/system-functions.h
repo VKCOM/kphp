2eda7964e13bd847116b22608ee370cd0ce4328b
Vadim Sadokhov <v.sadokhov@vk.team>
2025-07-29 17:42:13 +0300

    small fixes

--- runtime-light/stdlib/system/system-functions.h
+++ runtime-light/stdlib/system/system-functions.h
@@ -12,6 +12,7 @@
 #include "runtime-light/coroutine/io-scheduler.h"
 #include "runtime-light/coroutine/task.h"
 #include "runtime-light/state/image-state.h"
+#include "runtime-light/stdlib/diagnostics/logs.h"
 #include "runtime-light/stdlib/fork/fork-functions.h"
 #include "runtime-light/stdlib/system/system-state.h"
 
