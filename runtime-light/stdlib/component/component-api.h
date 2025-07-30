3f3d7c09bc7afa02e891a96aca10ab07b43adcbd
Alexander Polyakov <al.polyakov@vk.team>
2025-07-30 15:32:06 +0300

    add kphp::component API

--- runtime-light/stdlib/component/component-api.h
+++ runtime-light/stdlib/component/component-api.h
@@ -4,7 +4,10 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <expected>
+#include <span>
 #include <string_view>
 #include <utility>
 
@@ -12,7 +15,40 @@
 #include "runtime-common/core/class-instance/refcountable-php-classes.h"
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-light/coroutine/task.h"
+#include "runtime-light/stdlib/fork/fork-functions.h"
 #include "runtime-light/streams/stream.h"
+#include "runtime-light/utils/logs.h"
+
+namespace kphp::component {
+
+inline auto send_request(const kphp::component::stream& stream, std::span<const std::byte> request) noexcept -> kphp::coro::task<std::expected<void, int32_t>> {
+  if (auto expected{co_await stream.write(request)}; !expected) [[unlikely]] {
+    co_return std::move(expected);
+  }
+  stream.shutdown_write();
+  co_return std::expected<void, int32_t>{};
+}
+
+inline auto fetch_response(kphp::component::stream& stream) noexcept -> kphp::coro::task<std::expected<void, int32_t>> {
+  stream.clear();
+  co_return co_await stream.read();
+}
+
+inline auto fetch_request(kphp::component::stream& stream) noexcept -> kphp::coro::task<std::expected<void, int32_t>> {
+  stream.clear();
+  co_return co_await stream.read();
+}
+
+inline auto send_response(const kphp::component::stream& stream, std::span<const std::byte> response) noexcept
+    -> kphp::coro::task<std::expected<void, int32_t>> {
+  if (auto expected{co_await stream.write(response)}; !expected) [[unlikely]] {
+    co_return std::move(expected);
+  }
+  stream.shutdown_write();
+  co_return std::expected<void, int32_t>{};
+}
+
+} // namespace kphp::component
 
 // === ComponentQuery =============================================================================
 
@@ -46,14 +82,63 @@ public:
 
 // === component query client interface ===========================================================
 
-kphp::coro::task<class_instance<C$ComponentQuery>> f$component_client_send_request(string name, string message) noexcept;
+inline auto f$component_client_send_request(string name, string message) noexcept -> kphp::coro::task<class_instance<C$ComponentQuery>> {
+  auto expected_stream{kphp::component::stream::open({name.c_str(), name.size()}, k2::stream_kind::component)};
+  if (!expected_stream) [[unlikely]] {
+    co_return class_instance<C$ComponentQuery>{};
+  }
 
-kphp::coro::task<string> f$component_client_fetch_response(class_instance<C$ComponentQuery> query) noexcept;
+  auto stream{std::move(*expected_stream)};
+  std::span<const std::byte> request{reinterpret_cast<const std::byte*>(message.c_str()), message.size()};
+  if (auto expected{co_await kphp::forks::id_managed(kphp::component::send_request(stream, request))}; !expected) [[unlikely]] {
+    co_return class_instance<C$ComponentQuery>{};
+  }
+  co_return make_instance<C$ComponentQuery>(std::move(stream));
+}
+
+inline auto f$component_client_fetch_response(class_instance<C$ComponentQuery> query) noexcept -> kphp::coro::task<string> {
+  if (query.is_null()) [[unlikely]] {
+    kphp::log::warning("can't fetch component response from null query");
+    co_return string{};
+  }
+
+  auto& stream{query.get()->stream()};
+  if (auto expected{co_await kphp::forks::id_managed(kphp::component::fetch_response(stream))}; !expected) [[unlikely]] {
+    co_return string{};
+  }
+  co_return string{reinterpret_cast<const char*>(stream.data().data()), static_cast<string::size_type>(stream.data().size())};
+}
 
 // === component query server interface ===========================================================
 
-kphp::coro::task<class_instance<C$ComponentQuery>> f$component_server_accept_query() noexcept;
+inline auto f$component_server_accept_query() noexcept -> kphp::coro::task<class_instance<C$ComponentQuery>> {
+  auto opt_stream{co_await kphp::forks::id_managed(kphp::component::stream::accept())};
+  if (!opt_stream) [[unlikely]] {
+    co_return class_instance<C$ComponentQuery>{};
+  }
+  co_return make_instance<C$ComponentQuery>(std::move(*opt_stream));
+}
 
-kphp::coro::task<string> f$component_server_fetch_request(class_instance<C$ComponentQuery> query) noexcept;
+inline auto f$component_server_fetch_request(class_instance<C$ComponentQuery> query) noexcept -> kphp::coro::task<string> {
+  if (query.is_null()) [[unlikely]] {
+    kphp::log::warning("can't fetch server request from null query");
+    co_return string{};
+  }
 
-kphp::coro::task<> f$component_server_send_response(class_instance<C$ComponentQuery> query, string message) noexcept;
+  auto& stream{query.get()->stream()};
+  if (auto expected{co_await kphp::forks::id_managed(kphp::component::fetch_request(stream))}; !expected) [[unlikely]] {
+    co_return string{};
+  }
+  co_return string{reinterpret_cast<const char*>(stream.data().data()), static_cast<string::size_type>(stream.data().size())};
+}
+
+inline auto f$component_server_send_response(class_instance<C$ComponentQuery> query, string message) noexcept -> kphp::coro::task<> {
+  if (query.is_null()) [[unlikely]] {
+    kphp::log::warning("can't send server response to null query");
+    co_return;
+  }
+
+  auto& stream{query.get()->stream()};
+  std::span<const std::byte> response{reinterpret_cast<const std::byte*>(message.c_str()), message.size()};
+  co_await kphp::forks::id_managed(kphp::component::send_response(stream, response));
+}
