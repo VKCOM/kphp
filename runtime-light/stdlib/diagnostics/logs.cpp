1c2922c7a02ba1a02ff9d5ac33a2c27c0af7494c
Vadim Sadokhov <v.sadokhov@vk.team>
2025-07-30 12:51:58 +0300

    small fix

--- runtime-light/stdlib/diagnostics/logs.cpp
+++ runtime-light/stdlib/diagnostics/logs.cpp
@@ -34,22 +34,22 @@ void log(level level, std::optional<std::span<void* const>> trace, std::string_v
     return;
   }
 
-//  static constexpr std::string_view BACKTRACE_KEY = "trace";
-//  static constexpr size_t BACKTRACE_BUFFER_SIZE = 1024UZ * 4UZ;
-//  std::array<char, BACKTRACE_BUFFER_SIZE> backtrace_buffer; // NOLINT
-//  std::string_view backtrace{"[]"};
-//  if (auto backtrace_symbols{kphp::diagnostic::backtrace_symbols(*trace)}; !backtrace_symbols.empty()) {
-//    const auto [trace_out, trace_size]{std::format_to_n(backtrace_buffer.data(), backtrace_buffer.size() - 1, "\n{}", backtrace_symbols)};
-//    *trace_out = '\0';
-//    backtrace = std::string_view{backtrace_buffer.data(), static_cast<std::string_view::size_type>(trace_size)};
-//  } else if (auto backtrace_addresses{kphp::diagnostic::backtrace_addresses(*trace)}; !backtrace_addresses.empty()) {
-//    const auto [trace_out, trace_size]{std::format_to_n(backtrace_buffer.data(), backtrace_buffer.size() - 1, "{}", backtrace_addresses)};
-//    *trace_out = '\0';
-//    backtrace = std::string_view{backtrace_buffer.data(), static_cast<std::string_view::size_type>(trace_size)};
-//  }
-//  tagged_entries[1] =
-//      k2::LogTaggedEntry{.key = BACKTRACE_KEY.data(), .value = backtrace.data(), .key_len = BACKTRACE_KEY.size(), .value_len = backtrace.size()};
-//  k2::log(std::to_underlying(level), message, tagged_entries);
+  static constexpr std::string_view BACKTRACE_KEY = "trace";
+  static constexpr size_t BACKTRACE_BUFFER_SIZE = 1024UZ * 4UZ;
+  std::array<char, BACKTRACE_BUFFER_SIZE> backtrace_buffer; // NOLINT
+  std::string_view backtrace{"[]"};
+  if (auto backtrace_symbols{kphp::diagnostic::backtrace_symbols(*trace)}; !backtrace_symbols.empty()) {
+    const auto [trace_out, trace_size]{std::format_to_n(backtrace_buffer.data(), backtrace_buffer.size() - 1, "\n{}", backtrace_symbols)};
+    *trace_out = '\0';
+    backtrace = std::string_view{backtrace_buffer.data(), static_cast<std::string_view::size_type>(trace_size)};
+  } else if (auto backtrace_addresses{kphp::diagnostic::backtrace_addresses(*trace)}; !backtrace_addresses.empty()) {
+    const auto [trace_out, trace_size]{std::format_to_n(backtrace_buffer.data(), backtrace_buffer.size() - 1, "{}", backtrace_addresses)};
+    *trace_out = '\0';
+    backtrace = std::string_view{backtrace_buffer.data(), static_cast<std::string_view::size_type>(trace_size)};
+  }
+  tagged_entries[1] =
+      k2::LogTaggedEntry{.key = BACKTRACE_KEY.data(), .value = backtrace.data(), .key_len = BACKTRACE_KEY.size(), .value_len = backtrace.size()};
+  k2::log(std::to_underlying(level), message, tagged_entries);
 }
 
 } // namespace kphp::log::impl::details
