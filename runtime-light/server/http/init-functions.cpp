e56df2b47b480341c099dfbc4fa50901b1b066e1
Vadim Sadokhov <v.sadokhov@vk.team>
2025-08-07 15:01:30 +0300

    fix format

--- runtime-light/server/http/init-functions.cpp
+++ runtime-light/server/http/init-functions.cpp
@@ -388,13 +388,12 @@ kphp::coro::task<> finalize_server() noexcept {
   }
     // fill headers
     http_response.http_response.headers.value.reserve(http_server_instance_st.headers().size());
-    std::transform(
-        http_server_instance_st.headers().cbegin(), http_server_instance_st.headers().cend(), std::back_inserter(http_response.http_response.headers.value),
-        [](const auto& header_entry) noexcept {
-          const auto& [name, value]{header_entry};
-          return tl::httpHeaderEntry{.is_sensitive = {}, .name = {.value = {name.data(), name.size()}}, .value = {.value = {value.data(), value.size()}}};
-        });
-
+    std::transform(http_server_instance_st.headers().cbegin(), http_server_instance_st.headers().cend(),
+                   std::back_inserter(http_response.http_response.headers.value), [](const auto& header_entry) noexcept {
+                     const auto& [name, value]{header_entry};
+                     return tl::httpHeaderEntry{
+                         .is_sensitive = {}, .name = {.value = {name.data(), name.size()}}, .value = {.value = {value.data(), value.size()}}};
+                   });
     http_server_instance_st.response_state = kphp::http::response_state::headers_sent;
     [[fallthrough]];
   case kphp::http::response_state::headers_sent: {
