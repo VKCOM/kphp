6ca9a572527b474d81b779b62adbbf6c66eca755
Danil Ovchinnikov <danil42worker@gmail.com>
2025-08-09 23:30:52 +0300

    debugging python tests

--- tests/python/tests/conftest.py
+++ tests/python/tests/conftest.py
@@ -2,3 +2,14 @@ import os
 import pytest
 
 from python.lib.conftest_impl import skip_k2_unsupported_test, skip_k2_unsupported_test_suite, skip_kphp_unsupported_test, skip_kphp_unsupported_test_suite
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_protocol(item: pytest.Item):
+    tw = item.config.get_terminal_writer()
+
+    tw.write(f"START TEST: {item.nodeid}")
+    yield
+    tw.write("\n")
+    tw.write(f"FINISH TEST: {item.nodeid}")
+    tw.write("\n")
