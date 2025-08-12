bcb24955c6ce07f582cd84e5d016348b58434f6c
Vadim Sadokhov <v.sadokhov@vk.team>
2025-08-12 17:15:16 +0300

    add miss include

--- runtime-light/state/image-state.h
+++ runtime-light/state/image-state.h
@@ -14,6 +14,7 @@
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-light/allocator/allocator-state.h"
 #include "runtime-light/k2-platform/k2-api.h"
+#include "runtime-light/stdlib/diagnostics/logs.h"
 #include "runtime-light/stdlib/math/math-state.h"
 #include "runtime-light/stdlib/rpc/rpc-client-state.h"
 #include "runtime-light/stdlib/string/string-state.h"
