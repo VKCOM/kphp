e4da6bbb2c3d163bc945e651beefcbc54f1b78cc
Danil Ovchinnikov <danil42worker@gmail.com>
2025-08-13 12:38:43 +0300

    GCC diagnostic ignore

--- common/tl/compiler/tl-parser-new.cpp
+++ common/tl/compiler/tl-parser-new.cpp
@@ -2674,6 +2674,10 @@ int uniformize(struct tl_combinator_tree* L, struct tl_combinator_tree* R, tree_
   return 0;
 }
 
+#if __GNUC__ >= 13
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdangling-pointer"
+#endif
 void tl_type_check(struct tl_type* t) {
   if (!__ok) {
     return;
