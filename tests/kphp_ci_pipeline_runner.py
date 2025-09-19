eaea00b0a7200d6f2b176ca2140aed45600990e8
Danil Ovchinnikov <danil42worker@gmail.com>
2025-09-19 15:27:53 +0300

    fix percent

--- tests/kphp_ci_pipeline_runner.py
+++ tests/kphp_ci_pipeline_runner.py
@@ -233,14 +233,14 @@ def parse_args():
 
     return parser.parse_args()
 
-def _calculate_pytest_jobs_count(percent: int = 5) -> int:
+def _calculate_pytest_jobs_count(percent: int = 95) -> int:
     """
     What percentage of the total number of cores do we use to run parallel tests.
     If you set a large value, the cores may start to idle.
     """
 
     cpu_count = multiprocessing.cpu_count()
-    return math.floor(cpu_count * (1 - percent / 100))
+    return math.floor(cpu_count * percent / 100)
 
 if __name__ == "__main__":
     curses.setupterm()
