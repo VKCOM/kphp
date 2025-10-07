7f70433a70b7579b8c70719f57ed7c57ebb41d70
Danil Ovchinnikov <danil42worker@gmail.com>
2025-10-07 15:19:00 +0300

    Revert "removed unsafe check for `10.0.0.0/8` network"
    This reverts commit 0c9352a39eed1d3c078637a7641ca51d74921336.

--- net/net-ifnet.cpp
+++ net/net-ifnet.cpp
@@ -95,8 +95,9 @@ unsigned get_my_ipv4() {
     my_iface = "lo";
   }
 
-  assert(my_ip != 0 && "can't choose ip in given subnet");
-
+  if (force_ipv4_mask != 0xff000000 || force_ipv4_ip != (10 << 24)) {
+    assert(my_ip != 0 && "can't choose ip in given subnet");
+  }
   vkprintf (2, "using main IP %d.%d.%d.%d/%d at interface %s\n", (my_ip >> 24), (my_ip >> 16) & 255, (my_ip >> 8) & 255, my_ip & 255,
             __builtin_clz(~my_netmask), my_iface ?: "(none)");
   freeifaddrs(ifa_first);
