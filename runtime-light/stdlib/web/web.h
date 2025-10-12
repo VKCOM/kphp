5308c0eedbc2b3ef01d3fa74f21832baf8572f56
Petr Shumilov <p.shumilov@vkteam.ru>
2025-10-13 00:18:34 +0300

    Add CURL options processing
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- runtime-light/stdlib/web/web.h
+++ runtime-light/stdlib/web/web.h
@@ -4,14 +4,30 @@
 
 #pragma once
 
+#include <variant>
+
 #include "runtime-light/stdlib/web/defs.h"
 
 namespace kphp::web {
 
+inline auto process_error(tl::WebError&& error) noexcept -> Error {
+  switch (error.code.value) {
+  case TLParseErrorCode::ServerSide:
+  case InternalErrorCode::UnknownBackend:
+  case InternalErrorCode::UnknownMethod:
+  case InternalErrorCode::UnknownTransfer:
+  case BackendInternalError::CannotTakeHandlerOwnership:
+    return Error{.code = -1, .description = string(error.description.value.data(), error.description.value.size())};
+  default:
+    // BackendError
+    return Error{.code = error.code.value, .description = string(error.description.value.data(), error.description.value.size())};
+  }
+}
+
 inline auto simple_transfer_open(TransferBackend backend) noexcept -> kphp::coro::task<std::expected<SimpleTransfer, Error>> {
   auto session{WebInstanceState::get().session_get_or_init()};
   if (!session.has_value()) {
-    co_return std::unexpected{Error{.code = session.error(), .description = string("Failed to start or get session with Web component")}};
+    kphp::log::error("Failed to start or get session with Web component");
   }
 
   tl::SimpleWebTransferOpen web_transfer_open{.web_backend = {static_cast<tl::SimpleWebTransferOpen::web_backend_type::underlying_type>(backend)}};
@@ -26,17 +42,17 @@ inline auto simple_transfer_open(TransferBackend backend) noexcept -> kphp::coro
     }};
   })};
   if (session.value().get() == nullptr) {
-    co_return std::unexpected{Error{.code = k2::errno_eshutdown, .description = string("Session with Web components has been closed")}};
+    kphp::log::error("Session with Web components has been closed");
   }
   auto resp{co_await session.value().get()->client.query(tls.view(), std::move(response_buffer_provider))};
   if (!resp.has_value()) {
-    co_return std::unexpected{Error{.code = resp.error(), .description = string("Failed to send request of Simple descriptor creation")}};
+    kphp::log::error("Failed to send request of Simple descriptor creation");
   }
 
   tl::SimpleWebTransferOpenResponse simple_web_transfer_resp{};
   tl::fetcher tlf{resp.value()};
   if (!simple_web_transfer_resp.fetch(tlf)) [[unlikely]] {
-    co_return std::unexpected{Error{.code = k2::errno_einval, .description = string("Failed to parse response of Simple descriptor creation")}};
+    kphp::log::error("Failed to parse response of Simple descriptor creation");
   }
 
   std::expected<SimpleTransfer, Error> result;
@@ -46,81 +62,41 @@ inline auto simple_transfer_open(TransferBackend backend) noexcept -> kphp::coro
         if constexpr (std::same_as<value_t, tl::SimpleWebTransferOpenResultOk>) {
           result = std::expected<SimpleTransfer, Error>{static_cast<tl::SimpleWebTransferOpenResultOk>(v).desc.value};
         } else {
-          auto error{static_cast<tl::WebError>(v)};
-          result = std::unexpected{Error{.code = error.code.value, .description = string(error.description.value.data(), error.description.value.size())}};
+          result = std::unexpected{process_error(static_cast<tl::WebError>(v))};
         }
       },
       simple_web_transfer_resp.value);
   co_return result;
 }
 
-inline auto set_transfer_prop(SimpleTransfer st, PropertyId prop_id, PropertyValue prop_value) -> std::expected<Ok, Error> {
+inline auto set_transfer_prop(SimpleTransfer st, PropertyId prop_id, PropertyValue prop_value) -> std::expected<void, Error> {
   auto& simple2config{WebInstanceState::get().simple_transfer2config};
   if (!simple2config.contains(st)) {
     simple2config.emplace(st, SimpleTransferConfig{});
   }
   simple2config[st].properties.insert({prop_id, prop_value});
-  return std::expected<Ok, Error>{Ok{}};
-}
-
-inline auto property_value_serialize(PropertyValue value) noexcept -> tl::WebPropertyValue {
-  switch (value.get_type()) {
-  case mixed::type::BOOLEAN: {
-    return tl::WebPropertyValue{.value = tl::Bool{value.as_bool()}};
-  }
-  case mixed::type::INTEGER: {
-    return tl::WebPropertyValue{.value = tl::I64{tl::i64{value.as_int()}}};
-  }
-  case mixed::type::STRING: {
-    auto& s{value.as_string()};
-    return tl::WebPropertyValue{.value = tl::String{tl::string{{s.c_str(), s.size()}}}};
-  }
-  case mixed::type::ARRAY: {
-    const auto& a{value.as_array()};
-    if (a.is_vector()) {
-      tl::Vector<tl::WebPropertyValue> res{tl::vector<tl::WebPropertyValue>{.value = {}}};
-      for (const auto& i : a) {
-        res.inner.value.emplace_back(property_value_serialize(i.get_value()));
-      }
-      return tl::WebPropertyValue{.value = std::move(res)};
-    } else {
-      tl::Dictionary<tl::WebPropertyValue> res{tl::Dictionary<tl::WebPropertyValue>{tl::vector<tl::dictionaryField<tl::WebPropertyValue>>{.value = {}}}};
-      for (const auto& i : a) {
-        kphp::log::assertion(i.get_key().is_string());
-        const auto& key{i.get_key().as_string()};
-        const auto& val{i.get_value()};
-        res.inner.value.value.emplace_back(tl::dictionaryField{.key = tl::string{{key.c_str(), key.size()}}, .value = property_value_serialize(val)});
-      }
-      return tl::WebPropertyValue{.value = std::move(res)};
-    }
-  }
-  default:
-    // Unsupported kind
-    kphp::log::assertion(false);
-    return tl::WebPropertyValue{};
-  }
+  return std::expected<void, Error>{};
 }
 
 inline auto simple_transfer_perform(SimpleTransfer st) noexcept -> kphp::coro::task<std::expected<Response, Error>> {
   auto& simple2config{WebInstanceState::get().simple_transfer2config};
   if (!simple2config.contains(st)) {
-    co_return std::unexpected{Error{.code = k2::errno_enodata, .description = string("Unknown Simple descriptor")}};
+    kphp::log::error("Unknown Simple descriptor");
   }
 
   auto session{WebInstanceState::get().session_get_or_init()};
   if (!session.has_value()) {
-    co_return std::unexpected{Error{.code = session.error(), .description = string("Failed to start or get session with Web component")}};
+    kphp::log::error("Failed to start or get session with Web component");
   }
 
   if (session.value().get() == nullptr) {
-    co_return std::unexpected{Error{.code = k2::errno_eshutdown, .description = string("Session with Web components has been closed")}};
+    kphp::log::error("Session with Web components has been closed");
   }
 
   kphp::stl::vector<tl::WebProperty, kphp::memory::script_allocator> tl_props{};
   auto& props{simple2config[st].properties};
   for (const auto& [id, val] : props) {
-    tl::WebProperty p{.id = tl::u64{id}, .value = property_value_serialize(val)};
-    kphp::log::debug("id {} ", id);
+    tl::WebProperty p{.id = tl::u64{id}, .value = val.serialize()};
     tl_props.emplace_back(std::move(p));
   }
   tl::SimpleWebTransferConfig tl_config{tl::vector<tl::WebProperty>{std::move(tl_props)}};
@@ -158,16 +134,14 @@ inline auto simple_transfer_perform(SimpleTransfer st) noexcept -> kphp::coro::t
       tl::SimpleWebTransferPerformResponse simple_web_transfer_perform_resp{};
       tl::fetcher tlf{ok_or_error_buffer};
       if (!simple_web_transfer_perform_resp.fetch(tlf)) [[unlikely]] {
-        error.emplace(Error{.code = k2::errno_einval, .description = string("Failed to parse response of Simple descriptor performing")});
-        co_return false;
+        kphp::log::error("Failed to parse response of Simple descriptor performing");
       }
 
       std::visit(
           [&error](const auto& v) noexcept {
             using value_t = std::remove_cvref_t<decltype(v)>;
             if constexpr (std::same_as<value_t, tl::WebError>) {
-              auto tl_web_error{static_cast<tl::WebError>(v)};
-              error.emplace(Error{.code = tl_web_error.code.value, .description = string(tl_web_error.description.value.data(), tl_web_error.description.value.size())});
+              error.emplace(process_error(static_cast<tl::WebError>(v)));
             }
           },
           simple_web_transfer_perform_resp.value);
@@ -184,7 +158,7 @@ inline auto simple_transfer_perform(SimpleTransfer st) noexcept -> kphp::coro::t
   }};
 
   if (auto res{co_await session.value().get()->client.query(tls.view(), std::move(response_buffer_provider), response_handler)}; !res) {
-    co_return std::unexpected{Error{.code = res.error(), .description = string("Failed to send request of Simple descriptor performing")}};
+    kphp::log::error("Failed to send request of Simple descriptor performing");
   }
   if (error.has_value()) {
     co_return std::unexpected{std::move(error.value())};
@@ -192,11 +166,11 @@ inline auto simple_transfer_perform(SimpleTransfer st) noexcept -> kphp::coro::t
   co_return std::expected<Response, Error>{std::move(response)};
 }
 
-inline auto simple_transfer_reset(SimpleTransfer st) noexcept -> kphp::coro::task<std::expected<Ok, Error>> {
+inline auto simple_transfer_reset(SimpleTransfer st) noexcept -> kphp::coro::task<std::expected<void, Error>> {
   co_return std::unexpected{Error{.code = -1, .description = std::nullopt}};
 }
 
-inline auto simple_transfer_close(SimpleTransfer st) noexcept -> kphp::coro::task<std::expected<Ok, Error>> {
+inline auto simple_transfer_close(SimpleTransfer st) noexcept -> kphp::coro::task<std::expected<void, Error>> {
   co_return std::unexpected{Error{.code = -1, .description = std::nullopt}};
 }
 
