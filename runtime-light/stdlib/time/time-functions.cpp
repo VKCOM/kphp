368fa0f63c077e2657edc337bfa1c76087bdafd3
Karim Shamazov <k.shamazov@vkteam.ru>
2025-10-15 18:23:00 +0300

    fixes

--- runtime-light/stdlib/time/time-functions.cpp
+++ runtime-light/stdlib/time/time-functions.cpp
@@ -313,38 +313,42 @@ string date(const string& format, const tm& t, int64_t timestamp, bool local) no
 
 array<mixed> f$getdate(int64_t timestamp) noexcept {
   if (timestamp == std::numeric_limits<int64_t>::min()) {
-    timestamp = std::time(nullptr);
+    k2::SystemTime st{};
+    k2::system_time(std::addressof(st));
+    timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::nanoseconds{st.since_epoch_ns}).count();
   }
-  tm t;
+  tm t{};
   time_t timestamp_t = timestamp;
-  tm* tp = k2::localtime_r(&timestamp_t, &t);
+  tm* tp = k2::localtime_r(std::addressof(timestamp_t), std::addressof(t));
   if (tp == nullptr) {
-    char buff[256];
-    php_warning("Error \"%s\" in getdate with timestamp %" PRId64, strerror_r(errno, buff, sizeof(buff)), timestamp);
-    memset(&t, 0, sizeof(tm));
+    std::array<char, 256> buff{};
+    kphp::log::warning("Error \"{}\" in getdate with timestamp {}", strerror_r(errno, buff.data(), buff.size()), timestamp);
+    std::memset(std::addressof(t), 0, sizeof(tm));
   }
 
   array<mixed> result(array_size(11, false));
 
-  result.set_value(string("seconds", 7), t.tm_sec);
-  result.set_value(string("minutes", 7), t.tm_min);
-  result.set_value(string("hours", 5), t.tm_hour);
-  result.set_value(string("mday", 4), t.tm_mday);
-  result.set_value(string("wday", 4), t.tm_wday);
-  result.set_value(string("mon", 3), t.tm_mon + 1);
-  result.set_value(string("year", 4), t.tm_year + 1900);
-  result.set_value(string("yday", 4), t.tm_yday);
-  result.set_value(string("weekday", 7), string(PHP_TIMELIB_DAY_FULL_NAMES[t.tm_wday]));
-  result.set_value(string("month", 5), string(PHP_TIMELIB_MON_FULL_NAMES[t.tm_mon]));
-  result.set_value(string("0", 1), timestamp);
+  result.set_value(string{"seconds", 7}, t.tm_sec);
+  result.set_value(string{"minutes", 7}, t.tm_min);
+  result.set_value(string{"hours", 5}, t.tm_hour);
+  result.set_value(string{"mday", 4}, t.tm_mday);
+  result.set_value(string{"wday", 4}, t.tm_wday);
+  result.set_value(string{"mon", 3}, t.tm_mon + 1);
+  result.set_value(string{"year", 4}, t.tm_year + 1900);
+  result.set_value(string{"yday", 4}, t.tm_yday);
+  result.set_value(string{"weekday", 7}, string{PHP_TIMELIB_DAY_FULL_NAMES[t.tm_wday]});
+  result.set_value(string{"month", 5}, string{PHP_TIMELIB_MON_FULL_NAMES[t.tm_mon]});
+  result.set_value(string{"0", 1}, timestamp);
 
   return result;
 }
 
 int64_t f$gmmktime(int64_t h, int64_t m, int64_t s, int64_t month, int64_t day, int64_t year) noexcept {
-  tm t;
-  time_t timestamp_t = std::time(nullptr);
-  gmtime_r(&timestamp_t, &t);
+  tm t{};
+  k2::SystemTime st{};
+  k2::system_time(std::addressof(st));
+  time_t timestamp_t = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::nanoseconds{st.since_epoch_ns}).count();
+  gmtime_r(std::addressof(timestamp_t), std::addressof(t));
 
   if (h != std::numeric_limits<int64_t>::min()) {
     t.tm_hour = static_cast<int32_t>(h);
@@ -372,7 +376,7 @@ int64_t f$gmmktime(int64_t h, int64_t m, int64_t s, int64_t month, int64_t day, 
 
   t.tm_isdst = -1;
 
-  return timegm(&t);
+  return timegm(std::addressof(t));
 }
 
 bool f$checkdate(int64_t month, int64_t day, int64_t year) noexcept {
