4550f9f6c40b5db6ae5da5994c0b67b176692617
Vadim Sadokhov <v.sadokhov@vk.team>
2025-10-22 16:16:46 +0300

    fix

--- runtime-light/stdlib/fork/wait-queue-state.cpp
+++ runtime-light/stdlib/fork/wait-queue-state.cpp
@@ -2,8 +2,8 @@
 // Copyright (c) 2025 LLC «V Kontakte»
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
-#include "runtime-light/state/instance-state.h"
 #include "runtime-light/stdlib/fork/wait-queue-state.h"
+#include "runtime-light/state/instance-state.h"
 
 WaitQueueInstanceState& WaitQueueInstanceState::get() noexcept {
   return InstanceState::get().wait_queue_instance_state;
