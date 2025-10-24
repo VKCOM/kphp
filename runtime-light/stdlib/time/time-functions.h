fe5e24077e35933838de9f0670b757bf7af58806
Karim Shamazov <k.shamazov@vkteam.ru>
2025-10-24 15:39:16 +0300

    format

--- runtime-light/stdlib/time/time-functions.h
+++ runtime-light/stdlib/time/time-functions.h
@@ -75,9 +75,8 @@ inline int64_t f$time() noexcept {
 
 inline Optional<int64_t> f$mktime(Optional<int64_t> hour = {}, Optional<int64_t> minute = {}, Optional<int64_t> second = {}, Optional<int64_t> month = {},
                                   Optional<int64_t> day = {}, Optional<int64_t> year = {}) noexcept {
-  auto res = kphp::timelib::mktime(static_cast<std::optional<int64_t>>(hour), static_cast<std::optional<int64_t>>(minute),
-                                              static_cast<std::optional<int64_t>>(second), static_cast<std::optional<int64_t>>(month),
-                                              static_cast<std::optional<int64_t>>(day), static_cast<std::optional<int64_t>>(year));
+  auto res =
+      kphp::timelib::mktime(static_cast<std::optional<int64_t>>(hour), static_cast<std::optional<int64_t>>(minute), static_cast<std::optional<int64_t>>(second), static_cast<std::optional<int64_t>>(month), static_cast<std::optional<int64_t>>(day), static_cast<std::optional<int64_t>>(year));
   if (res.has_value()) {
     return *res;
   }
