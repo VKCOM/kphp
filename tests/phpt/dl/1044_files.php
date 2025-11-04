bd353338d8a06c62a92872ca6a771b357151d493
Nikita Siniachenko <n.sinyachenko@vk.team>
2025-11-04 22:20:49 +0300

    refactored 1044_files.php test

--- tests/phpt/dl/1044_files.php
+++ tests/phpt/dl/1044_files.php
@@ -8,57 +8,34 @@
   var_dump (basename ('/etc/sudoers.d', '.d'));
 
   $filename = "12341234124123";
-/*
-  var_dump ($filename);
-  $file = fopen ($filename, "w");
-  fwrite ($file, "1232225\n");
-  if (fwrite ($file, "qwreqer") != 7) {
-    echo "error1\n";
-  }
-  if (fwrite ($file, "") !== 0) {
-    echo "error2\n";
-  }
-  if (@(fwrite ("asdasdasd", "") !== false)) {
-    echo "error3\n";
-  }
-  fflush ($file);
-  fwrite ($file, "asdfdaf\n");
-  fwrite ($file, "asdfdaf");
-  fflush ($file);
-  var_dump (fread ($file, 100));
-  fclose ($file);
-  if (@(fwrite ($file, "1231") !== false)) {
-    echo "error4\n";
-  }
 
-  fwrite (STDERR, "Not a part of the answer\n");
-
-  $file = fopen ($filename, "rb");
-
-  var_dump (file_get_contents ($filename));
-  @var_dump (file_put_contents ('tmp', 'test'));
-  var_dump (file_put_contents ($filename, "test-test-test"));
-  var_dump (file_get_contents ($filename));
-  var_dump (file_put_contents ($filename, ''));
-  fclose($file);
- */
-
-
-  $large_text = str_repeat("s", 3000);
+  $large_text = str_repeat("s", 1000);
+  $large_text2 = str_repeat("t", 1000);
   var_dump (file_put_contents($filename, $large_text));
+  var_dump (file_put_contents($filename, $large_text2, 1)); // append
   $file = fopen ($filename, 'r');
+  var_dump (fread($file, 2000));
   fclose ($file);
-  @var_dump (file_get_contents ($filename));
-  @var_dump ($filename);
-  @var_dump (unlink ($filename));
-  @var_dump (file_get_contents ($filename));
 
-  $append_text = str_repeat("t", 30);
-  var_dump (file_put_contents($filename, $append_text, 1));
-  @var_dump (file_get_contents ($filename));
 
-  @var_dump (realpath ("../dl/473-file.php"));
-  @var_dump (realpath ("../dl/473_file.php"));
-  @var_dump (realpath ("phpt/dl/473-file.php"));
-  @var_dump (realpath ("phpt/dl/473_file.php"));
+  $file = fopen ($filename, 'w');
+  var_dump (fwrite($file, $large_text));
+  fclose ($file);
+  $file = fopen ($filename, 'a');
+  var_dump (fwrite($file, $large_text2));
+  fclose ($file);
+  $file = fopen ($filename, 'r');
+  var_dump (fread($file, 2000));
+  fclose ($file);
+// TODO    @var_dump (unlink ($filename));
+
+  $file = fopen ($filename, 'r');
+  var_dump (fread($file, 1));
+  fclose ($file);
+
+
+  var_dump (realpath ("../dl/473-file.php"));
+  var_dump (realpath ("../dl/473_file.php"));
+  var_dump (realpath ("phpt/dl/473-file.php"));
+  var_dump (realpath ("phpt/dl/473_file.php"));
 
