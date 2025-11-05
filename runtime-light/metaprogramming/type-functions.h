168c2fdbed0e3f705dd9eb6b6d86f5c53b28b165
Vadim Sadokhov <v.sadokhov@vk.team>
2025-11-05 17:31:12 +0300

    small fix

--- runtime-light/metaprogramming/type-functions.h
+++ runtime-light/metaprogramming/type-functions.h
@@ -5,17 +5,15 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
 #include <variant>
 
 namespace kphp::type_functions {
 
 template<typename variant_type, typename T, size_t index = 0>
+requires(index < std::variant_size_v<variant_type>)
 consteval size_t variant_index() noexcept {
-  static_assert(index < std::variant_size_v<variant_type>, "Type not found in variant");
-  if constexpr (index == std::variant_size_v<variant_type>) {
-    // To prevent the error of substituting an index that is too large in variant_alternative_t
-    return index;
-  } else if constexpr (std::is_same_v<std::variant_alternative_t<index, variant_type>, T>) {
+  if constexpr (std::is_same_v<std::variant_alternative_t<index, variant_type>, T>) {
     return index;
   } else {
     return variant_index<variant_type, T, index + 1>();
