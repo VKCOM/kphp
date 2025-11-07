391ff74807ee0c84e50ab1dc1e8c681f997ba05d
Alexander Polyakov <al.polyakov@vk.team>
2025-11-07 17:02:26 +0300

    minor changes in RPC queue

--- runtime-light/stdlib/rpc/rpc-queue-functions.h
+++ runtime-light/stdlib/rpc/rpc-queue-functions.h
@@ -7,18 +7,25 @@
 #include <chrono>
 #include <cstdint>
 #include <optional>
+#include <span>
+#include <utility>
 
+#include "runtime-common/core/runtime-core.h"
 #include "runtime-light/coroutine/io-scheduler.h"
 #include "runtime-light/coroutine/task.h"
 #include "runtime-light/stdlib/diagnostics/logs.h"
 #include "runtime-light/stdlib/fork/fork-functions.h"
-#include "runtime-light/stdlib/fork/wait-queue-functions.h"
 #include "runtime-light/stdlib/rpc/rpc-client-state.h"
 #include "runtime-light/stdlib/rpc/rpc-queue-state.h"
 
 namespace kphp::rpc {
 
 inline void rpc_queue_push(int64_t queue_id, int64_t request_id) noexcept {
+  static constexpr auto rpc_queue_wrapper_task{[](kphp::coro::shared_task<> awaiter_task, int64_t request_id) noexcept -> kphp::coro::task<int64_t> {
+    co_await std::move(awaiter_task).when_ready();
+    co_return request_id;
+  }};
+
   auto& rpc_client_instance_st{RpcClientInstanceState::get()};
 
   const auto it_awaiter_task{rpc_client_instance_st.response_awaiter_tasks.find(request_id)};
@@ -29,23 +36,18 @@ inline void rpc_queue_push(int64_t queue_id, int64_t request_id) noexcept {
 
   auto& rpc_queue_instance_st{RpcQueueInstanceState::get()};
   auto opt_await_set{rpc_queue_instance_st.get_queue(queue_id)};
-  if (!opt_await_set.has_value()) [[unlikely]] {
+  if (!opt_await_set) [[unlikely]] {
     kphp::log::warning("future with id {} isn't associated with rpc queue", queue_id);
     return;
   }
 
-  static constexpr auto rpc_queue_wrapper_task{[](kphp::coro::shared_task<> awaiter_task, int64_t request_id) noexcept -> kphp::coro::task<int64_t> {
-    co_await std::move(awaiter_task).when_ready();
-    co_return request_id;
-  }};
-
   auto& await_set{(*opt_await_set).get()};
-  await_set.push(rpc_queue_wrapper_task(static_cast<kphp::coro::shared_task<>>(std::move(it_awaiter_task->second)), request_id));
+  await_set.push(rpc_queue_wrapper_task(static_cast<kphp::coro::shared_task<>>(it_awaiter_task->second), request_id));
 }
 
-inline int64_t rpc_queue_create(std::span<int64_t> range) noexcept {
+inline int64_t rpc_queue_create(std::span<int64_t> request_ids) noexcept {
   const int64_t queue_id{RpcQueueInstanceState::get().create_queue()};
-  for (int64_t request_id : range) {
+  for (int64_t request_id : request_ids) {
     rpc_queue_push(queue_id, request_id);
   }
 
@@ -53,34 +55,35 @@ inline int64_t rpc_queue_create(std::span<int64_t> range) noexcept {
 }
 
 inline kphp::coro::task<std::optional<int64_t>> rpc_queue_next(int64_t queue_id, std::chrono::nanoseconds timeout) noexcept {
+  static constexpr auto rpc_queue_next_task{
+      [](auto await_set_awaitable) noexcept -> kphp::coro::task<std::optional<int64_t>> { co_return co_await std::move(await_set_awaitable); }};
+
   auto& rpc_queue_instance_st{RpcQueueInstanceState::get()};
   auto opt_await_set{rpc_queue_instance_st.get_queue(queue_id)};
 
-  if (!opt_await_set.has_value()) [[unlikely]] {
+  if (!opt_await_set) [[unlikely]] {
     kphp::log::warning("future with id {} isn't associated with rpc queue", queue_id);
     co_return std::nullopt;
   }
 
   auto& await_set{(*opt_await_set).get()};
-  static constexpr auto rpc_queue_next_task{
-      [](auto await_set_awaitable) noexcept -> kphp::coro::task<std::optional<int64_t>> { co_return co_await std::move(await_set_awaitable); }};
-  auto wait_result{co_await kphp::coro::io_scheduler::get().schedule(rpc_queue_next_task(await_set.next()), kphp::forks::detail::normalize_timeout(timeout))};
-  if (!wait_result) {
+  const auto expected_next{co_await kphp::coro::io_scheduler::get().schedule(rpc_queue_next_task(await_set.next()), timeout)};
+  if (!expected_next) {
     co_return std::nullopt;
   }
 
-  if (auto opt_request_id{*wait_result}; opt_request_id.has_value()) {
-    co_return *opt_request_id;
-  } else {
-    kphp::log::warning("await set associated with the wait queue was destroyed");
+  const auto opt_request_id{*expected_next};
+  if (!opt_request_id) [[unlikely]] {
+    kphp::log::warning("failed to get RPC response from rpc queue {}", queue_id);
     co_return kphp::rpc::INVALID_QUERY_ID;
   }
+  co_return *opt_request_id;
 }
 
 inline bool rpc_queue_empty(int64_t queue_id) noexcept {
   auto& rpc_queue_instance_st{RpcQueueInstanceState::get()};
   auto opt_await_set{rpc_queue_instance_st.get_queue(queue_id)};
-  if (!opt_await_set.has_value()) [[unlikely]] {
+  if (!opt_await_set) [[unlikely]] {
     kphp::log::warning("future with id {} isn't associated with rpc queue", queue_id);
     return true;
   }
