1ea6881d33d6f534f9d8afc779ce633e9fb2824f
Karim Shamazov <k.shamazov@vkteam.ru>
2025-11-13 12:19:01 +0300

    stash

---
+++ runtime-light/stdlib/time/datetime-interface.h
@@ -0,0 +1,16 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2022 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include "kphp/timelib/timelib.h"
+
+#include "runtime-common/core/class-instance/refcountable-php-classes.h"
+
+struct C$DateTimeInterface : public refcountable_polymorphic_php_classes_virt<> {
+  timelib_time* time{nullptr};
+
+  virtual const char* get_class() const noexcept = 0;
+  virtual int get_hash() const noexcept = 0;
+};
