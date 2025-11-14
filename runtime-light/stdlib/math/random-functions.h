8b1a001dd54d0b425d4409a4ace05df717ffd2b7
Nikita Siniachenko <n.sinyachenko@vk.team>
2025-11-14 18:41:35 +0300

    implemented f$lcg_value and f$uniqid

--- runtime-light/stdlib/math/random-functions.h
+++ runtime-light/stdlib/math/random-functions.h
@@ -1,17 +1,26 @@
 // Compiler for PHP (aka KPHP)
+// TODO change to 2025 ???????????????
 // Copyright (c) 2024 LLC «V Kontakte»
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
 #pragma once
 
+#include <array>
+#include <chrono>
+#include <cstddef>
 #include <cstdint>
+#include <format>
 #include <limits>
 #include <memory>
 #include <random>
 
+#include "runtime-common/core/runtime-core.h"
 #include "runtime-common/stdlib/math/random-functions.h"
 #include "runtime-light/k2-platform/k2-api.h"
+#include "runtime-light/state/instance-state.h"
 #include "runtime-light/stdlib/math/random-state.h"
+#include "runtime-light/stdlib/system/system-functions.h"
+#include "runtime-light/stdlib/time/util.h"
 
 inline int64_t f$mt_rand(int64_t l, int64_t r) noexcept {
   if (l > r) [[unlikely]] {
@@ -41,4 +50,58 @@ inline int64_t f$rand(int64_t l, int64_t r) noexcept {
 
 inline void f$srand(int64_t seed = std::numeric_limits<int64_t>::min()) noexcept {
   return f$mt_srand(seed);
+}
+
+double f$lcg_value() noexcept {
+  auto& random_instance_state{RandomInstanceState::get()};
+
+  // TODO extract to function in namespace random_impl_ ?
+  auto modmult{[](int32_t a, int32_t b, int32_t c, int32_t m, int32_t s) noexcept {
+    int32_t q{s / a};
+    int32_t res{b * (s - a * q) - c * q};
+    if (res < 0) {
+      res += m;
+    }
+    return res;
+  }};
+  random_instance_state.lcg1 = modmult(53668, 40014, 12211, 2147483563, random_instance_state.lcg1);
+  random_instance_state.lcg2 = modmult(52774, 40692, 3791, 2147483399, random_instance_state.lcg2);
+
+  int32_t z{random_instance_state.lcg1 - random_instance_state.lcg2};
+  if (z < 1) {
+    z += 2147483562;
+  }
+
+  return static_cast<double>(z) * 4.656613e-10;
+}
+
+inline string f$uniqid(const string& prefix, bool more_entropy) noexcept {
+  if (!more_entropy) {
+    // TODO discuss, do we really need this
+    f$usleep(1);
+  }
+
+  auto [sec, susec]{system_seconds_and_micros()};
+  constexpr size_t buf_size = 30;
+  std::array<char, buf_size> buf{};
+  RuntimeContext::get().static_SB.clean() << prefix;
+
+  if (more_entropy) {
+    // we multiply by 10 to get (0..10) value out of (0..1), because we want random digit before the point.
+    double lcg_rand_value{f$lcg_value() * 10};
+    std::format_to_n(buf.data(), buf_size, "{:08x}{:05x}{:.8f}", sec.count(), susec.count(), lcg_rand_value);
+    // TODO discuss naming and place of this constant
+    constexpr size_t rand_len = 23;
+    RuntimeContext::get().static_SB.append(buf.data(), rand_len);
+  } else {
+    std::format_to_n(buf.data(), buf_size, "{:08x}{:05x}", sec.count(), susec.count());
+    // TODO discuss naming and place of this constant
+    constexpr size_t rand_len = 13;
+    RuntimeContext::get().static_SB.append(buf.data(), rand_len);
+  }
+
+  return {
+      RuntimeContext::get().static_SB.c_str(),
+      RuntimeContext::get().static_SB.size(),
+  };
 }
--- runtime-light/stdlib/math/random-state.h
+++ runtime-light/stdlib/math/random-state.h
@@ -10,14 +10,23 @@
 
 #include "common/mixin/not_copyable.h"
 #include "runtime-light/k2-platform/k2-api.h"
+#include "runtime-light/stdlib/time/util.h"
 
 struct RandomInstanceState final : private vk::not_copyable {
   std::mt19937_64 mt_gen;
+  int32_t lcg1;
+  int32_t lcg2;
 
   RandomInstanceState() noexcept {
     uint64_t seed{};
     k2::os_rnd(sizeof(seed), std::addressof(seed));
     mt_gen = std::mt19937_64{seed};
+
+    // TODO may be replace this with k2::os_rnd ??????
+    auto [sec, susec]{system_seconds_and_micros()};
+    lcg1 = static_cast<int32_t>(sec.count()) ^ (static_cast<int32_t>(susec.count()) << 11);
+    auto [_, susec2]{system_seconds_and_micros()};
+    lcg2 = static_cast<int32_t>(k2::getpid()) ^ (static_cast<int32_t>(susec.count()) << 11);
   }
 
   static RandomInstanceState& get() noexcept;
