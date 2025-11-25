86f8fd3ad98703375b436745c3292a3e7af8bd8c
Nikita Siniachenko <n.sinyachenko@vk.team>
2025-11-25 18:47:00 +0300

    added strlen(uniqid(...)) checks to 454_langSkin.php

--- tests/phpt/dl/454_langSkin.php
+++ tests/phpt/dl/454_langSkin.php
@@ -25,10 +25,28 @@ EOF;
 var_dump (sLangBecomeTranslatorScript(1));
 var_dump (sLangAlBecomeTranslatorScript(1));
 
-/*
-var_dump (lcg_value());
-var_dump (uniqid());
-var_dump (uniqid('aba'));
-var_dump (uniqid('testtestsetsetsetsetseteest', false));
-var_dump (uniqid('', true));
-*/
+// var_dump (lcg_value());
+var_dump (strlen(uniqid()));
+var_dump (strlen(uniqid('aba')));
+var_dump (strlen(uniqid('testtestsetsetsetsetseteest', false)));
+
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
