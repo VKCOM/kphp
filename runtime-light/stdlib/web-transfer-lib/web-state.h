17156d2fd531e02db954dcc605d81a766c3e766f
Petr Shumilov <p.shumilov@vkteam.ru>
2025-11-25 11:42:34 +0300

    Add curl_multi_init
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- runtime-light/stdlib/web-transfer-lib/web-state.h
+++ runtime-light/stdlib/web-transfer-lib/web-state.h
@@ -26,7 +26,7 @@ struct WebInstanceState final : private vk::not_copyable {
   bool session_is_finished{false};
   kphp::stl::unordered_map<kphp::web::simple_transfer::descriptor_type, kphp::web::simple_transfer_config, kphp::memory::script_allocator>
       simple_transfer2config{};
-  kphp::stl::unordered_map<kphp::web::composite_transfer::descriptor_type, kphp::web::simple_transfer_config, kphp::memory::script_allocator>
+  kphp::stl::unordered_map<kphp::web::composite_transfer::descriptor_type, kphp::web::composite_transfer_config, kphp::memory::script_allocator>
       composite_transfer2config{};
 
   inline auto session_get_or_init() noexcept -> std::expected<shared_session_type, int32_t>;
