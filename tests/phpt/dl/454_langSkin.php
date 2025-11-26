bc5d1549f99e6c8f46c0c4a0a30ef0f1f8875a37
Nikita Sinyachenko <53365187+T-y-c-o-o-n@users.noreply.github.com>
2025-11-26 11:31:51 +0300

    [k2] f$uniqid (#1460)
    * implemented f$lcg_value and f$uniqid

--- tests/phpt/dl/454_langSkin.php
+++ tests/phpt/dl/454_langSkin.php
@@ -25,10 +25,28 @@ EOF;
 var_dump (sLangBecomeTranslatorScript(1));
 var_dump (sLangAlBecomeTranslatorScript(1));
 
-/*
-var_dump (lcg_value());
-var_dump (uniqid());
-var_dump (uniqid('aba'));
-var_dump (uniqid('testtestsetsetsetsetseteest', false));
-var_dump (uniqid('', true));
-*/
+// we dump strlen of uniqid result to check, whether seconds, microseconds and lcg_value are outputted to result with the necessary number of symbols
+var_dump (strlen(uniqid()));
+var_dump (strlen(uniqid('aba')));
+var_dump (strlen(uniqid('testtestsetsetsetsetseteest', false)));
+
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', false)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('', true)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', false)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
+var_dump (strlen(uniqid('my-own-prefix-', true)));
