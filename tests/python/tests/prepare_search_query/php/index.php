877b96ca21d74380cfcc077ed957e320cc061434
Nikita Siniachenko <n.sinyachenko@vk.team>
2025-11-27 19:27:09 +0300

    json data replaced with raw binary data

--- tests/python/tests/prepare_search_query/php/index.php
+++ tests/python/tests/prepare_search_query/php/index.php
@@ -2,8 +2,7 @@
 
 function main() {
   $raw_post_data = file_get_contents('php://input');
-  $post_data = json_decode($raw_post_data, $associative=true);
-  $res = prepare_search_query($post_data["post"]);
+  $res = prepare_search_query($raw_post_data);
   $resp = array("POST_BODY" => $res);
   echo json_encode($resp);
 }
