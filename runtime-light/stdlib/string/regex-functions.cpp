c614e63f3ea47b3f80b8fb6db57d0dbcd99d8b37
Karim Shamazov <k.shamazov@vkteam.ru>
2025-12-03 15:44:25 +0300

    remove const

--- runtime-light/stdlib/string/regex-functions.cpp
+++ runtime-light/stdlib/string/regex-functions.cpp
@@ -359,7 +359,7 @@ public:
   using value_type = pcre2_match_view;
   using difference_type = std::ptrdiff_t;
   using reference = value_type;
-  using pointer = const value_type*;
+  using pointer = value_type*;
   using iterator_category = std::forward_iterator_tag;
 
   pcre2_iterator() noexcept
