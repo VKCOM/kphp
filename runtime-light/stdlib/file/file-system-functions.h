068484440e62c873251f598ec812608a4b14993b
Nikita Sinyachenko <53365187+T-y-c-o-o-n@users.noreply.github.com>
2025-12-16 16:14:32 +0300

    [k2] f$file_put_contents, f$getimagesize (#1436)
    * implemented f$file_put_contents and f$getimagesize
    
    * small settings
    
    * fixed issues
    
    * make linter happy
    
    * fmt
    
    * fixed some issues
    
    * added license
    
    * fixed new issues
    
    * brace inits
    
    * added phpt tests
    
    * added image example to tests/data/images
    
    * removed util functions read_safe and write_safe
    
    * refactored 1044_files.php test
    
    * file-system-functions.cpp: added missing includes, removed direct k2_* calls, std::span<unsigned char> replaced with std::span<std::byte>...
    
    * added missing reinterpret_cast
    
    * min_size -> max_size
    
    * extracted some magic constants
    
    * inline constants in header
    
    * replace int with int32_t
    
    * pread: off_t replaced with uint64_t
    
    * atd::addressof and noexcept labmdas
    
    * kphp::memory::script::alloc used for image uniq ptr
    
    * k2::write and k2::stderr_write buffer parameter is now std::span<const std::byte>
    
    * subspan() instead of arithmetic
    
    * refactored f$getimagesize
    
    * fixed includes and types in f$getimagesize
    
    * fixed some issues
    
    * added file-system-constants with file open mode strings to image state
    
    * added new .cpp's to stdlib.cmake
    
    * removed FileSystemLibConstants
    
    ---------
    
    Co-authored-by: Nikita Siniachenko <n.sinyachenko@vk.team>

--- runtime-light/stdlib/file/file-system-functions.h
+++ runtime-light/stdlib/file/file-system-functions.h
@@ -17,10 +17,12 @@
 #include <utility>
 
 #include "runtime-common/core/runtime-core.h"
+#include "runtime-common/stdlib/array/array-functions.h"
 #include "runtime-common/stdlib/string/string-functions.h"
 #include "runtime-light/coroutine/task.h"
 #include "runtime-light/k2-platform/k2-api.h"
 #include "runtime-light/stdlib/diagnostics/logs.h"
+#include "runtime-light/stdlib/file/file-system-state.h"
 #include "runtime-light/stdlib/file/resource.h"
 #include "runtime-light/stdlib/fork/fork-functions.h"
 
@@ -191,9 +193,31 @@ inline resource f$stream_socket_client(const string& address, std::optional<std:
 }
 
 inline Optional<string> f$file_get_contents(const string& stream) noexcept {
-  if (auto sync_resource{from_mixed<class_instance<kphp::fs::sync_resource>>(f$fopen(stream, {}), {})}; !sync_resource.is_null()) {
+  if (auto sync_resource{from_mixed<class_instance<kphp::fs::sync_resource>>(f$fopen(stream, FileSystemImageState::get().READ_MODE), {})};
+      !sync_resource.is_null()) {
     auto expected{sync_resource.get()->get_contents()};
     return expected ? Optional<string>{*std::move(expected)} : Optional<string>{false};
   }
   return false;
 }
+
+inline Optional<int64_t> f$file_put_contents(const string& stream, const mixed& content_var, int64_t flags = 0) noexcept {
+  string content{content_var.is_array() ? f$implode(string{}, content_var.to_array()) : content_var.to_string()};
+
+  constexpr int64_t FILE_APPEND_FLAG{1};
+  if (flags & ~FILE_APPEND_FLAG) {
+    kphp::log::warning("flags other, than FILE_APPEND are not supported in file_put_contents");
+    flags &= FILE_APPEND_FLAG;
+  }
+
+  const auto& file_system_lib_constants{FileSystemImageState::get()};
+  const string& mode{((flags & FILE_APPEND_FLAG) != 0) ? file_system_lib_constants.APPEND_MODE : file_system_lib_constants.WRITE_MODE};
+  if (auto sync_resource{from_mixed<class_instance<kphp::fs::sync_resource>>(f$fopen(stream, mode), {})}; !sync_resource.is_null()) {
+    std::span<const char> data_span{content.c_str(), content.size()};
+    auto expected{sync_resource.get()->write(std::as_bytes(data_span))};
+    return expected ? Optional<int64_t>{static_cast<int64_t>(*std::move(expected))} : Optional<int64_t>{false};
+  }
+  return false;
+}
+
+mixed f$getimagesize(const string& name) noexcept;
