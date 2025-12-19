eacf20ffea280c11c49aa6905f98c677a8fe7944
Alexander Polyakov <al.polyakov@vk.team>
2025-12-19 13:17:40 +0300

    [k2] make io_scheduler support cancellation of already scheduled coroutine (#1490)
    This PR enhances the io_scheduler by adding support for the cancellation of coroutines that have already been scheduled. This change addresses scenarios where a coroutine might be scheduled multiple times, leading to potential issues.
    
    Let's look at the example:
    co_await kphp::coro::when_any(io_scheduler.yield(), io_scheduler.yield());
    
    In this case, the scheduler will schedule the same coroutine twice. When the coroutine is resumed for the first time, it may complete its execution and destroy its state. If the scheduler attempts to resume the coroutine again, it would be resuming a destroyed coroutine, which can lead to undefined behavior.
    
    To prevent this issue, the scheduler now tracks coroutines that have been cancelled or destroyed. It ensures that these coroutines are not resumed once they are in a cancelled or destroyed state.

--- runtime-light/coroutine/detail/poll-info.h
+++ runtime-light/coroutine/detail/poll-info.h
@@ -5,7 +5,8 @@
 #pragma once
 
 #include <coroutine>
-#include <optional>
+#include <utility>
+#include <variant>
 
 #include "runtime-common/core/allocator/script-allocator.h"
 #include "runtime-common/core/std/containers.h"
@@ -17,15 +18,25 @@ namespace kphp::coro::detail {
 struct poll_info {
   using timed_events = kphp::stl::multimap<k2::TimePoint, detail::poll_info&, kphp::memory::script_allocator>;
   using parked_polls = kphp::stl::multimap<k2::descriptor, detail::poll_info&, kphp::memory::script_allocator>;
+  // TODO: Consider using an intrusive list for scheduled_coroutines.
+  using scheduled_coroutines = kphp::stl::list<std::coroutine_handle<>, kphp::memory::script_allocator>;
+
+  // Each coroutine in the scheduler can be in one of the following states, represented by the `schedule_position` variant:
+  // 1. `std::monostate`: a coroutine has not been scheduled yet or has already been resumed by the scheduler;
+  // 2. `timed_events::iterator`: a coroutine is waiting for a timer event to occur;
+  // 3. `parked_polls::iterator`: a coroutine is waiting for an event on a non-timer descriptor;
+  // 4. `std::pair<timed_events::iterator, parked_polls::iterator>`: a coroutine is waiting for an event on either a timer or a non-timer descriptor;
+  // 5. `scheduled_coroutines::iterator`: a coroutine is scheduled and waiting to be resumed by the scheduler.
+  using schedule_position = std::variant<std::monostate, timed_events::iterator, parked_polls::iterator,
+                                         std::pair<timed_events::iterator, parked_polls::iterator>, scheduled_coroutines::iterator>;
+
+  schedule_position m_schedule_position{std::monostate{}};
 
   k2::descriptor m_descriptor{k2::INVALID_PLATFORM_DESCRIPTOR};
   std::coroutine_handle<> m_awaiting_coroutine;
-  std::optional<timed_events::iterator> m_timer_pos;
-  std::optional<parked_polls::iterator> m_parking_pos;
 
   kphp::coro::poll_status m_poll_status{kphp::coro::poll_status::error};
   kphp::coro::poll_op m_poll_op;
-  bool m_processed{};
 
   poll_info(k2::descriptor descriptor, kphp::coro::poll_op poll_op) noexcept
       : m_descriptor(descriptor),
@@ -54,6 +65,7 @@ struct poll_info {
       }
 
       auto await_resume() const noexcept -> kphp::coro::poll_status {
+        m_poll_info.m_schedule_position = std::monostate{};
         return m_poll_info.m_poll_status;
       }
     };
