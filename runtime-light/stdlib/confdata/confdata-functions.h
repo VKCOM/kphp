92f2e336b410a5b440120a4645888400580dc957
Alexander Polyakov <al.polyakov@vk.team>
2025-12-19 16:27:13 +0300

    [k2] remove references from confdata functions parameters

--- runtime-light/stdlib/confdata/confdata-functions.h
+++ runtime-light/stdlib/confdata/confdata-functions.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <utility>
+
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-light/coroutine/task.h"
 #include "runtime-light/k2-platform/k2-api.h"
@@ -14,14 +16,11 @@ inline bool f$is_confdata_loaded() noexcept {
   return k2::component_access(kphp::confdata::COMPONENT_NAME) == k2::errno_ok;
 }
 
-// SAFETY: `key` is only used before await point
-kphp::coro::task<mixed> f$confdata_get_value(const string& key) noexcept;
+kphp::coro::task<mixed> f$confdata_get_value(string key) noexcept;
 
-// SAFETY: `wildcard` is only used before await point
-kphp::coro::task<array<mixed>> f$confdata_get_values_by_any_wildcard(const string& wildcard) noexcept;
+kphp::coro::task<array<mixed>> f$confdata_get_values_by_any_wildcard(string wildcard) noexcept;
 
-// SAFETY: `f$confdata_get_values_by_any_wildcard` is coro-pass-by-ref safe
-inline kphp::coro::task<array<mixed>> f$confdata_get_values_by_predefined_wildcard(const string& wildcard) noexcept {
+inline kphp::coro::task<array<mixed>> f$confdata_get_values_by_predefined_wildcard(string wildcard) noexcept {
   kphp::log::info("k2-confdata doesn't support predefined wildcard optimization. wildcard: {}", wildcard.c_str());
-  co_return co_await f$confdata_get_values_by_any_wildcard(wildcard);
+  co_return co_await f$confdata_get_values_by_any_wildcard(std::move(wildcard));
 }
