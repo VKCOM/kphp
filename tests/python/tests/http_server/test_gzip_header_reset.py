177dd8b329de5c91ec8fe51440a166f5cdd801ea
Vadim Sadokhov <v.sadokhov@vk.team>
2026-02-11 14:45:15 +0300

    add tests

--- tests/python/tests/http_server/test_gzip_header_reset.py
+++ tests/python/tests/http_server/test_gzip_header_reset.py
@@ -14,7 +14,15 @@ class TestGzipHeaderReset(WebServerAutoTestCase):
             _ = response.headers["Content-Encoding"]
 
     def test_gzip_handler_after_reset(self):
-        response = self.gzip_request("gzip-handler-after-reset")
+        response = self.gzip_request("gzhandler-after-reset")
+        self.assertEqual(response.headers["Content-Encoding"], "gzip")
+
+    def test_deflate_handler_after_reset(self):
+        response = self.deflate_request("gzhandler-after-reset")
+        self.assertEqual(response.headers["Content-Encoding"], "deflate")
+
+    def test_gzip_handler_with_nested_buffer(self):
+        response = self.gzip_request("gzhandler-with-nested-buffer")
         self.assertEqual(response.headers["Content-Encoding"], "gzip")
 
     def test_gzip_without_handler(self):
@@ -27,6 +35,13 @@ class TestGzipHeaderReset(WebServerAutoTestCase):
         response = self.gzip_request("ignore-second-handler")
         with self.assertRaises(KeyError):
             _ = response.headers["Content-Encoding"]
+
+    def deflate_request(self, type):
+        url = "/test_script_gzip_header?type=" + type
+        return self.web_server.http_get(url, headers={
+            "Host": "localhost",
+            "Accept-Encoding": "deflate"
+        })
 
     def gzip_request(self, type):
         url = "/test_script_gzip_header?type=" + type
