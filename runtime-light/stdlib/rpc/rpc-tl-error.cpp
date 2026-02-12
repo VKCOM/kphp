be147d9ac16ec7ba2394512f4018ea2a3c373549
Karim Shamazov <k.shamazov@vkteam.ru>
2026-02-12 14:37:16 +0300

    modify rpc_server_instance_state_fetcher if error is fetched

--- runtime-light/stdlib/rpc/rpc-tl-error.cpp
+++ runtime-light/stdlib/rpc/rpc-tl-error.cpp
@@ -11,8 +11,8 @@
 #include "runtime-light/tl/tl-types.h"
 
 bool TlRpcError::try_fetch() noexcept {
-  // Copying the fetcher so we don't modify it
-  auto fetcher{RpcServerInstanceState::get().tl_fetcher};
+  auto& rpc_server_instance_state_fetcher{RpcServerInstanceState::get().tl_fetcher};
+  auto fetcher{rpc_server_instance_state_fetcher};
   const auto backup_pos{fetcher.pos()};
   if (tl::ReqResult req_result{}; req_result.fetch(fetcher) && std::holds_alternative<tl::reqResultHeader>(req_result.value)) {
     fetcher = tl::fetcher{std::get<tl::reqResultHeader>(req_result.value).result};
@@ -26,5 +26,6 @@ bool TlRpcError::try_fetch() noexcept {
   auto& rpc_req_error{std::get<tl::rpcReqError>(rpc_req_result.value)};
   error_code = rpc_req_error.error_code.value;
   error_msg = {rpc_req_error.error.value.data(), static_cast<string::size_type>(rpc_req_error.error.value.size())};
+  rpc_server_instance_state_fetcher = std::move(fetcher);
   return true;
 }
