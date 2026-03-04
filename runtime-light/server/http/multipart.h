7df626cae3a46e69cdabf8a872c83ac96c69de3f
Vadim Sadokhov <v.sadokhov@vk.team>
2026-03-04 15:53:43 +0300

    wip

--- runtime-light/server/http/multipart.h
+++ runtime-light/server/http/multipart.h
@@ -1,15 +1,16 @@
 // Compiler for PHP (aka KPHP)
-// Copyright (c) 2024 LLC «V Kontakte»
+// Copyright (c) 2026 LLC «V Kontakte»
 // Distributed under the GPL v3 License, see LICENSE.notice.txt
 
+#include <optional>
 #include <string_view>
 
 #include "runtime-common/core/runtime-core.h"
 
-namespace kphp::http {
+namespace kphp::http::multipart {
 
-void parse_multipart(const std::string_view body, const std::string_view boundary, mixed &v$_POST, mixed &v$_FILES);
+void parse_multipart(std::string_view body, std::string_view boundary, mixed& v$_POST, mixed& v$_FILES);
 
-std::optional<std::string_view> parse_boundary(const std::string_view content_type);
+std::optional<std::string_view> extract_boundary(std::string_view content_type) noexcept;
 
-} // namespace kphp::http
+} // namespace kphp::http::multipart
