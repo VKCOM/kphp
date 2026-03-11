a51b7f7fc48dec61beca8b5fb1adfca39ef67e19
Karim Shamazov <k.shamazov@vkteam.ru>
2026-03-11 18:05:00 +0300

    fix RUST_LOG

--- tests/python/lib/kphp_run_once.py
+++ tests/python/lib/kphp_run_once.py
@@ -149,7 +149,7 @@ class KphpRunOnce(KphpBuilder):
 
         env = os.environ.copy()
         if "RUST_LOG" not in env:
-            env["RUST_LOG"] = "Debug"
+            env["RUST_LOG"] = "Warn,component-log=Debug"
 
         k2_runtime_proc = subprocess.Popen(cmd,
                                            cwd=self._kphp_runtime_tmp_dir,
