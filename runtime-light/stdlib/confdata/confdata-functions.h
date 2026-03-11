995cb48bfdf2a771eac34b1c5628b3d9a365321b
Alexander Polyakov <al.polyakov@vk.team>
2026-03-11 17:00:52 +0300

    [k2] remove info log from confdata_get_values_by_any_wildcard

--- runtime-light/stdlib/confdata/confdata-functions.h
+++ runtime-light/stdlib/confdata/confdata-functions.h
@@ -10,7 +10,6 @@
 #include "runtime-light/coroutine/task.h"
 #include "runtime-light/k2-platform/k2-api.h"
 #include "runtime-light/stdlib/confdata/confdata-constants.h"
-#include "runtime-light/stdlib/diagnostics/logs.h"
 
 inline bool f$is_confdata_loaded() noexcept {
   return k2::component_access(kphp::confdata::COMPONENT_NAME) == k2::errno_ok;
@@ -21,6 +20,5 @@ kphp::coro::task<mixed> f$confdata_get_value(string key) noexcept;
 kphp::coro::task<array<mixed>> f$confdata_get_values_by_any_wildcard(string wildcard) noexcept;
 
 inline kphp::coro::task<array<mixed>> f$confdata_get_values_by_predefined_wildcard(string wildcard) noexcept {
-  kphp::log::info("k2-confdata doesn't support predefined wildcard optimization. wildcard: {}", wildcard.c_str());
   co_return co_await f$confdata_get_values_by_any_wildcard(std::move(wildcard));
 }
