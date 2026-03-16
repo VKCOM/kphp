001012bc7a95b4908feb3c67dde95f9b9175e185
Alexander Polyakov <al.polyakov@vk.team>
2026-03-16 16:54:46 +0300

    minor fix

--- runtime-light/stdlib/string/mbstring-functions.h
+++ runtime-light/stdlib/string/mbstring-functions.h
@@ -8,18 +8,18 @@
 
 #include "runtime-common/core/runtime-core.h"
 #include "runtime-common/stdlib/string/mbstring-functions.h"
-#include "runtime-common/stdlib/string/string-context.h"
 #include "runtime-light/stdlib/diagnostics/logs.h"
+#include "runtime-light/stdlib/string/string-state.h"
 
-inline string f$mb_strtolower(const string& str, const string& encoding = StringLibConstants::get().CP1251_STR) noexcept {
+inline string f$mb_strtolower(const string& str, const string& encoding = StringImageState::get().CP1251_STR) noexcept {
   return mb_strtolower_impl(str, [](bool condition) noexcept { kphp::log::assertion(condition); }, encoding);
 }
 
-inline string f$mb_strtoupper(const string& str, const string& encoding = StringLibConstants::get().CP1251_STR) noexcept {
+inline string f$mb_strtoupper(const string& str, const string& encoding = StringImageState::get().CP1251_STR) noexcept {
   return mb_strtoupper_impl(str, [](bool condition) noexcept { kphp::log::assertion(condition); }, encoding);
 }
 
 inline Optional<int64_t> f$mb_stripos(const string& haystack, const string& needle, int64_t offset = 0,
-                                      const string& encoding = StringLibConstants::get().CP1251_STR) noexcept {
+                                      const string& encoding = StringImageState::get().CP1251_STR) noexcept {
   return mb_stripos_impl(haystack, needle, [](bool condition) noexcept { kphp::log::assertion(condition); }, offset, encoding);
 }
