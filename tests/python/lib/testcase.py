19941166ccd013c0dc2f98ab7e772abcdbe2b480
Karim Shamazov <k.shamazov@vkteam.ru>
2026-03-16 16:29:10 +0300

    fixes

--- tests/python/lib/testcase.py
+++ tests/python/lib/testcase.py
@@ -52,10 +52,10 @@ class BaseTestCase(TestCase):
         kphp_server_working_dir: pathlib.Path,
         artifacts_dir: pathlib.Path,
     ):
-        request.cls.kphp_build_working_dir = working_dir
-        request.cls.web_server_working_dir = kphp_server_working_dir
-        request.cls.artifacts_dir = artifacts_dir
-        request.cls.test_dir = request.path.parent
+        request.cls.kphp_build_working_dir = str(working_dir)
+        request.cls.web_server_working_dir = str(kphp_server_working_dir)
+        request.cls.artifacts_dir = str(artifacts_dir)
+        request.cls.test_dir = str(request.path.parent)
 
     @pytest.fixture(scope="class", autouse=True)
     def _base_setup(self, request: pytest.FixtureRequest, setup_tmp_folder):
