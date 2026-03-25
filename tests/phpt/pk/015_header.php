5998e5b174217b5419e592c77c563608b30540b4
Petr Shumilov <p.shumilov@vkteam.ru>
2026-03-25 16:43:05 +0300

    Enable and refactor test
    Signed-off-by: Petr Shumilov <p.shumilov@vkteam.ru>

--- tests/phpt/pk/015_header.php
+++ tests/phpt/pk/015_header.php
@@ -1,4 +1,4 @@
-@ok k2_skip
+@ok
 <?php
 
 header('X-TEST-1: value1');
@@ -11,18 +11,23 @@ header('X-TEST-3: value3', true);
 header('Date: haha, date in tests');
 setcookie('test-cookie', 'blabla');
 $x = headers_list();
+foreach ($x as $_ => &$v) {
+    $v = strtolower($v);
+}
+sort($x);
+
 
 #ifndef KPHP
 // not working in console php
 $x = [
-  'Server: nginx/0.3.33',
-  'Date: haha, date in tests',
-  'Content-Type: text/html; charset=windows-1251',
-  'X-TEST-1: value2', 
-  'X-TEST-2: value1', 
-  'X-TEST-2: value2', 
-  'X-TEST-3: value3', 
-  'Set-Cookie: test-cookie=blabla'
+    'content-type: text/html; charset=windows-1251',
+    'date: haha, date in tests',
+    'server: nginx/0.3.33',
+    'set-cookie: test-cookie=blabla',
+    'x-test-1: value2',
+    'x-test-2: value1',
+    'x-test-2: value2',
+    'x-test-3: value3'
 ];
 #endif
 
