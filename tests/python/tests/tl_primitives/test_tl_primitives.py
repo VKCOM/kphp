809783fbf6f89c9194a7e6e4dfb8aaeb5c95b83f
Karim Shamazov <k.shamazov@vkteam.ru>
2026-03-31 09:56:29 +0300

    add _assert prefix

--- tests/python/tests/tl_primitives/test_tl_primitives.py
+++ tests/python/tests/tl_primitives/test_tl_primitives.py
@@ -11,23 +11,23 @@ def _operation(kind: str, kwargs: typing.Dict[str, typing.Any], expected_result:
     }
 
 
-def _rpc_parse(new_rpc_data: str, expected_result: bool):
+def _assert_rpc_parse(new_rpc_data: str, expected_result: bool):
     return _operation('rpc_parse', dict(new_rpc_data=new_rpc_data), expected_result)
 
 
-def _fetch_int(expected_result: int):
+def _assert_fetch_int(expected_result: int):
     return _operation('fetch_int', dict(), expected_result)
 
 
-def _fetch_long(expected_result: int):
+def _assert_fetch_long(expected_result: int):
     return _operation('fetch_long', dict(), expected_result)
 
 
-def _fetch_double(expected_result: float):
+def _assert_fetch_double(expected_result: float):
     return _operation('fetch_double', dict(), expected_result)
 
 
-def _fetch_string(expected_result: str):
+def _assert_fetch_string(expected_result: str):
     return _operation('fetch_string', dict(), expected_result)
 
 
@@ -39,9 +39,11 @@ class TestTlPrimitives(WebServerAutoTestCase):
 
     def test_rpc_parse(self):
         self.call(
-            _rpc_parse('\x12\x34\x56\x78\x42\x00\x00\x00\x00\x00\x00\x00\x68\x56\x5b\x56\x06\x22\x09\x40\x03abc', True),
-            _fetch_int(0x78563412),
-            _fetch_long(0x42),
-            _fetch_double(3.14161365),
-            _fetch_string('abc'),
+            _assert_rpc_parse(
+                '\x12\x34\x56\x78\x42\x00\x00\x00\x00\x00\x00\x00\x68\x56\x5b\x56\x06\x22\x09\x40\x03abc', True
+            ),
+            _assert_fetch_int(0x78563412),
+            _assert_fetch_long(0x42),
+            _assert_fetch_double(3.14161365),
+            _assert_fetch_string('abc'),
         )
