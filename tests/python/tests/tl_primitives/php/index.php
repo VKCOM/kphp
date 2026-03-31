e9ba925a37aeea926e7a9e22bd67b51b8f8199e4
Karim Shamazov <k.shamazov@vkteam.ru>
2026-03-31 10:22:56 +0300

    add rpc_clean test

--- tests/python/tests/tl_primitives/php/index.php
+++ tests/python/tests/tl_primitives/php/index.php
@@ -3,35 +3,46 @@
 $operations = json_decode(file_get_contents('php://input'));
 
 foreach ($operations as $op) {
-  switch ($op['kind']) {
-    case 'rpc_parse':
-      if (rpc_parse($op['new_rpc_data']) != $op['expected']) {
-        critical_error('error');
-      }
-      break;
-    case 'fetch_int':
-      if (fetch_int() != $op['expected']) {
-        critical_error('error');
-      }
-      break;
-    case 'fetch_long':
-      if (fetch_long() != $op['expected']) {
-        critical_error('error');
-      }
-      break;
-    case 'fetch_double':
-      if (fetch_double() != $op['expected']) {
-        critical_error('error');
-      }
-      break;
-    case 'fetch_string':
-      if (fetch_string() != $op['expected']) {
-        critical_error('error');
-      }
-      break;
-    default:
-      critical_error('unknown op ' . $op['kind']);
-  };
+  try {
+    switch ($op['kind']) {
+      case 'rpc_parse':
+        if (rpc_parse($op['new_rpc_data']) != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      case 'rpc_clean':
+        if (rpc_clean() != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      case 'fetch_int':
+        if (fetch_int() != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      case 'fetch_long':
+        if (fetch_long() != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      case 'fetch_double':
+        if (fetch_double() != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      case 'fetch_string':
+        if (fetch_string() != $op['expected']) {
+          critical_error('error');
+        }
+        break;
+      default:
+        critical_error('unknown op ' . $op['kind']);
+    }
+  } catch (Exception $e) {
+    if (!$op['expected_exception']) {
+      throw $e;
+    }
+  }
 }
 
 echo 'ok';
--- tests/python/tests/tl_primitives/test_tl_primitives.py
+++ tests/python/tests/tl_primitives/test_tl_primitives.py
@@ -3,32 +3,41 @@ import typing
 from python.lib.testcase import WebServerAutoTestCase
 
 
-def _operation(kind: str, kwargs: typing.Dict[str, typing.Any], expected_result: typing.Any):
+def _operation(
+        kind: str, kwargs: typing.Dict[str, typing.Any], expected_result: typing.Any, expected_exception: bool = False
+):
     return {
         "kind": kind,
         **kwargs,
         "expected": expected_result,
+        "expected_exception": expected_exception,
     }
 
 
-def _assert_rpc_parse(new_rpc_data: str, expected_result: bool):
-    return _operation('rpc_parse', dict(new_rpc_data=new_rpc_data), expected_result)
+def _assert_rpc_parse(new_rpc_data: str, expected_result: bool, expected_exception: bool = False):
+    return _operation(
+        'rpc_parse', dict(new_rpc_data=new_rpc_data), expected_result, expected_exception=expected_exception
+    )
 
 
-def _assert_fetch_int(expected_result: int):
-    return _operation('fetch_int', dict(), expected_result)
+def _assert_rpc_clean(expected_result: bool):
+    return _operation('rpc_clean', dict(), expected_result)
 
 
-def _assert_fetch_long(expected_result: int):
-    return _operation('fetch_long', dict(), expected_result)
+def _assert_fetch_int(expected_result: int, expected_exception: bool = False):
+    return _operation('fetch_int', dict(), expected_result, expected_exception=expected_exception)
 
 
-def _assert_fetch_double(expected_result: float):
-    return _operation('fetch_double', dict(), expected_result)
+def _assert_fetch_long(expected_result: int, expected_exception: bool = False):
+    return _operation('fetch_long', dict(), expected_result, expected_exception=expected_exception)
 
 
-def _assert_fetch_string(expected_result: str):
-    return _operation('fetch_string', dict(), expected_result)
+def _assert_fetch_double(expected_result: float, expected_exception: bool = False):
+    return _operation('fetch_double', dict(), expected_result, expected_exception=expected_exception)
+
+
+def _assert_fetch_string(expected_result: str, expected_exception: bool = False):
+    return _operation('fetch_string', dict(), expected_result, expected_exception=expected_exception)
 
 
 class TestTlPrimitives(WebServerAutoTestCase):
@@ -37,7 +46,7 @@ class TestTlPrimitives(WebServerAutoTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "ok")
 
-    def test_rpc_parse(self):
+    def test_fetch(self):
         self.call(
             _assert_rpc_parse(
                 '\x12\x34\x56\x78\x42\x00\x00\x00\x00\x00\x00\x00\x68\x56\x5b\x56\x06\x22\x09\x40\x03abc', True
@@ -46,4 +55,11 @@ class TestTlPrimitives(WebServerAutoTestCase):
             _assert_fetch_long(0x42),
             _assert_fetch_double(3.14161365),
             _assert_fetch_string('abc'),
+        )
+
+    def test_rpc_clean(self):
+        self.call(
+            _assert_rpc_parse('\x12\x34\x56\x78', True),
+            _assert_rpc_clean(True),
+            _assert_fetch_int(0x777, expected_exception=True),
         )
