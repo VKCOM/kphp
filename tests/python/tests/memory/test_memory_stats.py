50b480de517617b02030ac972b95416310d0a76b
Karim Shamazov <k.shamazov@vkteam.ru>
2026-04-06 21:36:11 +0300

    pass args with keys

--- tests/python/tests/memory/test_memory_stats.py
+++ tests/python/tests/memory/test_memory_stats.py
@@ -17,22 +17,24 @@ class TestMemoryUsage(WebServerAutoTestCase):
         )
 
     def test_memory_usage(self):
-        self._template("test_memory_usage", 2**20, 0, 2**19 + 2**20)
+        self._template(
+            "test_memory_usage", expected_usage=2**20, expected_usage_after_unset=0, expected_peak_usage=2**19 + 2**20
+        )
 
     @pytest.mark.k2_skip
     def test_total_memory_usage_kphp(self):
         self._template(
             "test_total_memory_usage",
-            48 + sum(2**n for n in range(12, 21)),
-            48 + sum(2**n for n in range(12, 20)),
-            48 + sum(2**n for n in range(12, 21)),
+            expected_usage=48 + sum(2**n for n in range(12, 21)),
+            expected_usage_after_unset=48 + sum(2**n for n in range(12, 20)),
+            expected_peak_usage=48 + sum(2**n for n in range(12, 21)),
         )
 
     @pytest.mark.kphp_skip
     def test_total_memory_usage_k2(self):
         self._template(
             "test_total_memory_usage",
-            96 + sum(2**n for n in range(12, 21)),
-            96 + sum(2**n for n in range(12, 20)),
-            96 + sum(2**n for n in range(12, 21)),
+            expected_usage=96 + sum(2**n for n in range(12, 21)),
+            expected_usage_after_unset=96 + sum(2**n for n in range(12, 20)),
+            expected_peak_usage=96 + sum(2**n for n in range(12, 21)),
         )
