3aa0943a92580c97726c76f529bd950a6732fb2f
Alexander Polyakov <al.polyakov@vk.team>
2026-05-21 19:13:12 +0300

    [k2] use initial-exec TLS model for performance critical thread locals (#1629)

--- runtime-light/runtime-light.cpp
+++ runtime-light/runtime-light.cpp
@@ -7,6 +7,7 @@
 #include "runtime-light/core/globals/php-init-scripts.h"
 #include "runtime-light/coroutine/io-scheduler.h"
 #include "runtime-light/k2-platform/k2-api.h"
+#include "runtime-light/k2-platform/k2-header.h"
 #include "runtime-light/state/component-state.h"
 #include "runtime-light/state/image-state.h"
 #include "runtime-light/state/instance-state.h"
@@ -15,43 +16,53 @@
 #define VISIBILITY_DEFAULT __attribute__((visibility("default")))
 
 VISIBILITY_DEFAULT ImageState* k2_create_image() {
-  kphp::log::debug("start image state creation, requested {} bytes", sizeof(ImageState));
-  auto* image_state_ptr{static_cast<ImageState*>(k2::alloc(sizeof(ImageState)))};
-  kphp::log::assertion(image_state_ptr != nullptr); // Not enough memory for image state
-  kphp::log::debug("finish image state creation");
-  return image_state_ptr;
+  k2::details::image_state_ptr = nullptr;
+  k2::details::component_state_ptr = nullptr;
+  k2::details::instance_state_ptr = nullptr;
+  return static_cast<ImageState*>(k2::alloc_align(sizeof(ImageState), alignof(ImageState)));
 }
 
 VISIBILITY_DEFAULT void k2_init_image() {
-  kphp::log::debug("start image state init");
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = nullptr;
+  k2::details::instance_state_ptr = nullptr;
   new (const_cast<ImageState*>(k2::image_state())) ImageState{};
   init_php_scripts_once_in_master();
-  kphp::log::debug("finish image state init");
 }
 
 VISIBILITY_DEFAULT ComponentState* k2_create_component() {
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = nullptr;
+  k2::details::instance_state_ptr = nullptr;
   kphp::log::debug("start component state creation, requested {} bytes", sizeof(ComponentState));
-  auto* component_state_ptr{static_cast<ComponentState*>(k2::alloc(sizeof(ComponentState)))};
-  kphp::log::assertion(component_state_ptr != nullptr); // Not enough memory for component state
+  auto* component_state_ptr{static_cast<ComponentState*>(k2::alloc_align(sizeof(ComponentState), alignof(ComponentState)))};
   kphp::log::debug("finish component state creation");
   return component_state_ptr;
 }
 
 VISIBILITY_DEFAULT void k2_init_component() {
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = k2_component_state();
+  k2::details::instance_state_ptr = nullptr;
   kphp::log::debug("start component state init");
   new (const_cast<ComponentState*>(k2::component_state())) ComponentState{};
   kphp::log::debug("finish component state init");
 }
 
 VISIBILITY_DEFAULT InstanceState* k2_create_instance() {
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = k2_component_state();
+  k2::details::instance_state_ptr = nullptr;
   kphp::log::debug("start instance state creation, requested {} bytes", sizeof(InstanceState));
-  auto* instance_state_ptr{static_cast<InstanceState*>(k2::alloc(sizeof(InstanceState)))};
-  kphp::log::assertion(instance_state_ptr != nullptr); // Not enough memory for instance state
+  auto* instance_state_ptr{static_cast<InstanceState*>(k2::alloc_align(sizeof(InstanceState), alignof(InstanceState)))};
   kphp::log::debug("finish instance state creation");
   return instance_state_ptr;
 }
 
 VISIBILITY_DEFAULT void k2_init_instance() {
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = k2_component_state();
+  k2::details::instance_state_ptr = k2_instance_state();
   kphp::log::debug("start instance state init");
   new (k2::instance_state()) InstanceState{};
   k2::instance_state()->init_script_execution();
@@ -59,6 +70,9 @@ VISIBILITY_DEFAULT void k2_init_instance() {
 }
 
 VISIBILITY_DEFAULT k2::PollStatus k2_poll() {
+  k2::details::image_state_ptr = k2_image_state();
+  k2::details::component_state_ptr = k2_component_state();
+  k2::details::instance_state_ptr = k2_instance_state();
   kphp::log::debug("k2_poll started");
   const auto poll_status{kphp::coro::io_scheduler::get().process_events()};
   kphp::log::debug("k2_poll finished: {}", std::to_underlying(poll_status));
