0608cf2d2ed6471c9ce762ff064b7c1e6324aa03
Denis Zubarev <denis.zubarev@vkteam.ru>
2026-06-21 14:55:45 +0300

    rename

--- runtime-light/stdlib/diagnostics/metrics.h
+++ runtime-light/stdlib/diagnostics/metrics.h
@@ -13,7 +13,7 @@
 #include "runtime-common/core/std/containers.h"
 #include "runtime-light/k2-platform/k2-api.h"
 
-struct MetricsBuffer final : vk::movable_only {
+struct MetricBuilder final : vk::movable_only {
 private:
   using bytes_vector = kphp::stl::vector<uint8_t, kphp::memory::script_allocator>;
 
@@ -21,9 +21,9 @@ private:
   kphp::stl::unordered_map<std::string, std::string, kphp::memory::script_allocator> tags;
   size_t msg_size{0};
 
-  explicit MetricsBuffer(std::string_view metric_name) noexcept
+  explicit MetricBuilder(std::string_view metric_name) noexcept
       : metric_name{metric_name} {
-    this->msg_size += MetricsBuffer::string_sizeof(metric_name);
+    this->msg_size += MetricBuilder::string_sizeof(metric_name);
   }
 
   template<typename T>
@@ -34,16 +34,16 @@ private:
   }
 
   static void store_string(bytes_vector& buf, const std::string_view& string) noexcept {
-    MetricsBuffer::store_number(buf, string.size());
+    MetricBuilder::store_number(buf, string.size());
     buf.insert(buf.end(), string.begin(), string.end());
   }
 
   void store_msg(bytes_vector& buf) const noexcept {
-    MetricsBuffer::store_number(buf, this->msg_size);
-    MetricsBuffer::store_string(buf, std::string_view{this->metric_name.c_str(), this->metric_name.size()});
+    MetricBuilder::store_number(buf, this->msg_size);
+    MetricBuilder::store_string(buf, std::string_view{this->metric_name.c_str(), this->metric_name.size()});
     for (const auto& [tag_name, tag_value] : this->tags) {
-      MetricsBuffer::store_string(buf, std::string_view{tag_name.c_str(), tag_name.size()});
-      MetricsBuffer::store_string(buf, std::string_view{tag_value.c_str(), tag_value.size()});
+      MetricBuilder::store_string(buf, std::string_view{tag_name.c_str(), tag_name.size()});
+      MetricBuilder::store_string(buf, std::string_view{tag_value.c_str(), tag_value.size()});
     }
   }
 
@@ -56,13 +56,13 @@ private:
   }
 
 public:
-  static MetricsBuffer metric(std::string_view metric_name) noexcept {
-    return MetricsBuffer{metric_name};
+  static MetricBuilder metric(std::string_view metric_name) noexcept {
+    return MetricBuilder{metric_name};
   }
 
-  MetricsBuffer& tag(std::string_view tag_name, std::string_view tag_value) noexcept {
+  MetricBuilder& tag(std::string_view tag_name, std::string_view tag_value) noexcept {
     this->tags[std::string{tag_name}] = std::string{tag_value};
-    this->msg_size += MetricsBuffer::string_sizeof(tag_name) + MetricsBuffer::string_sizeof(tag_value);
+    this->msg_size += MetricBuilder::string_sizeof(tag_name) + MetricBuilder::string_sizeof(tag_value);
     return *this;
   }
 
@@ -71,11 +71,11 @@ public:
     buf.reserve(sizeof(uint32_t) + sizeof(uint32_t) + sizeof(double) + sizeof(size_t) +
                 this->msg_size); // timestamp_u32 + value_mask_u32 + value_f64 + msg_size_usize + msg_len
 
-    uint32_t s_timestamp{timestamp.value_or(MetricsBuffer::s_timestamp_now())};
+    uint32_t s_timestamp{timestamp.value_or(MetricBuilder::s_timestamp_now())};
 
-    MetricsBuffer::store_number(buf, s_timestamp);
-    MetricsBuffer::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::VALUE_MASK));
-    MetricsBuffer::store_number(buf, value);
+    MetricBuilder::store_number(buf, s_timestamp);
+    MetricBuilder::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::VALUE_MASK));
+    MetricBuilder::store_number(buf, value);
     this->store_msg(buf);
     return buf;
   }
@@ -86,13 +86,13 @@ public:
     buf.reserve(sizeof(uint32_t) + sizeof(uint32_t) + sizeof(size_t) + sizeof(double) * values.size() + sizeof(size_t) +
                 this->msg_size); // timestamp_u32 + value_mask_u32 + array_len_usize + value_f64*array_len + msg_size_usize + msg_len
 
-    uint32_t s_timestamp{timestamp.value_or(MetricsBuffer::s_timestamp_now())};
+    uint32_t s_timestamp{timestamp.value_or(MetricBuilder::s_timestamp_now())};
 
-    MetricsBuffer::store_number(buf, s_timestamp);
-    MetricsBuffer::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::VALUES_ARRAY_MASK));
-    MetricsBuffer::store_number(buf, values.size());
+    MetricBuilder::store_number(buf, s_timestamp);
+    MetricBuilder::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::VALUES_ARRAY_MASK));
+    MetricBuilder::store_number(buf, values.size());
     for (const auto& value : values) {
-      MetricsBuffer::store_number(buf, value);
+      MetricBuilder::store_number(buf, value);
     }
     this->store_msg(buf);
     return buf;
@@ -103,11 +103,11 @@ public:
     buf.reserve(sizeof(uint32_t) + sizeof(uint32_t) + sizeof(double) + sizeof(size_t) +
                 this->msg_size); // timestamp_u32 + value_mask_u32 + count_f64 + msg_size_usize + msg_len
 
-    uint32_t s_timestamp{timestamp.value_or(MetricsBuffer::s_timestamp_now())};
+    uint32_t s_timestamp{timestamp.value_or(MetricBuilder::s_timestamp_now())};
 
-    MetricsBuffer::store_number(buf, s_timestamp);
-    MetricsBuffer::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::COUNT_MASK));
-    MetricsBuffer::store_number(buf, count);
+    MetricBuilder::store_number(buf, s_timestamp);
+    MetricBuilder::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::COUNT_MASK));
+    MetricBuilder::store_number(buf, count);
     this->store_msg(buf);
     return buf;
   }
@@ -116,10 +116,10 @@ public:
     bytes_vector buf{};
     buf.reserve(sizeof(uint32_t) + sizeof(uint32_t) + sizeof(size_t) + this->msg_size); // timestamp_u32 + value_mask_u32 + msg_size_usize + msg_len
 
-    uint32_t s_timestamp{timestamp.value_or(MetricsBuffer::s_timestamp_now())};
+    uint32_t s_timestamp{timestamp.value_or(MetricBuilder::s_timestamp_now())};
 
-    MetricsBuffer::store_number(buf, s_timestamp);
-    MetricsBuffer::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::INC_MASK));
+    MetricBuilder::store_number(buf, s_timestamp);
+    MetricBuilder::store_number(buf, static_cast<uint32_t>(k2::MetricValueMask::INC_MASK));
     this->store_msg(buf);
     return buf;
   }
