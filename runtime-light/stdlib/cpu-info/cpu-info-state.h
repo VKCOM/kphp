5e9260380c62e6e325620d6715caaa23c2ad9fe5
Denis Zubarev <denis.zubarev@vkteam.ru>
2026-07-07 13:14:13 +0300

    added sending of metrics

--- runtime-light/stdlib/cpu-info/cpu-info-state.h
+++ runtime-light/stdlib/cpu-info/cpu-info-state.h
@@ -5,40 +5,34 @@
 #pragma once
 
 #include <cstdint>
+
+#if defined(__x86_64__) || defined(__i386__)
 #include <x86intrin.h>
+#endif
 
 #include "common/containers/final_action.h"
-#include "runtime-light/stdlib/diagnostics/logs.h"
 
 class CpuInfoInstanceState {
+private:
+  [[gnu::always_inline]] static uint64_t rdtsc() noexcept {
+#if defined(__x86_64__) || defined(__i386__)
+    return __rdtsc();
+#else
+    return 0;
+#endif
+  }
+
 public:
   CpuInfoInstanceState() noexcept = default;
 
   static CpuInfoInstanceState& get() noexcept;
 
-  [[gnu::always_inline]] static uint64_t rdtsc() noexcept {
-    return __rdtsc();
-  }
-
-  static auto write_cycles(uint64_t& dist) noexcept {
+  [[nodiscard]] static auto write_cycles(uint64_t& cycles_accumulator) noexcept {
     uint64_t start{CpuInfoInstanceState::rdtsc()};
-    return vk::final_action([start, &dist]() noexcept { dist += CpuInfoInstanceState::rdtsc() - start; });
+    return vk::final_action([start, &cycles_accumulator]() noexcept { cycles_accumulator += CpuInfoInstanceState::rdtsc() - start; });
   }
 
-  double get_percent(uint64_t cycles) const noexcept {
-    if (cycles > total_cycles) {
-      kphp::log::warning("CpuInfo. Something wrong: cycles {} > total_cycles {}", cycles, total_cycles);
-    }
-    return 100.0 * cycles / total_cycles;
-  }
-
-  void init() noexcept {
-    total_cycles = 0;
-    coro_alloc_cycles = 0;
-    coro_free_cycles = 0;
-  }
-
-  uint64_t total_cycles;
-  uint64_t coro_alloc_cycles;
-  uint64_t coro_free_cycles;
+  uint64_t processing_cycles{0};
+  uint64_t coro_alloc_cycles{0};
+  uint64_t coro_free_cycles{0};
 };
