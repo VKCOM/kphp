64e53880b9ce7fa80af10edbb45958cb95a26454
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-13 17:01:59 +0300

    use universal initialization for variable in array_all function

--- runtime-light/stdlib/array/array-functions.h
+++ runtime-light/stdlib/array/array-functions.h
@@ -367,7 +367,7 @@ template<class T, class F>
 requires std::is_invocable_v<F, T, typename array<T>::key_type> &&
          std::same_as<kphp::coro::async_function_return_type_t<F, T, typename array<T>::key_type>, bool>
 kphp::coro::task<bool> f$array_all(array<T> a, F f) noexcept {
-  bool result = false;
+  bool result{false};
   for (const auto& it : std::as_const(a)) {
     if constexpr (kphp::coro::is_async_function_v<F, T, typename array<T>::key_type>) {
       result = co_await std::invoke(f, it.get_value(), it.get_key());
