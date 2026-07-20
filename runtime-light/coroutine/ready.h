93db591a439daeb28f93af2834a7cb2289f77b62
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-20 17:53:44 +0300

    implement ready<T> awaiter

---
+++ runtime-light/coroutine/ready.h
@@ -0,0 +1,56 @@
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2026 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include <coroutine>
+#include <utility>
+
+namespace kphp::coro {
+
+template<typename T>
+class ready final {
+private:
+  T m_val;
+
+public:
+  constexpr explicit ready(T val) noexcept
+      : m_val(std::move(val)) {}
+
+  ready(const ready& other) = delete;
+  constexpr ready(ready&& other) noexcept = default;
+
+  ready& operator=(const ready& other) = delete;
+  constexpr ready& operator=(ready&& other) noexcept = default;
+
+  constexpr ~ready() = default;
+
+  constexpr auto await_ready() const noexcept -> bool {
+    return true;
+  }
+
+  constexpr auto await_suspend(std::coroutine_handle<> /*unused*/) const noexcept -> void {
+    std::unreachable();
+  }
+
+  constexpr auto await_resume() noexcept -> T {
+    return std::move(m_val);
+  }
+};
+
+template<>
+class ready<void> final {
+public:
+  constexpr auto await_ready() const noexcept -> bool {
+    return true;
+  }
+
+  constexpr auto await_suspend(std::coroutine_handle<> /*unused*/) const noexcept -> void {
+    std::unreachable();
+  }
+
+  constexpr auto await_resume() noexcept -> void {}
+};
+
+} // namespace kphp::coro
