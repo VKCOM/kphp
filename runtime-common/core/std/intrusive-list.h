7e676645d651bbc2f115544a91eefb3cca9124aa
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-23 19:52:01 +0300

    move definition of hook_base methods to cpp file

--- runtime-common/core/std/intrusive-list.h
+++ runtime-common/core/std/intrusive-list.h
@@ -17,57 +17,24 @@ class hook_base {
   hook_base* m_prev{this};
   hook_base* m_next{this};
 
-  void move_hook(hook_base&& other) noexcept {
-    assert(!is_linked());
-    if (other.is_linked()) {
-      m_prev = std::exchange(other.m_prev, std::addressof(other));
-      m_next = std::exchange(other.m_next, std::addressof(other));
-
-      m_prev->m_next = this;
-      m_next->m_prev = this;
-    }
-  }
+  void move_hook(hook_base&& other) noexcept;
 
 public:
   hook_base() noexcept = default;
 
-  hook_base(const hook_base& /*unused*/) noexcept
-      : hook_base() {}
+  hook_base(const hook_base& /*unused*/) noexcept;
 
-  hook_base(hook_base&& other) noexcept {
-    move_hook(std::move(other));
-  }
+  hook_base(hook_base&& other) noexcept;
 
-  hook_base& operator=(const hook_base& /*unused*/) noexcept {
-    return *this;
-  }
+  hook_base& operator=(const hook_base& /*unused*/) noexcept;
 
-  hook_base& operator=(hook_base&& other) noexcept {
-    if (this == std::addressof(other)) {
-      return *this;
-    }
+  hook_base& operator=(hook_base&& other) noexcept;
 
-    unlink();
-    move_hook(std::move(other));
+  ~hook_base();
 
-    return *this;
-  }
+  bool is_linked() const noexcept;
 
-  ~hook_base() {
-    unlink();
-  }
-
-  bool is_linked() const noexcept {
-    return m_prev != this;
-  }
-
-  void unlink() noexcept {
-    m_prev->m_next = m_next;
-    m_next->m_prev = m_prev;
-
-    m_prev = this;
-    m_next = this;
-  }
+  void unlink() noexcept;
 };
 
 } // namespace details
