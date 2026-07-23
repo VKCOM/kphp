6f8bcf6ec6df90f5954202c869b28e52f1c15ef6
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-23 20:02:13 +0300

    apply code style

--- runtime-common/core/std/intrusive-list.cpp
+++ runtime-common/core/std/intrusive-list.cpp
@@ -2,7 +2,7 @@
 
 namespace kphp::stl::intrusive_list::details {
 
-void hook_base::move_hook(hook_base&& other) noexcept {
+auto hook_base::move_hook(hook_base&& other) noexcept -> void {
   assert(!is_linked());
   if (other.is_linked()) {
     m_prev = std::exchange(other.m_prev, std::addressof(other));
@@ -20,11 +20,11 @@ hook_base::hook_base(hook_base&& other) noexcept {
   move_hook(std::move(other));
 }
 
-hook_base& hook_base::operator=(const hook_base& /*unused*/) noexcept {
+auto hook_base::operator=(const hook_base& /*unused*/) noexcept -> hook_base& {
   return *this;
 }
 
-hook_base& hook_base::operator=(hook_base&& other) noexcept {
+auto hook_base::operator=(hook_base&& other) noexcept -> hook_base& {
   if (this == std::addressof(other)) {
     return *this;
   }
@@ -39,11 +39,11 @@ hook_base::~hook_base() {
   unlink();
 }
 
-bool hook_base::is_linked() const noexcept {
+auto hook_base::is_linked() const noexcept -> bool {
   return m_prev != this;
 }
 
-void hook_base::unlink() noexcept {
+auto hook_base::unlink() noexcept -> void {
   m_prev->m_next = m_next;
   m_next->m_prev = m_prev;
 
