43a0a3108ecf19d3227e231a94f99b1dcc595584
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-23 14:48:41 +0300

    update tests for uasort

--- tests/phpt/array/042_uasort.php
+++ tests/phpt/array/042_uasort.php
@@ -1,4 +1,4 @@
-@skip php8
+@ok
 <?php
 
 require_once 'kphp_tester_include.php';
@@ -23,17 +23,13 @@ function test_uasort_ints() {
     uasort($arr1, 'cmp_int');
     var_dump($arr1);
 
-    $arr2 = [5, 1, 4, 2, 3, 2, 2, 5];
+    $arr2 = [1, 2, 3, 4, 5];
     uasort($arr2, 'cmp_int');
     var_dump($arr2);
 
-    $arr3 = [1, 2, 3, 4, 5];
+    $arr3 = [5, 4, 3, 2, 1];
     uasort($arr3, 'cmp_int');
     var_dump($arr3);
-
-    $arr4 = [5, 4, 3, 2, 1];
-    uasort($arr4, 'cmp_int');
-    var_dump($arr4);
 }
 
 function test_uasort_strings() {
@@ -41,17 +37,13 @@ function test_uasort_strings() {
     uasort($arr1, 'cmp_str');
     var_dump($arr1);
 
-    $arr2 = ["d", "b", "a", "e", "c", "e", "e", "d"];
+    $arr2 = ["a", "b", "c", "d", "e"];
     uasort($arr2, 'cmp_str');
     var_dump($arr2);
 
-    $arr3 = ["a", "b", "c", "d", "e"];
+    $arr3 = ["e", "d", "c", "b", "a"];
     uasort($arr3, 'cmp_str');
-    var_dump($arr3);
-
-    $arr4 = ["e", "d", "c", "b", "a"];
-    uasort($arr4, 'cmp_str');
-    var_dump($arr4);    
+    var_dump($arr3);    
 }
 
 function test_uasort_with_keys() {
@@ -59,17 +51,13 @@ function test_uasort_with_keys() {
     uasort($arr1, 'cmp_int');
     var_dump($arr1);
 
-    $arr2 = ["a" => 5, "b" => 1, "c" => 4, "d" => 2, "e" => 3, "f" => 2, "g" => 2, "h" => 5];
+    $arr2 = ["a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5];
     uasort($arr2, 'cmp_int');
     var_dump($arr2);
 
-    $arr3 = ["a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5];
+    $arr3 = ["a" => 5, "b" => 4, "c" => 3, "d" => 2, "e" => 1];
     uasort($arr3, 'cmp_int');
     var_dump($arr3);
-
-    $arr4 = ["a" => 5, "b" => 4, "c" => 3, "d" => 2, "e" => 1];
-    uasort($arr4, 'cmp_int');
-    var_dump($arr4);
 }
 
 function test_uasort_empty_async() {
@@ -84,17 +72,13 @@ function test_uasort_ints_async() {
     uasort($arr1, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr1);
 
-    $arr2 = [5, 1, 4, 2, 3, 2, 2, 5];
+    $arr2 = [1, 2, 3, 4, 5];
     uasort($arr2, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr2);
 
-    $arr3 = [1, 2, 3, 4, 5];
+    $arr3 = [5, 4, 3, 2, 1];
     uasort($arr3, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr3);
-
-    $arr4 = [5, 4, 3, 2, 1];
-    uasort($arr4, function($a, $b) { sleep(0); return cmp_int($a, $b); });
-    var_dump($arr4);
 }
 
 function test_uasort_strings_async() {
@@ -102,17 +86,13 @@ function test_uasort_strings_async() {
     uasort($arr1, function($a, $b) { sleep(0); return cmp_str($a, $b); });
     var_dump($arr1);
 
-    $arr2 = ["d", "b", "a", "e", "c", "e", "e", "d"];
+    $arr2 = ["a", "b", "c", "d", "e"];
     uasort($arr2, function($a, $b) { sleep(0); return cmp_str($a, $b); });
     var_dump($arr2);
 
-    $arr3 = ["a", "b", "c", "d", "e"];
+    $arr3 = ["e", "d", "c", "b", "a"];
     uasort($arr3, function($a, $b) { sleep(0); return cmp_str($a, $b); });
-    var_dump($arr3);
-
-    $arr4 = ["e", "d", "c", "b", "a"];
-    uasort($arr4, function($a, $b) { sleep(0); return cmp_str($a, $b); });
-    var_dump($arr4);    
+    var_dump($arr3);    
 }
 
 function test_uasort_with_keys_async() {
@@ -120,17 +100,13 @@ function test_uasort_with_keys_async() {
     uasort($arr1, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr1);
 
-    $arr2 = ["a" => 5, "b" => 1, "c" => 4, "d" => 2, "e" => 3, "f" => 2, "g" => 2, "h" => 5];
+    $arr2 = ["a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5];
     uasort($arr2, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr2);
 
-    $arr3 = ["a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5];
+    $arr3 = ["a" => 5, "b" => 4, "c" => 3, "d" => 2, "e" => 1];
     uasort($arr3, function($a, $b) { sleep(0); return cmp_int($a, $b); });
     var_dump($arr3);
-
-    $arr4 = ["a" => 5, "b" => 4, "c" => 3, "d" => 2, "e" => 1];
-    uasort($arr4, function($a, $b) { sleep(0); return cmp_int($a, $b); });
-    var_dump($arr4);
 }
 
 test_uasort_empty();
