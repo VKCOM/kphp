374b2372f6c7711fd544d533a676614154b20f0f
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-24 16:22:55 +0300

    implement helper structures and functions

--- runtime-common/core/std/intrusive-list.h
+++ runtime-common/core/std/intrusive-list.h
@@ -4,67 +4,206 @@
 
 #pragma once
 
-#include <cassert>
 #include <cstddef>
-#include <memory>
+#include <iterator>
 #include <type_traits>
 #include <utility>
 
-namespace kphp::stl::intrusive_list {
+namespace kphp::stl::intrusive {
 
 namespace details {
 
-class hook_base {
-  hook_base* m_prev{this};
-  hook_base* m_next{this};
+class list_node_base {
+  list_node_base* m_prev{this};
+  list_node_base* m_next{this};
 
-  auto move_hook(hook_base&& other) noexcept -> void;
+  constexpr auto insert_instead(list_node_base&& other) noexcept -> void;
 
 public:
-  hook_base() noexcept = default;
+  constexpr list_node_base() noexcept = default;
 
-  hook_base(const hook_base& /*unused*/) noexcept;
+  list_node_base(const list_node_base& /*unused*/) noexcept = delete;
 
-  hook_base(hook_base&& other) noexcept;
+  constexpr list_node_base(list_node_base&& other) noexcept;
 
-  auto operator=(const hook_base& /*unused*/) noexcept -> hook_base&;
+  auto operator=(const list_node_base& /*unused*/) noexcept -> list_node_base& = delete;
 
-  auto operator=(hook_base&& other) noexcept -> hook_base&;
+  constexpr auto operator=(list_node_base&& other) noexcept -> list_node_base&;
 
-  ~hook_base();
+  ~list_node_base();
 
-  auto is_linked() const noexcept -> bool;
+  constexpr auto is_linked() const noexcept -> bool;
 
-  auto unlink() noexcept -> void;
+  constexpr auto unlink() noexcept -> void;
+};
+
+template<typename T>
+class list_node final : public list_node_base {
+  T m_value;
+
+public:
+  constexpr explicit list_node(T value) noexcept
+      : m_value{std::move(value)} {}
+
+  list_node(const list_node& other) = delete;
+
+  list_node(list_node&& other) = delete;
+
+  auto operator=(const list_node& other) -> list_node& = delete;
+
+  auto operator=(list_node&& other) -> list_node& = delete;
+
+  constexpr ~list_node() = default;
+
+  constexpr auto get() noexcept -> T& {
+    return m_value;
+  }
+
+  constexpr auto get() const noexcept -> const T& {
+    return m_value;
+  }
+};
+
+template<typename T>
+class list_node<T&> final : public list_node_base {
+  T& m_value;
+
+public:
+  constexpr explicit list_node(T& value) noexcept
+      : m_value{value} {}
+
+  list_node(const list_node& other) = delete;
+
+  list_node(list_node&& other) = delete;
+
+  auto operator=(const list_node& other) -> list_node& = delete;
+
+  auto operator=(list_node&& other) -> list_node& = delete;
+
+  constexpr ~list_node() = default;
+
+  constexpr auto get() noexcept -> T& {
+    return m_value;
+  }
+
+  constexpr auto get() const noexcept -> const T& {
+    return m_value;
+  }
 };
 
 } // namespace details
 
-struct default_tag {};
+// Node for object that is stored in single intrusive list
+template<typename T>
+using owning_list_node = details::list_node<std::remove_pointer_t<std::remove_reference_t<T>>>;
 
-template<typename Tag = default_tag>
-class hook : private details::hook_base {};
+// Node for object that is stored in multiple intrusive lists
+template<typename T>
+using non_owning_list_node = details::list_node<std::remove_pointer_t<std::remove_reference_t<T>>&>;
 
-template<typename T, typename Tag = default_tag>
-class intrusive_list final {
-  static_assert(std::is_base_of_v<hook<Tag>, T>, "T must be derived from hook<Tag>");
+template<typename T>
+constexpr auto make_owning_list_node(T value) noexcept -> owning_list_node<T> {
+  return owning_list_node<T>{std::move(value)};
+}
 
-  details::hook_base m_sentinel;
+template<typename T>
+constexpr auto make_non_owning_list_node(T& value) noexcept -> non_owning_list_node<T> {
+  return non_owning_list_node<T>{value};
+}
+
+template<typename T>
+class list_iterator;
+
+template<typename T>
+class list final {
+  details::list_node_base m_sentinel;
 
 public:
   using value_type = T;
-  using size_type = std::size_t;
-  using difference_type = std::ptrdiff_t;
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
   using reference = value_type&;
   using const_reference = const value_type&;
   using pointer = value_type*;
   using const_pointer = const value_type*;
-  using iterator = void;
-  using const_iterator = void;
-  using reverse_iterator = void;
-  using const_reverse_iterator = void;
+  using iterator = list_iterator<T>;
+  using const_iterator = list_iterator<const T>;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-  intrusive_list();
+  constexpr list() noexcept = default;
+
+  template<typename It>
+  constexpr list(It first, It last);
+
+  list(const list& other) = delete;
+
+  constexpr list(list&& other) noexcept = default;
+
+  auto operator=(const list& other) -> list& = delete;
+
+  constexpr auto operator=(list&& other) noexcept -> list& = default;
+
+  ~list() = default;
+
+  constexpr auto front() -> reference;
+
+  constexpr auto front() const -> const_reference;
+
+  constexpr auto back() -> reference;
+
+  constexpr auto back() const -> const_reference;
+
+  constexpr auto begin() -> iterator;
+
+  constexpr auto begin() const -> const_iterator;
+
+  constexpr auto cbegin() const -> const_iterator;
+
+  constexpr auto end() -> iterator;
+
+  constexpr auto end() const -> const_iterator;
+
+  constexpr auto cend() const -> const_iterator;
+
+  constexpr auto rbegin() -> reverse_iterator;
+
+  constexpr auto rbegin() const -> const_reverse_iterator;
+
+  constexpr auto crbegin() const -> const_reverse_iterator;
+
+  constexpr auto rend() -> reverse_iterator;
+
+  constexpr auto rend() const -> const_reverse_iterator;
+
+  constexpr auto crend() const -> const_reverse_iterator;
+
+  constexpr auto empty() const -> bool;
+
+  constexpr auto size() const -> size_type;
+
+  constexpr auto clear() -> void;
+
+  constexpr auto insert(const_iterator pos, const_reference value) -> iterator;
+
+  template<typename It>
+  constexpr auto insert(const_iterator pos, It first, It last) -> iterator;
+
+  constexpr auto erase(const_iterator pos) -> iterator;
+
+  constexpr auto erase(const_iterator first, const_iterator last) -> iterator;
+
+  constexpr auto push_back(const_reference value) -> void;
+
+  constexpr auto push_front(const_reference value) -> void;
+
+  constexpr auto pop_back() -> void;
+
+  constexpr auto pop_front() -> void;
+
+  constexpr auto swap(list& other) -> void;
+
+  constexpr auto merge(list& other) -> void;
 };
 
-} // namespace kphp::stl::intrusive_list
+} // namespace kphp::stl::intrusive
