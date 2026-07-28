da30160d60739198ac20c29bc7b31fecad886c0e
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-28 17:01:51 +0300

    apply code style

--- runtime-common/core/std/intrusive-list.h
+++ runtime-common/core/std/intrusive-list.h
@@ -419,7 +419,7 @@ public:
     if (this == std::addressof(other)) {
       return;
     }
-    
+
     details::list_node_base tmp{std::move(m_sentinel)};
     m_sentinel = std::move(other.m_sentinel);
     other.m_sentinel = std::move(tmp);
