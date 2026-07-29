a90f1b0fc0c9349c95c82bf31f764c484b6596f7
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-07-29 19:34:02 +0300

    small fixes

--- common/containers/intrusive-list.h
+++ common/containers/intrusive-list.h
@@ -348,6 +348,7 @@ public:
     return !m_sentinel.is_linked();
   }
 
+  // complexity O(n)
   auto size() const noexcept -> size_type {
     return std::distance(begin(), end());
   }
@@ -356,6 +357,7 @@ public:
     m_sentinel.unlink();
   }
 
+  // if the node is linked in other list with the same tag, it will be unlinked from other list before insertion
   auto insert(const_iterator pos, Node& node) noexcept -> iterator {
     auto* next_node = pos.m_curr;
     auto* new_node = list_node_base_from_list_node(node);
