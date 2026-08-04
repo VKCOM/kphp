51e7642903b5f38cb597180c620d772573e98a51
Alexander Polyakov <al.polyakov@vk.team>
2026-08-05 01:16:32 +0300

    a bit of confdata cmake refactoring

--- runtime-light/components/confdata/confdata.cmake
+++ runtime-light/components/confdata/confdata.cmake
@@ -1,16 +1,29 @@
 # k2-confdata image: a standalone C++ component that shares the common
 # runtime-light machinery but provides its own entry points and bindings
-set(K2_CONFDATA_SRC
+set(K2_CONFDATA_COMPONENT_SRC
     ${RUNTIME_LIGHT_DIR}/components/confdata/confdata-component.cpp
     ${RUNTIME_LIGHT_DIR}/components/confdata/bindings/bindings.cpp
-    ${RUNTIME_LIGHT_DIR}/components/confdata/state/instance-state.cpp
+    ${RUNTIME_LIGHT_DIR}/components/confdata/state/instance-state.cpp)
+
+set(K2_CONFDATA_ALLOCATOR_SRC
     ${RUNTIME_LIGHT_DIR}/allocator/runtime-light-allocator.cpp
+    ${RUNTIME_LIGHT_DIR}/memory-resource-impl/monotonic-light-buffer-resource.cpp)
+
+set(K2_CONFDATA_DIAGNOSTICS_SRC
     ${RUNTIME_LIGHT_DIR}/stdlib/diagnostics/backtrace.cpp
-    ${RUNTIME_LIGHT_DIR}/stdlib/diagnostics/php-assert.cpp
+    ${RUNTIME_LIGHT_DIR}/stdlib/diagnostics/php-assert.cpp)
+
+set(K2_CONFDATA_MEMORY_RESOURCE_SRC
     ${RUNTIME_COMMON_DIR}/core/memory-resource/unsynchronized_pool_resource.cpp
     ${RUNTIME_COMMON_DIR}/core/memory-resource/monotonic_buffer_resource.cpp
     ${RUNTIME_COMMON_DIR}/core/memory-resource/details/memory_chunk_tree.cpp
-    ${RUNTIME_COMMON_DIR}/core/memory-resource/details/memory_ordered_chunk_list.cpp
+    ${RUNTIME_COMMON_DIR}/core/memory-resource/details/memory_ordered_chunk_list.cpp)
+
+set(K2_CONFDATA_SRC
+    ${K2_CONFDATA_COMPONENT_SRC}
+    ${K2_CONFDATA_ALLOCATOR_SRC}
+    ${K2_CONFDATA_DIAGNOSTICS_SRC}
+    ${K2_CONFDATA_MEMORY_RESOURCE_SRC}
     # link the alloc-wrapper objects directly (not as an archive) so that
     # __wrap_* definitions are always present regardless of link order
     $<TARGET_OBJECTS:libc-alloc-wrapper-pic>)
