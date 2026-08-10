25aa85c8040e5c47adb975060d6369ed9eeccabf
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-10 18:05:36 +0300

    apply code style

--- runtime-light/allocator/coroutine-allocator.h
+++ runtime-light/allocator/coroutine-allocator.h
@@ -1,6 +1,6 @@
-//  Compiler for PHP (aka KPHP)
-//  Copyright (c) 2026 LLC «V Kontakte»
-//  Distributed under the GPL v3 License, see LICENSE.notice.txt
+// Compiler for PHP (aka KPHP)
+// Copyright (c) 2026 LLC «V Kontakte»
+// Distributed under the GPL v3 License, see LICENSE.notice.txt
 
 #pragma once
 
