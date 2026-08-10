21d42dd7abf193a3038e302bdf5d662e74284147
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-10 18:04:23 +0300

    implement coroutine allocator

---
+++ runtime-light/allocator/coroutine-allocator.h
@@ -0,0 +1,34 @@
+//  Compiler for PHP (aka KPHP)
+//  Copyright (c) 2026 LLC «V Kontakte»
+//  Distributed under the GPL v3 License, see LICENSE.notice.txt
+
+#pragma once
+
+#include <cstddef>
+
+#include "common/mixin/not_copyable.h"
+#include "runtime-common/core/memory-resource/unsynchronized_pool_resource.h"
+
+struct CoroutineAllocator final : vk::not_copyable {
+  static auto get() noexcept -> CoroutineAllocator&;
+
+  CoroutineAllocator() = default;
+  CoroutineAllocator(size_t mem_size, size_t min_extra_mem_size, size_t oom_handling_mem_size);
+
+  void init(void* buffer, size_t mem_size, size_t oom_handling_mem_size);
+  void free();
+
+  auto alloc_memory(size_t size) noexcept -> void*;
+  auto alloc0_memory(size_t size) noexcept -> void*;
+  auto realloc_memory(void* mem, size_t new_size, size_t old_size) noexcept -> void*;
+  auto free_memory(void* mem, size_t size) noexcept -> void;
+
+private:
+  auto request_extra_memory(size_t requested_size) noexcept -> void;
+
+public:
+  memory_resource::unsynchronized_pool_resource memory_resource;
+
+private:
+  size_t m_min_extra_mem_size{0};
+};
