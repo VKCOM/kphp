3a19375f0f1fbb98cf3abf4eaea0de3822c8b49d
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-13 17:57:08 +0300

    replace inheritance with composition

--- runtime-light/allocator/runtime-light-allocator.cpp
+++ runtime-light/allocator/runtime-light-allocator.cpp
@@ -5,46 +5,49 @@
 #include "runtime-common/core/allocator/runtime-allocator.h"
 #include "runtime-light/allocator/allocator-state.h"
 
-RuntimeAllocator& RuntimeAllocator::get() noexcept {
+auto RuntimeAllocator::get() noexcept -> RuntimeAllocator& {
   return AllocatorState::get_mutable().allocator;
 }
 
+RuntimeAllocator::RuntimeAllocator(size_t script_mem_size, size_t min_extra_mem_size, size_t oom_handling_mem_size) noexcept
+    : m_allocator{script_mem_size, min_extra_mem_size, oom_handling_mem_size} {}
+
 auto RuntimeAllocator::init(void* buffer, size_t script_mem_size, size_t oom_handling_mem_size) noexcept -> void {
-  kphp::memory::details::PoolAllocator::PoolAllocator::init(buffer, script_mem_size, oom_handling_mem_size);
+  m_allocator.init(buffer, script_mem_size, oom_handling_mem_size);
 }
 
 auto RuntimeAllocator::free() noexcept -> void {
-  kphp::memory::details::PoolAllocator::PoolAllocator::free();
+  m_allocator.free();
 }
 
 auto RuntimeAllocator::alloc_script_memory(size_t size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::alloc_script_memory(size);
+  return m_allocator.alloc_script_memory(size);
 }
 
 auto RuntimeAllocator::alloc0_script_memory(size_t size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::alloc0_script_memory(size);
+  return m_allocator.alloc0_script_memory(size);
 }
 
 auto RuntimeAllocator::realloc_script_memory(void* mem, size_t new_size, size_t old_size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::realloc_script_memory(mem, new_size, old_size);
+  return m_allocator.realloc_script_memory(mem, new_size, old_size);
 }
 
 auto RuntimeAllocator::free_script_memory(void* mem, size_t size) noexcept -> void {
-  kphp::memory::details::PoolAllocator::PoolAllocator::free_script_memory(mem, size);
+  m_allocator.free_script_memory(mem, size);
 }
 
 auto RuntimeAllocator::alloc_global_memory(size_t size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::alloc_global_memory(size);
+  return m_allocator.alloc_global_memory(size);
 }
 
 auto RuntimeAllocator::alloc0_global_memory(size_t size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::alloc0_global_memory(size);
+  return m_allocator.alloc0_global_memory(size);
 }
 
 auto RuntimeAllocator::realloc_global_memory(void* mem, size_t new_size, size_t old_size) noexcept -> void* {
-  return kphp::memory::details::PoolAllocator::PoolAllocator::realloc_global_memory(mem, new_size, old_size);
+  return m_allocator.realloc_global_memory(mem, new_size, old_size);
 }
 
 auto RuntimeAllocator::free_global_memory(void* mem, size_t size) noexcept -> void {
-  kphp::memory::details::PoolAllocator::PoolAllocator::free_global_memory(mem, size);
+  m_allocator.free_global_memory(mem, size);
 }
