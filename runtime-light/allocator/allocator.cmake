ff1e1cbc3b681859e091030248ae88335340c7d9
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-13 23:20:15 +0300

    add sources to cmake

--- runtime-light/allocator/allocator.cmake
+++ runtime-light/allocator/allocator.cmake
@@ -1,1 +1,4 @@
-set(RUNTIME_LIGHT_ALLOCATOR_SRC allocator/runtime-light-allocator.cpp)
+set(RUNTIME_LIGHT_ALLOCATOR_SRC
+    allocator/runtime-light-allocator.cpp
+    allocator/runtime-coroutine-allocator.cpp
+    allocator/details/pool-allocator.cpp)
