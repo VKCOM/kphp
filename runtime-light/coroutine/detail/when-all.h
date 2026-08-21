48812b1e2d70bab72d0d4c4a34d5701faeebd2ed
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-21 19:03:40 +0300

    refactor coroutine memory

--- runtime-light/coroutine/detail/when-all.h
+++ runtime-light/coroutine/detail/when-all.h
@@ -14,7 +14,6 @@
 #include <type_traits>
 #include <utility>
 
-#include "runtime-light/allocator/coroutine-malloc-interface.h"
 #include "runtime-light/coroutine/async-stack.h"
 #include "runtime-light/coroutine/concepts.h"
 #include "runtime-light/coroutine/control-functions.h"
