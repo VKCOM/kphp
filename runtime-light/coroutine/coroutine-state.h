eef3249c07b9ffd21f610c44ac75385810a406e3
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-23 22:20:28 +0300

    add bootstrap memory resource for tasks

--- runtime-light/coroutine/coroutine-state.h
+++ runtime-light/coroutine/coroutine-state.h
@@ -4,11 +4,13 @@
 
 #pragma once
 
-#include "common/mixin/not_copyable.h"
+#include <memory>
 
+#include "common/mixin/not_copyable.h"
 #include "runtime-light/coroutine/async-stack.h"
 #include "runtime-light/coroutine/detail/allocator/runtime-coroutine-allocator.h"
 #include "runtime-light/coroutine/detail/allocator/task-allocator.h"
+#include "runtime-light/coroutine/root-promise.h"
 
 namespace kphp::coro {
 
@@ -17,12 +19,15 @@ struct instance_state final : private vk::not_copyable {
   instance_state(size_t coroutine_mem_size, size_t min_extra_coroutine_mem_size, size_t oom_handling_coroutine_mem_size, size_t task_mem_size,
                  size_t task_allocator_segment_size, size_t min_extra_task_mem_size, size_t oom_handling_task_mem_size) noexcept
       : coroutine_allocator{coroutine_mem_size, min_extra_coroutine_mem_size, oom_handling_coroutine_mem_size},
-        task_allocator{task_mem_size, task_allocator_segment_size, min_extra_task_mem_size, oom_handling_task_mem_size} {}
+        task_allocator{task_mem_size, task_allocator_segment_size, min_extra_task_mem_size, oom_handling_task_mem_size} {
+    task_allocator.set(std::addressof(bootstrap_root_promise.get_task_memory_resource()));
+  }
 
   static instance_state& get() noexcept;
 
   kphp::coro::detail::memory::RuntimeCoroutineAllocator coroutine_allocator;
   kphp::coro::detail::memory::task_allocator task_allocator;
+  kphp::coro::root_promise bootstrap_root_promise;
   kphp::coro::async_stack_root coroutine_stack_root;
 };
 
