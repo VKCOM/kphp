806b7485954d57ba1e1eeb2fd6d17abff58d48b5
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-08-23 22:20:28 +0300

    add bootstrap memory resource for tasks

--- runtime-light/coroutine/coroutine-state.h
+++ runtime-light/coroutine/coroutine-state.h
@@ -12,6 +12,7 @@
 #include "runtime-light/coroutine/async-stack.h"
 #include "runtime-light/coroutine/detail/allocator/runtime-coroutine-allocator.h"
 #include "runtime-light/coroutine/detail/allocator/task-allocator.h"
+#include "runtime-light/coroutine/root-promise.h"
 
 namespace kphp::coro {
 
@@ -30,6 +31,7 @@ struct instance_state final : private vk::not_copyable {
 
   kphp::memory::pool_allocator coroutine_allocator;
   kphp::coro::detail::memory::task_allocator task_allocator;
+  kphp::coro::root_promise bootstrap_root_promise;
   kphp::coro::async_stack_root coroutine_stack_root;
 };
 
