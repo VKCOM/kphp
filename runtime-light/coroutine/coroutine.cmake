1b08a8d921d13d542cddd75ac660dee7ac01bef9
Kirill Kotliar <k.kotliar@vkteam.ru>
2026-09-01 12:39:54 +0300

    fix circular includes

--- runtime-light/coroutine/coroutine.cmake
+++ runtime-light/coroutine/coroutine.cmake
@@ -1,3 +1,3 @@
 set(RUNTIME_LIGHT_COROUTINE_SRC
-    coroutine/detail/allocator/runtime-coroutine-allocator.cpp
-    coroutine/detail/allocator/task-allocator.cpp)
+    coroutine/detail/allocator/task-allocator.cpp
+    coroutine/detail/allocator/runtime-coroutine-allocator.cpp)
