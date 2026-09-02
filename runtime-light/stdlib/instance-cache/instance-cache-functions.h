4ebc228bff8291974f27d5e879d9d7fad8be165a
Denis Zubarev <denis.zubarev@vkteam.ru>
2026-09-02 17:38:04 +0300

    added: k2_free_shared_memory + removed: fetch optimisation, part of test_delete

--- runtime-light/stdlib/instance-cache/instance-cache-functions.h
+++ runtime-light/stdlib/instance-cache/instance-cache-functions.h
@@ -117,7 +117,7 @@ ClassInstanceType f$instance_cache_fetch(const string& class_name, const string&
     return unwrap(it->second);
   }
 
-  auto get_result{k2::get_shared_memory(std::string_view{key.c_str(), key.size()}, true)};
+  auto get_result{k2::get_shared_memory(std::string_view{key.c_str(), key.size()})};
   if (!get_result.has_value()) {
     return {};
   }
